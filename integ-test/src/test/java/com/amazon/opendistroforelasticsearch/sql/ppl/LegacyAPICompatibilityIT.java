bcb7b18d0389202ac29915bf147cd2175ec0c2f6
Chen Dai <daichen@amazon.com>
2021-04-22 13:51:36 -0700

    Add missing license header
    Signed-off-by: Chen Dai <daichen@amazon.com>

--- integ-test/src/test/java/com/amazon/opendistroforelasticsearch/sql/ppl/LegacyAPICompatibilityIT.java
+++ integ-test/src/test/java/com/amazon/opendistroforelasticsearch/sql/ppl/LegacyAPICompatibilityIT.java
@@ -1,3 +1,13 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
 package com.amazon.opendistroforelasticsearch.sql.ppl;
 
 import static com.amazon.opendistroforelasticsearch.sql.plugin.rest.RestPPLQueryAction.LEGACY_EXPLAIN_API_ENDPOINT;
