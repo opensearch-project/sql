f63bee6133213fa4a0507ea369eb5747b18d169c
chloe-zh <chloezh1102@gmail.com>
2021-05-22 21:18:07 -0700

    update style
    Signed-off-by: chloe-zh <chloezh1102@gmail.com>

--- opensearch/src/main/java/org/opensearch/sql/opensearch/setting/LegacyOpenDistroSettings.java
+++ opensearch/src/main/java/org/opensearch/sql/opensearch/setting/LegacyOpenDistroSettings.java
@@ -21,61 +21,61 @@ import org.opensearch.sql.common.setting.LegacySettings;
 public class LegacyOpenDistroSettings {
 
   public static final Setting<Boolean> SQL_ENABLED_SETTING = Setting.boolSetting(
-    LegacySettings.Key.SQL_ENABLED.getKeyValue(),
-    true,
-    Setting.Property.NodeScope,
-    Setting.Property.Dynamic,
-    Setting.Property.Deprecated);
+      LegacySettings.Key.SQL_ENABLED.getKeyValue(),
+      true,
+      Setting.Property.NodeScope,
+      Setting.Property.Dynamic,
+      Setting.Property.Deprecated);
 
   public static final Setting<Integer> SQL_QUERY_SLOWLOG_SETTING = Setting.intSetting(
-    LegacySettings.Key.SQL_QUERY_SLOWLOG.getKeyValue(),
-    2,
-    Setting.Property.NodeScope,
-    Setting.Property.Dynamic,
-    Setting.Property.Deprecated);
+      LegacySettings.Key.SQL_QUERY_SLOWLOG.getKeyValue(),
+      2,
+      Setting.Property.NodeScope,
+      Setting.Property.Dynamic,
+      Setting.Property.Deprecated);
 
   public static final Setting<TimeValue> SQL_CURSOR_KEEPALIVE_SETTING = Setting.positiveTimeSetting(
-    LegacySettings.Key.SQL_CURSOR_KEEPALIVE.getKeyValue(),
-    timeValueMinutes(1),
-    Setting.Property.NodeScope,
-    Setting.Property.Dynamic,
-    Setting.Property.Deprecated);
+      LegacySettings.Key.SQL_CURSOR_KEEPALIVE.getKeyValue(),
+      timeValueMinutes(1),
+      Setting.Property.NodeScope,
+      Setting.Property.Dynamic,
+      Setting.Property.Deprecated);
 
   public static final Setting<Long> METRICS_ROLLING_WINDOW_SETTING = Setting.longSetting(
-    LegacySettings.Key.METRICS_ROLLING_WINDOW.getKeyValue(),
-    3600L,
-    2L,
-    Setting.Property.NodeScope,
-    Setting.Property.Dynamic,
-    Setting.Property.Deprecated);
+      LegacySettings.Key.METRICS_ROLLING_WINDOW.getKeyValue(),
+      3600L,
+      2L,
+      Setting.Property.NodeScope,
+      Setting.Property.Dynamic,
+      Setting.Property.Deprecated);
 
   public static final Setting<Long> METRICS_ROLLING_INTERVAL_SETTING = Setting.longSetting(
-    LegacySettings.Key.METRICS_ROLLING_INTERVAL.getKeyValue(),
-    60L,
-    1L,
-    Setting.Property.NodeScope,
-    Setting.Property.Dynamic,
-    Setting.Property.Deprecated);
+      LegacySettings.Key.METRICS_ROLLING_INTERVAL.getKeyValue(),
+      60L,
+      1L,
+      Setting.Property.NodeScope,
+      Setting.Property.Dynamic,
+      Setting.Property.Deprecated);
 
   public static final Setting<Boolean> PPL_ENABLED_SETTING = Setting.boolSetting(
-    LegacySettings.Key.PPL_ENABLED.getKeyValue(),
-    true,
-    Setting.Property.NodeScope,
-    Setting.Property.Dynamic,
-    Setting.Property.Deprecated);
+      LegacySettings.Key.PPL_ENABLED.getKeyValue(),
+      true,
+      Setting.Property.NodeScope,
+      Setting.Property.Dynamic,
+      Setting.Property.Deprecated);
 
   public static final Setting<ByteSizeValue>
-    PPL_QUERY_MEMORY_LIMIT_SETTING = Setting.memorySizeSetting(
-    LegacySettings.Key.PPL_QUERY_MEMORY_LIMIT.getKeyValue(),
-    "85%",
-    Setting.Property.NodeScope,
-    Setting.Property.Dynamic,
-    Setting.Property.Deprecated);
+      PPL_QUERY_MEMORY_LIMIT_SETTING = Setting.memorySizeSetting(
+      LegacySettings.Key.PPL_QUERY_MEMORY_LIMIT.getKeyValue(),
+      "85%",
+      Setting.Property.NodeScope,
+      Setting.Property.Dynamic,
+      Setting.Property.Deprecated);
 
   public static final Setting<Integer> QUERY_SIZE_LIMIT_SETTING = Setting.intSetting(
-    LegacySettings.Key.QUERY_SIZE_LIMIT.getKeyValue(),
-    200,
-    Setting.Property.NodeScope,
-    Setting.Property.Dynamic,
-    Setting.Property.Deprecated);
+      LegacySettings.Key.QUERY_SIZE_LIMIT.getKeyValue(),
+      200,
+      Setting.Property.NodeScope,
+      Setting.Property.Dynamic,
+      Setting.Property.Deprecated);
 }
--- opensearch/src/main/java/org/opensearch/sql/opensearch/setting/OpenSearchSettings.java
+++ opensearch/src/main/java/org/opensearch/sql/opensearch/setting/OpenSearchSettings.java
@@ -62,67 +62,67 @@ public class OpenSearchSettings extends Settings {
   private final Map<Settings.Key, Object> latestSettings = new ConcurrentHashMap<>();
 
   public static final Setting<?> SQL_ENABLED_SETTING = Setting.boolSetting(
-    Key.SQL_ENABLED.getKeyValue(),
-    LegacyOpenDistroSettings.SQL_ENABLED_SETTING,
-    Setting.Property.NodeScope,
-    Setting.Property.Dynamic);
+      Key.SQL_ENABLED.getKeyValue(),
+      LegacyOpenDistroSettings.SQL_ENABLED_SETTING,
+      Setting.Property.NodeScope,
+      Setting.Property.Dynamic);
 
   public static final Setting<?> SQL_SLOWLOG_SETTING = Setting.intSetting(
-    Key.SQL_SLOWLOG.getKeyValue(),
-    LegacyOpenDistroSettings.SQL_QUERY_SLOWLOG_SETTING,
-    0,
-    Setting.Property.NodeScope,
-    Setting.Property.Dynamic);
+      Key.SQL_SLOWLOG.getKeyValue(),
+      LegacyOpenDistroSettings.SQL_QUERY_SLOWLOG_SETTING,
+      0,
+      Setting.Property.NodeScope,
+      Setting.Property.Dynamic);
 
   public static final Setting<?> SQL_CURSOR_KEEP_ALIVE_SETTING = Setting.positiveTimeSetting(
-    Key.SQL_CURSOR_KEEP_ALIVE.getKeyValue(),
-    LegacyOpenDistroSettings.SQL_CURSOR_KEEPALIVE_SETTING,
-    timeValueMinutes(1),
-    Setting.Property.NodeScope,
-    Setting.Property.Dynamic);
+      Key.SQL_CURSOR_KEEP_ALIVE.getKeyValue(),
+      LegacyOpenDistroSettings.SQL_CURSOR_KEEPALIVE_SETTING,
+      timeValueMinutes(1),
+      Setting.Property.NodeScope,
+      Setting.Property.Dynamic);
 
   public static final Setting<?> SQL_DELETE_ENABLED_SETTING = Setting.boolSetting(
-    Key.SQL_DELETE_ENABLED.getKeyValue(),
-    false,
-    Setting.Property.NodeScope,
-    Setting.Property.Dynamic);
+      Key.SQL_DELETE_ENABLED.getKeyValue(),
+      false,
+      Setting.Property.NodeScope,
+      Setting.Property.Dynamic);
 
   public static final Setting<?> PPL_ENABLED_SETTING = Setting.boolSetting(
-    Key.PPL_ENABLED.getKeyValue(),
-    LegacyOpenDistroSettings.PPL_ENABLED_SETTING,
-    Setting.Property.NodeScope,
-    Setting.Property.Dynamic);
+      Key.PPL_ENABLED.getKeyValue(),
+      LegacyOpenDistroSettings.PPL_ENABLED_SETTING,
+      Setting.Property.NodeScope,
+      Setting.Property.Dynamic);
 
   public static final Setting<?> QUERY_MEMORY_LIMIT_SETTING = new Setting<>(
-    Key.QUERY_MEMORY_LIMIT.getKeyValue(),
-    LegacyOpenDistroSettings.PPL_QUERY_MEMORY_LIMIT_SETTING,
-    (s) -> MemorySizeValue.parseBytesSizeValueOrHeapRatio(
-      s, LegacySettings.Key.PPL_QUERY_MEMORY_LIMIT.getKeyValue()),
-    Setting.Property.NodeScope,
-    Setting.Property.Dynamic);
+      Key.QUERY_MEMORY_LIMIT.getKeyValue(),
+      LegacyOpenDistroSettings.PPL_QUERY_MEMORY_LIMIT_SETTING,
+      (s) -> MemorySizeValue.parseBytesSizeValueOrHeapRatio(
+          s, LegacySettings.Key.PPL_QUERY_MEMORY_LIMIT.getKeyValue()),
+      Setting.Property.NodeScope,
+      Setting.Property.Dynamic);
 
   public static final Setting<?> QUERY_SIZE_LIMIT_SETTING = Setting.intSetting(
-    Key.QUERY_SIZE_LIMIT.getKeyValue(),
-    LegacyOpenDistroSettings.QUERY_SIZE_LIMIT_SETTING,
-    0,
-    Setting.Property.NodeScope,
-    Setting.Property.Dynamic);
+      Key.QUERY_SIZE_LIMIT.getKeyValue(),
+      LegacyOpenDistroSettings.QUERY_SIZE_LIMIT_SETTING,
+      0,
+      Setting.Property.NodeScope,
+      Setting.Property.Dynamic);
 
   public static final Setting<?> METRICS_ROLLING_WINDOW_SETTING = new Setting<>(
-    Key.METRICS_ROLLING_WINDOW.getKeyValue(),
-    LegacyOpenDistroSettings.METRICS_ROLLING_WINDOW_SETTING,
-    (s) -> Setting.parseLong(
-      s, 2L, Key.METRICS_ROLLING_WINDOW.getKeyValue()),
-    Setting.Property.NodeScope,
-    Setting.Property.Dynamic);
+      Key.METRICS_ROLLING_WINDOW.getKeyValue(),
+      LegacyOpenDistroSettings.METRICS_ROLLING_WINDOW_SETTING,
+      (s) -> Setting.parseLong(
+          s, 2L, Key.METRICS_ROLLING_WINDOW.getKeyValue()),
+      Setting.Property.NodeScope,
+      Setting.Property.Dynamic);
 
   public static final Setting<?> METRICS_ROLLING_INTERVAL_SETTING = new Setting<>(
-    Key.METRICS_ROLLING_INTERVAL.getKeyValue(),
-    LegacyOpenDistroSettings.METRICS_ROLLING_INTERVAL_SETTING,
-    (s) -> Setting.parseLong(
-      s, 1L, Key.METRICS_ROLLING_INTERVAL.getKeyValue()),
-    Setting.Property.NodeScope,
-    Setting.Property.Dynamic);
+      Key.METRICS_ROLLING_INTERVAL.getKeyValue(),
+      LegacyOpenDistroSettings.METRICS_ROLLING_INTERVAL_SETTING,
+      (s) -> Setting.parseLong(
+          s, 1L, Key.METRICS_ROLLING_INTERVAL.getKeyValue()),
+      Setting.Property.NodeScope,
+      Setting.Property.Dynamic);
 
   /**
    * Construct ElasticsearchSetting.
@@ -130,44 +130,44 @@ public class OpenSearchSettings extends Settings {
    */
   @SuppressWarnings("unchecked")
   public OpenSearchSettings(ClusterSettings clusterSettings) {
-  ImmutableMap.Builder<Key, Setting<?>> settingBuilder = new ImmutableMap.Builder<>();
-  register(settingBuilder, clusterSettings, Key.SQL_ENABLED,
-    SQL_ENABLED_SETTING, new Updater(Key.SQL_ENABLED));
-  register(settingBuilder, clusterSettings, Key.SQL_SLOWLOG,
-    SQL_SLOWLOG_SETTING, new Updater(Key.SQL_SLOWLOG));
-  register(settingBuilder, clusterSettings, Key.SQL_CURSOR_KEEP_ALIVE,
-    SQL_CURSOR_KEEP_ALIVE_SETTING, new Updater(Key.SQL_CURSOR_KEEP_ALIVE));
-  register(settingBuilder, clusterSettings, Key.SQL_DELETE_ENABLED,
-    SQL_DELETE_ENABLED_SETTING, new Updater(Key.SQL_DELETE_ENABLED));
-  register(settingBuilder, clusterSettings, Key.PPL_ENABLED,
-    PPL_ENABLED_SETTING, new Updater(Key.PPL_ENABLED));
-  register(settingBuilder, clusterSettings, Key.QUERY_MEMORY_LIMIT,
-    QUERY_MEMORY_LIMIT_SETTING, new Updater(Key.QUERY_MEMORY_LIMIT));
-  register(settingBuilder, clusterSettings, Key.QUERY_SIZE_LIMIT,
-    QUERY_SIZE_LIMIT_SETTING, new Updater(Key.QUERY_SIZE_LIMIT));
-  register(settingBuilder, clusterSettings, Key.METRICS_ROLLING_WINDOW,
-    METRICS_ROLLING_WINDOW_SETTING, new Updater(Key.METRICS_ROLLING_WINDOW));
-  register(settingBuilder, clusterSettings, Key.METRICS_ROLLING_INTERVAL,
-    METRICS_ROLLING_INTERVAL_SETTING, new Updater(Key.METRICS_ROLLING_INTERVAL));
-  defaultSettings = settingBuilder.build();
+    ImmutableMap.Builder<Key, Setting<?>> settingBuilder = new ImmutableMap.Builder<>();
+    register(settingBuilder, clusterSettings, Key.SQL_ENABLED,
+        SQL_ENABLED_SETTING, new Updater(Key.SQL_ENABLED));
+    register(settingBuilder, clusterSettings, Key.SQL_SLOWLOG,
+        SQL_SLOWLOG_SETTING, new Updater(Key.SQL_SLOWLOG));
+    register(settingBuilder, clusterSettings, Key.SQL_CURSOR_KEEP_ALIVE,
+        SQL_CURSOR_KEEP_ALIVE_SETTING, new Updater(Key.SQL_CURSOR_KEEP_ALIVE));
+    register(settingBuilder, clusterSettings, Key.SQL_DELETE_ENABLED,
+        SQL_DELETE_ENABLED_SETTING, new Updater(Key.SQL_DELETE_ENABLED));
+    register(settingBuilder, clusterSettings, Key.PPL_ENABLED,
+        PPL_ENABLED_SETTING, new Updater(Key.PPL_ENABLED));
+    register(settingBuilder, clusterSettings, Key.QUERY_MEMORY_LIMIT,
+        QUERY_MEMORY_LIMIT_SETTING, new Updater(Key.QUERY_MEMORY_LIMIT));
+    register(settingBuilder, clusterSettings, Key.QUERY_SIZE_LIMIT,
+        QUERY_SIZE_LIMIT_SETTING, new Updater(Key.QUERY_SIZE_LIMIT));
+    register(settingBuilder, clusterSettings, Key.METRICS_ROLLING_WINDOW,
+        METRICS_ROLLING_WINDOW_SETTING, new Updater(Key.METRICS_ROLLING_WINDOW));
+    register(settingBuilder, clusterSettings, Key.METRICS_ROLLING_INTERVAL,
+        METRICS_ROLLING_INTERVAL_SETTING, new Updater(Key.METRICS_ROLLING_INTERVAL));
+    defaultSettings = settingBuilder.build();
   }
 
   @SuppressWarnings("unchecked")
   @Override
   public <T> T getSettingValue(Settings.Key key) {
-  return (T) latestSettings.getOrDefault(key, defaultSettings.get(key).getDefault(EMPTY));
+    return (T) latestSettings.getOrDefault(key, defaultSettings.get(key).getDefault(EMPTY));
   }
 
   /**
    * Register the pair of {key, setting}.
    */
   private void register(ImmutableMap.Builder<Key, Setting<?>> settingBuilder,
-            ClusterSettings clusterSettings, Settings.Key key,
-            Setting setting,
-            Consumer<Object> updater) {
-  settingBuilder.put(key, setting);
-  clusterSettings
-    .addSettingsUpdateConsumer(setting, updater);
+                        ClusterSettings clusterSettings, Settings.Key key,
+                        Setting setting,
+                        Consumer<Object> updater) {
+    settingBuilder.put(key, setting);
+    clusterSettings
+        .addSettingsUpdateConsumer(setting, updater);
   }
 
   /**
@@ -177,36 +177,36 @@ public class OpenSearchSettings extends Settings {
   @VisibleForTesting
   @RequiredArgsConstructor
   class Updater implements Consumer {
-  private final Settings.Key key;
+    private final Settings.Key key;
 
-  @Override
-  public void accept(Object newValue) {
-    log.debug("The value of setting [{}] changed to [{}]", key, newValue);
-    latestSettings.put(key, newValue);
-  }
+    @Override
+    public void accept(Object newValue) {
+      log.debug("The value of setting [{}] changed to [{}]", key, newValue);
+      latestSettings.put(key, newValue);
+    }
   }
 
   /**
    * Used by Plugin to init Setting.
    */
   public static List<Setting<?>> pluginSettings() {
-  return new ImmutableList.Builder<Setting<?>>()
-    .add(SQL_ENABLED_SETTING)
-    .add(SQL_SLOWLOG_SETTING)
-    .add(SQL_CURSOR_KEEP_ALIVE_SETTING)
-    .add(SQL_DELETE_ENABLED_SETTING)
-    .add(PPL_ENABLED_SETTING)
-    .add(QUERY_MEMORY_LIMIT_SETTING)
-    .add(QUERY_SIZE_LIMIT_SETTING)
-    .add(METRICS_ROLLING_WINDOW_SETTING)
-    .add(METRICS_ROLLING_INTERVAL_SETTING)
-    .build();
+    return new ImmutableList.Builder<Setting<?>>()
+        .add(SQL_ENABLED_SETTING)
+        .add(SQL_SLOWLOG_SETTING)
+        .add(SQL_CURSOR_KEEP_ALIVE_SETTING)
+        .add(SQL_DELETE_ENABLED_SETTING)
+        .add(PPL_ENABLED_SETTING)
+        .add(QUERY_MEMORY_LIMIT_SETTING)
+        .add(QUERY_SIZE_LIMIT_SETTING)
+        .add(METRICS_ROLLING_WINDOW_SETTING)
+        .add(METRICS_ROLLING_INTERVAL_SETTING)
+        .build();
   }
 
   /**
    * Used by local cluster to get settings from a setting instance
    */
   public List<Setting<?>> getSettings() {
-  return pluginSettings();
+    return pluginSettings();
   }
 }
