d5717a699714e293013510e1174c027f96d078dd
chloe-zh <chloezh1102@gmail.com>
2021-05-22 20:38:39 -0700

    fixed failed ITs
    Signed-off-by: chloe-zh <chloezh1102@gmail.com>

--- integ-test/src/test/java/org/opensearch/sql/legacy/PluginIT.java
+++ integ-test/src/test/java/org/opensearch/sql/legacy/PluginIT.java
@@ -273,7 +273,7 @@ public class PluginIT extends SQLIntegTestCase {
         "    \"plugins.query.metrics.rolling_window\": \"3700\"" +
         "  }," +
         "  \"persistent\": {" +
-        "    \"plugins.sql.query.slowlog\" : \"2\"" +
+        "    \"plugins.sql.slowlog\" : \"2\"" +
         "  }" +
         "}";
     JSONObject actual = updateViaSQLSettingsAPI(settings);
@@ -313,7 +313,7 @@ public class PluginIT extends SQLIntegTestCase {
         "    \"search.max_keep_alive\": \"24h\"" +
         "  }," +
         "  \"persistent\": {" +
-        "    \"plugins.query.query.slowlog\": \"2\"," +
+        "    \"plugins.query.slowlog\": \"2\"," +
         "    \"plugins.alerting.metrics.rolling_window\": \"3700\"," +
         "    \"thread_pool.analyze.queue_size\": \"16\"" +
         "  }" +
