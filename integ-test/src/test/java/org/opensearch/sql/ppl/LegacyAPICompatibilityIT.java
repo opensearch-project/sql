9802e17529d3116452dad65930eb3b0ec33cb52f
Chloe Zhang <chloezh1102@gmail.com>
2021-05-26 15:20:12 -0700

    added legacy setting prefix in setting action; added ITs
    Signed-off-by: chloe-zh <chloezh1102@gmail.com>

--- integ-test/src/test/java/org/opensearch/sql/ppl/LegacyAPICompatibilityIT.java
+++ integ-test/src/test/java/org/opensearch/sql/ppl/LegacyAPICompatibilityIT.java
@@ -13,11 +13,13 @@ package org.opensearch.sql.ppl;
 import static org.opensearch.sql.plugin.rest.RestPPLQueryAction.LEGACY_EXPLAIN_API_ENDPOINT;
 import static org.opensearch.sql.plugin.rest.RestPPLQueryAction.LEGACY_QUERY_API_ENDPOINT;
 import static org.opensearch.sql.plugin.rest.RestPPLStatsAction.PPL_LEGACY_STATS_API_ENDPOINT;
+import static org.opensearch.sql.plugin.rest.RestQuerySettingsAction.SETTINGS_API_ENDPOINT;
 
 import java.io.IOException;
 import org.junit.Assert;
 import org.junit.Test;
 import org.opensearch.client.Request;
+import org.opensearch.client.RequestOptions;
 import org.opensearch.client.Response;
 
 /**
@@ -51,6 +53,41 @@ public class LegacyAPICompatibilityIT extends PPLIntegTestCase {
     Request request = new Request("GET", PPL_LEGACY_STATS_API_ENDPOINT);
     Response response = client().performRequest(request);
     Assert.assertEquals(200, response.getStatusLine().getStatusCode());
+  }
+
+  @Test
+  public void legacySettingNewEndpoint() throws IOException {
+    String requestBody = "{"
+        + "  \"persistent\": {"
+        + "    \"opendistro.ppl.query.memory_limit\": \"80%\""
+        + "  }"
+        + "}";
+    Response response = updateSetting(SETTINGS_API_ENDPOINT, requestBody);
+    Assert.assertEquals(200, response.getStatusLine().getStatusCode());
+  }
+
+  @Test
+  public void newSettingNewEndpoint() throws IOException {
+    String requestBody = "{"
+        + "  \"persistent\": {"
+        + "    \"plugins.query.size_limit\": \"100\""
+        + "  }"
+        + "}";
+    Response response = updateSetting(SETTINGS_API_ENDPOINT, requestBody);
+    Assert.assertEquals(200, response.getStatusLine().getStatusCode());
+  }
+
+  private Response updateSetting(String endpoint, String requestBody) throws IOException {
+    Request request = new Request("PUT", endpoint);
+    request.setJsonEntity(requestBody);
+    request.setOptions(buildJsonOption());
+    return client().performRequest(request);
+  }
+
+  private RequestOptions.Builder buildJsonOption() {
+    RequestOptions.Builder restOptionsBuilder = RequestOptions.DEFAULT.toBuilder();
+    restOptionsBuilder.addHeader("Content-Type", "application/json");
+    return restOptionsBuilder;
   }
 
 }
