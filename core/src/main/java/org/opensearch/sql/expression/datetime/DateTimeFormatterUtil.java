5bd05d85429fec03d9a193f6b81dcfebc7c4aa77
vamsi-amazon <reddyvam@amazon.com>
2022-03-04 11:12:04 -0800

    Corrected regex to exclude few more edge cases
    Signed-off-by: vamsi-amazon <reddyvam@amazon.com>

--- core/src/main/java/org/opensearch/sql/expression/datetime/DateTimeFormatterUtil.java
+++ core/src/main/java/org/opensearch/sql/expression/datetime/DateTimeFormatterUtil.java
@@ -80,7 +80,7 @@ class DateTimeFormatterUtil {
 
   private static final Pattern pattern = Pattern.compile("%.");
   private static final Pattern CHARACTERS_WITH_NO_MOD_LITERAL_BEHIND_PATTERN
-          = Pattern.compile("(?<!%)[a-zA-Z&&[^ydmshiHIMYDS]]+");
+          = Pattern.compile("(?<!%)[a-zA-Z&&[^aydmshiHIMYDSEL]]+");
   private static final String MOD_LITERAL = "%";
 
   private DateTimeFormatterUtil() {
