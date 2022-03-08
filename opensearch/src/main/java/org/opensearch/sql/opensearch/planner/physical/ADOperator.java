30e1188e2cbbabb9a96949395ccc4d9370c1d2aa
jackieyanghan <jkhanjob@gmail.com>
2022-03-08 13:42:15 -0800

    Add license title for new ml-commons integration related classes
    Signed-off-by: jackieyanghan <jkhanjob@gmail.com>

--- opensearch/src/main/java/org/opensearch/sql/opensearch/planner/physical/ADOperator.java
+++ opensearch/src/main/java/org/opensearch/sql/opensearch/planner/physical/ADOperator.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+
 package org.opensearch.sql.opensearch.planner.physical;
 
 import static org.opensearch.sql.utils.MLCommonsConstants.SHINGLE_SIZE;
