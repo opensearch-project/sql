026ac4c71eef40eb2630be05c71e08e364cf002e
MaxKsyunz <maxk@bitquilltech.com>
2022-05-09 15:38:50 -0700

    100% test coverage for MatchPhraseQuery
    Signed-off-by: MaxKsyunz <maxk@bitquilltech.com>

--- opensearch/src/test/java/org/opensearch/sql/opensearch/storage/script/filter/lucene/MatchPhraseQueryTest.java
+++ opensearch/src/test/java/org/opensearch/sql/opensearch/storage/script/filter/lucene/MatchPhraseQueryTest.java
@@ -1,7 +1,6 @@
 package org.opensearch.sql.opensearch.storage.script.filter.lucene;
 
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
@@ -15,6 +14,7 @@ import org.opensearch.sql.exception.SemanticCheckException;
 import org.opensearch.sql.expression.DSL;
 import org.opensearch.sql.expression.Expression;
 import org.opensearch.sql.expression.FunctionExpression;
+import org.opensearch.sql.expression.NamedArgumentExpression;
 import org.opensearch.sql.expression.config.ExpressionConfig;
 import org.opensearch.sql.expression.env.Environment;
 import org.opensearch.sql.expression.function.FunctionName;
@@ -27,40 +27,71 @@ public class MatchPhraseQueryTest {
   private final MatchPhraseQuery matchPhraseQuery = new MatchPhraseQuery();
   private final FunctionName matchPhrase = FunctionName.of("match_phrase");
 
+  private NamedArgumentExpression namedArgument(String name, String value) {
+    return dsl.namedArgument(name, DSL.literal(value));
+  }
+
   @Test
   public void test_SemanticCheckException_when_no_arguments() {
 
     List<Expression> arguments = List.of();
-    FunctionExpression fe = new MatchPhraseExpression(arguments);
-    assertThrows(SemanticCheckException.class, () -> matchPhraseQuery.build(fe));
+    assertThrows(SemanticCheckException.class,
+        () -> matchPhraseQuery.build(new MatchPhraseExpression(arguments)));
   }
 
   @Test
   public void test_SemanticCheckException_when_one_argument() {
-    List<Expression> arguments = List.of(dsl.namedArgument("field",
-        DSL.literal("test")));
-    FunctionExpression fe = new MatchPhraseExpression(arguments);
-    assertThrows(SemanticCheckException.class, () -> matchPhraseQuery.build(fe));
+    List<Expression> arguments = List.of(namedArgument("field", "test"));
+    assertThrows(SemanticCheckException.class,
+        () -> matchPhraseQuery.build(new MatchPhraseExpression(arguments)));
   }
 
   @Test
   public void test_SemanticCheckException_when_invalid_parameter() {
     List<Expression> arguments = List.of(
-        dsl.namedArgument("field", DSL.literal("test")),
-        dsl.namedArgument("query", DSL.literal("test2")),
-        dsl.namedArgument("unsupported", DSL.literal(3)));
-    FunctionExpression fe = new MatchPhraseExpression(arguments);
-    Assertions.assertThrows(SemanticCheckException.class, () -> matchPhraseQuery.build(fe));
+        namedArgument("field", "test"),
+        namedArgument("query", "test2"),
+        namedArgument("unsupported", "3"));
+    Assertions.assertThrows(SemanticCheckException.class,
+        () -> matchPhraseQuery.build(new MatchPhraseExpression(arguments)));
+  }
+
+  @Test
+  public void test_analyzer_parameter() {
+    List<Expression> arguments = List.of(
+        namedArgument("field", "t1"),
+        namedArgument("query", "t2"),
+        namedArgument("analyzer", "standard")
+    );
+    Assertions.assertNotNull(matchPhraseQuery.build(new MatchPhraseExpression(arguments)));
   }
 
   @Test
   public void build_succeeds_with_two_arguments() {
-
     List<Expression> arguments = List.of(
-        dsl.namedArgument("field", DSL.literal("test")),
-        dsl.namedArgument("query", DSL.literal("test2")));
-    FunctionExpression fe = new MatchPhraseExpression(arguments);
-    Assertions.assertNotNull(matchPhraseQuery.build(fe));
+        namedArgument("field", "test"),
+        namedArgument("query", "test2"));
+    Assertions.assertNotNull(matchPhraseQuery.build(new MatchPhraseExpression(arguments)));
+  }
+
+  @Test
+  public void test_slop_parameter() {
+    List<Expression> arguments = List.of(
+        namedArgument("field", "t1"),
+        namedArgument("query", "t2"),
+        namedArgument("slop", "2")
+    );
+    Assertions.assertNotNull(matchPhraseQuery.build(new MatchPhraseExpression(arguments)));
+  }
+
+  @Test
+  public void test_zero_terms_query_parameter() {
+    List<Expression> arguments = List.of(
+        namedArgument("field", "t1"),
+        namedArgument("query", "t2"),
+        namedArgument("zero_terms_query", "ALL")
+    );
+    Assertions.assertNotNull(matchPhraseQuery.build(new MatchPhraseExpression(arguments)));
   }
 
   private class MatchPhraseExpression extends FunctionExpression {
