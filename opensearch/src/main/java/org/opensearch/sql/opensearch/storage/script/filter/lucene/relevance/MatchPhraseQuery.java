d3667cebc145f2a96e610df695198964a8ec5c19
MaxKsyunz <maxk@bitquilltech.com>
2022-05-20 00:51:45 -0700

    Add RelevanceQuery -- a base class for MatchQuery and MatchPhraseQuery.
    Signed-off-by: MaxKsyunz <maxk@bitquilltech.com>

--- opensearch/src/main/java/org/opensearch/sql/opensearch/storage/script/filter/lucene/relevance/MatchPhraseQuery.java
+++ opensearch/src/main/java/org/opensearch/sql/opensearch/storage/script/filter/lucene/relevance/MatchPhraseQuery.java
@@ -23,51 +23,22 @@ import org.opensearch.sql.opensearch.storage.script.filter.lucene.LuceneQuery;
 /**
  * Lucene query that builds a match_phrase query.
  */
-public class MatchPhraseQuery extends LuceneQuery {
-  private final FluentAction analyzer =
-      (b, v) -> b.analyzer(v.stringValue());
-  private final FluentAction slop =
-      (b, v) -> b.slop(Integer.parseInt(v.stringValue()));
-  private final FluentAction
-        zeroTermsQuery = (b, v) -> b.zeroTermsQuery(
-               org.opensearch.index.search.MatchQuery.ZeroTermsQuery.valueOf(v.stringValue()));
-
-  interface FluentAction
-      extends BiFunction<MatchPhraseQueryBuilder, ExprValue, MatchPhraseQueryBuilder>  {
+public class MatchPhraseQuery extends RelevanceQuery<MatchPhraseQueryBuilder> {
+  /**
+   *  Default constructor for MatchPhraseQuery configures how RelevanceQuery.build() handles
+   * named arguments.
+   */
+  public MatchPhraseQuery() {
+    super(ImmutableMap.<String, QueryBuilderStep<MatchPhraseQueryBuilder>>builder()
+        .put("analyzer", (b, v) -> b.analyzer(v.stringValue()))
+        .put("slop", (b, v) -> b.slop(Integer.parseInt(v.stringValue())))
+        .put("zero_terms_query", (b, v) -> b.zeroTermsQuery(
+            org.opensearch.index.search.MatchQuery.ZeroTermsQuery.valueOf(v.stringValue())))
+        .build());
   }
 
-  ImmutableMap<Object, FluentAction>
-      argAction =
-      ImmutableMap.<Object, FluentAction>builder()
-          .put("analyzer", analyzer)
-          .put("slop", slop)
-          .put("zero_terms_query", zeroTermsQuery)
-          .build();
-
   @Override
-  public QueryBuilder build(FunctionExpression func) {
-    List<Expression> arguments = func.getArguments();
-    if (arguments.size() < 2) {
-      throw new SemanticCheckException("match_phrase requires at least two parameters");
-    }
-    NamedArgumentExpression field = (NamedArgumentExpression) arguments.get(0);
-    NamedArgumentExpression query = (NamedArgumentExpression) arguments.get(1);
-    MatchPhraseQueryBuilder queryBuilder = QueryBuilders.matchPhraseQuery(
-        field.getValue().valueOf(null).stringValue(),
-        query.getValue().valueOf(null).stringValue());
-
-    Iterator<Expression> iterator = arguments.listIterator(2);
-    while (iterator.hasNext()) {
-      NamedArgumentExpression arg = (NamedArgumentExpression) iterator.next();
-      if (!argAction.containsKey(arg.getArgName())) {
-        throw new SemanticCheckException(String
-            .format("Parameter %s is invalid for match_phrase function.", arg.getArgName()));
-      }
-      (Objects.requireNonNull(
-          argAction
-              .get(arg.getArgName())))
-          .apply(queryBuilder, arg.getValue().valueOf(null));
-    }
-    return queryBuilder;
+  protected MatchPhraseQueryBuilder createQueryBuilder(String field, String query) {
+    return QueryBuilders.matchPhraseQuery(field, query);
   }
 }
---
+++ opensearch/src/main/java/org/opensearch/sql/opensearch/storage/script/filter/lucene/relevance/RelevanceQuery.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.sql.opensearch.storage.script.filter.lucene.relevance;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.BiFunction;
+import org.opensearch.index.query.MatchPhraseQueryBuilder;
+import org.opensearch.index.query.QueryBuilder;
+import org.opensearch.index.query.QueryBuilders;
+import org.opensearch.sql.data.model.ExprValue;
+import org.opensearch.sql.exception.SemanticCheckException;
+import org.opensearch.sql.expression.Expression;
+import org.opensearch.sql.expression.FunctionExpression;
+import org.opensearch.sql.expression.NamedArgumentExpression;
+import org.opensearch.sql.opensearch.storage.script.filter.lucene.LuceneQuery;
+
+/**
+ * Base class for query abstraction that builds a relevance query from function expression.
+ */
+public abstract class RelevanceQuery<T extends QueryBuilder> extends LuceneQuery {
+  protected Map<String, QueryBuilderStep<T>> queryBuildActions;
+
+  protected RelevanceQuery(Map<String, QueryBuilderStep<T>> actionMap) {
+    queryBuildActions = actionMap;
+  }
+
+  @Override
+  public QueryBuilder build(FunctionExpression func) {
+    List<Expression> arguments = func.getArguments();
+    if (arguments.size() < 2) {
+      String queryName = createQueryBuilder("", "").getWriteableName();
+      throw new SemanticCheckException(
+          String.format("%s requires at least two parameters", queryName));
+    }
+    NamedArgumentExpression field = (NamedArgumentExpression) arguments.get(0);
+    NamedArgumentExpression query = (NamedArgumentExpression) arguments.get(1);
+    T queryBuilder = createQueryBuilder(
+        field.getValue().valueOf(null).stringValue(),
+        query.getValue().valueOf(null).stringValue());
+
+    Iterator<Expression> iterator = arguments.listIterator(2);
+    while (iterator.hasNext()) {
+      NamedArgumentExpression arg = (NamedArgumentExpression) iterator.next();
+      if (!queryBuildActions.containsKey(arg.getArgName())) {
+        throw new SemanticCheckException(String
+            .format("Parameter %s is invalid for %s function.", arg.getArgName(), queryBuilder.getWriteableName()));
+      }
+      (Objects.requireNonNull(
+          queryBuildActions
+              .get(arg.getArgName())))
+          .apply(queryBuilder, arg.getValue().valueOf(null));
+    }
+    return queryBuilder;
+  }
+
+  protected abstract T createQueryBuilder(String field, String query);
+
+  /**
+   * Convenience interface for a function that updates a QueryBuilder
+   * based on ExprValue.
+   * @param <T> Concrete query builder
+   */
+  public interface QueryBuilderStep<T extends QueryBuilder> extends
+      BiFunction<T, ExprValue, T> {
+
+  }
+}
