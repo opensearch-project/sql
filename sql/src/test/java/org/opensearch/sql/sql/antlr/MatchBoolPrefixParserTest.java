2f664ac63e22926fea532651dbdec172a575a939
forestmvey <forestv@bitquilltech.com>
2022-05-26 16:32:11 -0700

    Adding parameter options parser tests
    Signed-off-by: forestmvey <forestv@bitquilltech.com>

--- sql/src/test/java/org/opensearch/sql/sql/antlr/MatchBoolPrefixParserTest.java
+++ sql/src/test/java/org/opensearch/sql/sql/antlr/MatchBoolPrefixParserTest.java
@@ -1,13 +1,33 @@
 package org.opensearch.sql.sql.antlr;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class MatchBoolPrefixParserTest extends SQLParserTest {
 
   @Test
   public void testDefaultParameters() {
     acceptQuery("SELECT * FROM T WHERE MATCH_BOOL_PREFIX(message, 'query')");
+  }
+
+  static Stream<String> generateAvailableParameters() {
+    return Stream.of(
+        new String("minimum_should_match=3"),
+        new String("fuzziness=AUTO"),
+        new String("prefix_length=0"),
+        new String("max_expansions=50"),
+        new String("fuzzy_transpositions=true"),
+        new String("fuzzy_rewrite=constant_score")
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("generateAvailableParameters")
+  public void testAvailableParameters(String arg) {
+    acceptQuery("SELECT * FROM T WHERE MATCH_BOOL_PREFIX(message, 'query', " + arg  + ")");
   }
 
   @Test
