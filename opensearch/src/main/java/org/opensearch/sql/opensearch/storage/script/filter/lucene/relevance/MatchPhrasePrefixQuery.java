bb946c25ca03c95ee8116c1af7b90d0438d15ac9
MaxKsyunz <maxk@bitquilltech.com>
2022-06-04 00:32:35 -0700

    Add a license header.
    Signed-off-by: MaxKsyunz <maxk@bitquilltech.com>

--- opensearch/src/main/java/org/opensearch/sql/opensearch/storage/script/filter/lucene/relevance/MatchPhrasePrefixQuery.java
+++ opensearch/src/main/java/org/opensearch/sql/opensearch/storage/script/filter/lucene/relevance/MatchPhrasePrefixQuery.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.opensearch.sql.opensearch.storage.script.filter.lucene.relevance;
 
 import com.google.common.collect.ImmutableMap;
