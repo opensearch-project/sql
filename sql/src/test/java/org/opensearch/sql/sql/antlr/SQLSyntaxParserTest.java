9d7738150547849352834cde73a4799e9f52a1fc
MaxKsyunz <maxk@bitquilltech.com>
2022-06-03 22:37:47 -0700

    SQL parser test for match_phrase_prefix with required arguments.
    Change generateQueries signature to use more appropriate Map type.
    
    Signed-off-by: MaxKsyunz <maxk@bitquilltech.com>

--- sql/src/test/java/org/opensearch/sql/sql/antlr/SQLSyntaxParserTest.java
+++ sql/src/test/java/org/opensearch/sql/sql/antlr/SQLSyntaxParserTest.java
@@ -9,11 +9,13 @@ package org.opensearch.sql.sql.antlr;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Streams;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Random;
 import java.util.stream.Stream;
 import org.apache.commons.lang3.RandomStringUtils;
@@ -173,9 +175,20 @@ class SQLSyntaxParserTest {
   }
 
   @ParameterizedTest
-  @MethodSource({"matchPhraseComplexQueries",
-          "matchPhraseGeneratedQueries", "generateMatchPhraseQueries"})
+  @MethodSource({
+      "matchPhraseComplexQueries",
+      "matchPhraseGeneratedQueries",
+      "generateMatchPhraseQueries",
+  })
   public void canParseComplexMatchPhraseArgsTest(String query) {
+    assertNotNull(parser.parse(query));
+  }
+
+  @ParameterizedTest
+  @MethodSource({
+      "generateMatchPhrasePrefixQueries"
+  })
+  public void canParseComplexMatchPhrasePrefixQueries(String query) {
     assertNotNull(parser.parse(query));
   }
 
@@ -223,8 +236,13 @@ class SQLSyntaxParserTest {
     return generateQueries("match_phrase", matchPhraseArgs);
   }
 
+  private static Stream<String> generateMatchPhrasePrefixQueries() {
+    return generateQueries("match_phrase_prefix", ImmutableMap.<String, Object[]>builder()
+        .build());
+  }
+
   private static Stream<String> generateQueries(String function,
-                                                HashMap<String, Object[]> functionArgs) {
+                                                Map<String, Object[]> functionArgs) {
     var rand = new Random(0);
 
     class QueryGenerator implements Iterator<String> {
