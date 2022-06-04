521beca3869e9aa84f2e9111f1f8721af7625a9f
Yury Fridlyand <yuryf@bitquilltech.com>
2022-06-03 19:39:46 -0700

    Rework `SimpleQueryStringQuery` to use the `RelevanceFunction` interface.
    Signed-off-by: Yury Fridlyand <yuryf@bitquilltech.com>

--- opensearch/src/main/java/org/opensearch/sql/opensearch/storage/script/filter/lucene/relevance/SimpleQueryStringQuery.java
+++ opensearch/src/main/java/org/opensearch/sql/opensearch/storage/script/filter/lucene/relevance/SimpleQueryStringQuery.java
@@ -6,64 +6,44 @@
 package org.opensearch.sql.opensearch.storage.script.filter.lucene.relevance;
 
 import com.google.common.collect.ImmutableMap;
-import io.vavr.Tuple;
 import java.util.Iterator;
-import java.util.Map;
-import java.util.function.BiFunction;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import org.opensearch.index.query.Operator;
 import org.opensearch.index.query.QueryBuilder;
 import org.opensearch.index.query.QueryBuilders;
 import org.opensearch.index.query.SimpleQueryStringBuilder;
 import org.opensearch.index.query.SimpleQueryStringFlag;
-import org.opensearch.sql.data.model.ExprValue;
 import org.opensearch.sql.exception.SemanticCheckException;
 import org.opensearch.sql.expression.Expression;
 import org.opensearch.sql.expression.FunctionExpression;
 import org.opensearch.sql.expression.NamedArgumentExpression;
-import org.opensearch.sql.opensearch.storage.script.filter.lucene.LuceneQuery;
 
-public class SimpleQueryStringQuery extends LuceneQuery {
-  private final BiFunction<SimpleQueryStringBuilder, ExprValue, SimpleQueryStringBuilder>
-      analyzeWildcard = (b, v) -> b.analyzeWildcard(Boolean.parseBoolean(v.stringValue()));
-  private final BiFunction<SimpleQueryStringBuilder, ExprValue, SimpleQueryStringBuilder>
-      analyzer = (b, v) -> b.analyzer(v.stringValue());
-  private final BiFunction<SimpleQueryStringBuilder, ExprValue, SimpleQueryStringBuilder>
-      autoGenerateSynonymsPhraseQuery = (b, v) ->
-          b.autoGenerateSynonymsPhraseQuery(Boolean.parseBoolean(v.stringValue()));
-  private final BiFunction<SimpleQueryStringBuilder, ExprValue, SimpleQueryStringBuilder>
-      defaultOperator = (b, v) -> b.defaultOperator(Operator.fromString(v.stringValue()));
-  private final BiFunction<SimpleQueryStringBuilder, ExprValue, SimpleQueryStringBuilder>
-      flags = (b, v) -> b.flags(SimpleQueryStringFlag.valueOf(v.stringValue()));
-  private final BiFunction<SimpleQueryStringBuilder, ExprValue, SimpleQueryStringBuilder>
-      fuzzyMaxExpansions = (b, v) -> b.fuzzyMaxExpansions(Integer.parseInt(v.stringValue()));
-  private final BiFunction<SimpleQueryStringBuilder, ExprValue, SimpleQueryStringBuilder>
-      fuzzyPrefixLength = (b, v) -> b.fuzzyPrefixLength(Integer.parseInt(v.stringValue()));
-  private final BiFunction<SimpleQueryStringBuilder, ExprValue, SimpleQueryStringBuilder>
-      fuzzyTranspositions = (b, v) -> b.fuzzyTranspositions(Boolean.parseBoolean(v.stringValue()));
-  private final BiFunction<SimpleQueryStringBuilder, ExprValue, SimpleQueryStringBuilder>
-      lenient = (b, v) -> b.lenient(Boolean.parseBoolean(v.stringValue()));
-  private final BiFunction<SimpleQueryStringBuilder, ExprValue, SimpleQueryStringBuilder>
-      minimumShouldMatch = (b, v) -> b.minimumShouldMatch(v.stringValue());
-  private final BiFunction<SimpleQueryStringBuilder, ExprValue, SimpleQueryStringBuilder>
-      quoteFieldSuffix = (b, v) -> b.quoteFieldSuffix(v.stringValue());
-  private final BiFunction<SimpleQueryStringBuilder, ExprValue, SimpleQueryStringBuilder>
-      boost = (b, v) -> b.boost(Float.parseFloat(v.stringValue()));
-
-  ImmutableMap<Object, Object> argAction = ImmutableMap.builder()
-      .put("analyze_wildcard", analyzeWildcard)
-      .put("analyzer", analyzer)
-      .put("auto_generate_synonyms_phrase_query", autoGenerateSynonymsPhraseQuery)
-      .put("flags", flags)
-      .put("fuzzy_max_expansions", fuzzyMaxExpansions)
-      .put("fuzzy_prefix_length", fuzzyPrefixLength)
-      .put("fuzzy_transpositions", fuzzyTranspositions)
-      .put("lenient", lenient)
-      .put("default_operator", defaultOperator)
-      .put("minimum_should_match", minimumShouldMatch)
-      .put("quote_field_suffix", quoteFieldSuffix)
-      .put("boost", boost)
-      .build();
+public class SimpleQueryStringQuery extends RelevanceQuery<SimpleQueryStringBuilder> {
+  /**
+   *  Default constructor for SimpleQueryString configures how RelevanceQuery.build() handles
+   * named arguments.
+   */
+  public SimpleQueryStringQuery() {
+    super(ImmutableMap.<String, QueryBuilderStep<SimpleQueryStringBuilder>>builder()
+        .put("analyze_wildcard", (b, v) -> b.analyzeWildcard(Boolean.parseBoolean(v.stringValue())))
+        .put("analyzer", (b, v) -> b.analyzer(v.stringValue()))
+        .put("auto_generate_synonyms_phrase_query", (b, v) ->
+            b.autoGenerateSynonymsPhraseQuery(Boolean.parseBoolean(v.stringValue())))
+        .put("boost", (b, v) -> b.boost(Float.parseFloat(v.stringValue())))
+        .put("default_operator", (b, v) -> b.defaultOperator(Operator.fromString(v.stringValue())))
+        .put("flags", (b, v) -> b.flags(SimpleQueryStringFlag.valueOf(v.stringValue())))
+        .put("fuzzy_max_expansions", (b, v) ->
+            b.fuzzyMaxExpansions(Integer.parseInt(v.stringValue())))
+        .put("fuzzy_prefix_length", (b, v) ->
+            b.fuzzyPrefixLength(Integer.parseInt(v.stringValue())))
+        .put("fuzzy_transpositions", (b, v) ->
+            b.fuzzyTranspositions(Boolean.parseBoolean(v.stringValue())))
+        .put("lenient", (b, v) -> b.lenient(Boolean.parseBoolean(v.stringValue())))
+        .put("minimum_should_match", (b, v) -> b.minimumShouldMatch(v.stringValue()))
+        .put("quote_field_suffix", (b, v) -> b.quoteFieldSuffix(v.stringValue()))
+        .build());
+  }
 
   @Override
   public QueryBuilder build(FunctionExpression func) {
@@ -82,19 +62,26 @@ public class SimpleQueryStringQuery extends LuceneQuery {
         .stream()
         .collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue().floatValue()));
 
-    SimpleQueryStringBuilder queryBuilder = QueryBuilders
-        .simpleQueryStringQuery(query.getValue().valueOf(null).stringValue())
+    SimpleQueryStringBuilder queryBuilder = createQueryBuilder(null,
+            query.getValue().valueOf(null).stringValue())
         .fields(fieldsAndWeights);
     while (iterator.hasNext()) {
       NamedArgumentExpression arg = (NamedArgumentExpression) iterator.next();
-      if (!argAction.containsKey(arg.getArgName())) {
-        throw new SemanticCheckException(String
-            .format("Parameter %s is invalid for simple_query_string function.", arg.getArgName()));
+      if (!queryBuildActions.containsKey(arg.getArgName())) {
+        throw new SemanticCheckException(
+            String.format("Parameter %s is invalid for %s function.",
+                arg.getArgName(), queryBuilder.getWriteableName()));
       }
-      ((BiFunction<SimpleQueryStringBuilder, ExprValue, SimpleQueryStringBuilder>) argAction
-          .get(arg.getArgName()))
+      (Objects.requireNonNull(
+          queryBuildActions
+              .get(arg.getArgName())))
           .apply(queryBuilder, arg.getValue().valueOf(null));
     }
     return queryBuilder;
+  }
+
+  @Override
+  protected SimpleQueryStringBuilder createQueryBuilder(String field, String query) {
+    return QueryBuilders.simpleQueryStringQuery(query);
   }
 }
