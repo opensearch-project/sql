3f10b8b50acc58a80bbf3d0475837dd9bc19950c
MaxKsyunz <maxk@bitquilltech.com>
2022-07-05 11:00:59 -0700

    Represent highlight as UnresolvedExpression.
    Signed-off-by: MaxKsyunz <maxk@bitquilltech.com>

--- core/src/main/java/org/opensearch/sql/ast/expression/HighlightFunction.java
+++ core/src/main/java/org/opensearch/sql/ast/expression/HighlightFunction.java
@@ -1,7 +1,24 @@
 package org.opensearch.sql.ast.expression;
 
-public class HighlightFunction extends UnresolvedExpression {
-  public HighlightFunction(String field) {
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import org.opensearch.sql.ast.AbstractNodeVisitor;
 
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@Getter
+@RequiredArgsConstructor
+@ToString
+public class HighlightFunction extends UnresolvedExpression {
+  @Getter
+  private final String highlightField;
+
+
+  @Override
+  public <T, C> T accept(AbstractNodeVisitor<T, C> nodeVisitor, C context) {
+    return nodeVisitor.visitHighlight(this, context);
   }
 }
