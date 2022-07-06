5c8db0ac3c93e07b43a6d8f08db9e4c9f7ad7ab4
MaxKsyunz <maxk@bitquilltech.com>
2022-07-06 00:49:13 -0700

    Add a simple IT test for highlight.
    Signed-off-by: MaxKsyunz <maxk@bitquilltech.com>

---
+++ integ-test/src/test/java/org/opensearch/sql/sql/HighlightFunctionIT.java
@@ -0,0 +1,22 @@
+package org.opensearch.sql.sql;
+
+import org.json.JSONObject;
+import org.junit.Test;
+import org.opensearch.sql.legacy.SQLIntegTestCase;
+import org.opensearch.sql.legacy.TestsConstants;
+
+public class HighlightFunctionIT extends SQLIntegTestCase {
+
+  @Override
+  protected void init() throws Exception {
+    loadIndex(Index.BEER);
+  }
+
+  @Test
+  public void defaultParameters() {
+    String query = "SELECT Tags, highlight('Tags') from %s WHERE match(Tags, 'yeast') limit 1";
+//    String query = "SELECT Tags from %s WHERE match(Tags, 'yeast') limit 1";
+    JSONObject response = executeJdbcRequest(String.format(query, TestsConstants.TEST_INDEX_BEER));
+    assertEquals(1, response.getInt("total"));
+  }
+}
