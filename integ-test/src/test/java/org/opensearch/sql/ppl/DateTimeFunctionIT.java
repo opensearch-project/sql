c480211e613a1198c0ce2c412194fa88d2e2d9a1
MitchellGale-BitQuill <mitchellg@bitquilltech.com>
2022-09-01 00:08:16 -0700

    Removed changes.
    Signed-off-by: MitchellGale-BitQuill <mitchellg@bitquilltech.com>

--- integ-test/src/test/java/org/opensearch/sql/ppl/DateTimeFunctionIT.java
+++ integ-test/src/test/java/org/opensearch/sql/ppl/DateTimeFunctionIT.java
@@ -9,7 +9,6 @@ package org.opensearch.sql.ppl;
 import static org.opensearch.sql.legacy.TestsConstants.TEST_INDEX_DATE;
 import static org.opensearch.sql.util.MatcherUtils.rows;
 import static org.opensearch.sql.util.MatcherUtils.schema;
-import static org.opensearch.sql.util.MatcherUtils.verifyDataRows;
 import static org.opensearch.sql.util.MatcherUtils.verifySchema;
 import static org.opensearch.sql.util.MatcherUtils.verifySome;
 
