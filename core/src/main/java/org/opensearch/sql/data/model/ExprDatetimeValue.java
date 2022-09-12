5cf21a7d72a3d873ab3efaf3b0b32c8bea76b570
MitchellGale-BitQuill <mitchellg@bitquilltech.com>
2022-09-12 11:41:10 -0700

    Removed extra import.
    Signed-off-by: MitchellGale-BitQuill <mitchellg@bitquilltech.com>

--- core/src/main/java/org/opensearch/sql/data/model/ExprDatetimeValue.java
+++ core/src/main/java/org/opensearch/sql/data/model/ExprDatetimeValue.java
@@ -16,7 +16,6 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.format.DateTimeParseException;
-import java.time.format.ResolverStyle;
 import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
 import lombok.RequiredArgsConstructor;
