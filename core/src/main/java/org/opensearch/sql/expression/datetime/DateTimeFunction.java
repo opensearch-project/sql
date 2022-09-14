9c0e7193fa71efe92251f5d24cc9ffa162cedebb
MitchellGale-BitQuill <mitchellg@bitquilltech.com>
2022-09-14 09:24:16 -0700

    Deleted fromTZ
    Signed-off-by: MitchellGale-BitQuill <mitchellg@bitquilltech.com>

--- core/src/main/java/org/opensearch/sql/expression/datetime/DateTimeFunction.java
+++ core/src/main/java/org/opensearch/sql/expression/datetime/DateTimeFunction.java
@@ -571,7 +571,6 @@ public class DateTimeFunction {
     ExprValue convertTZResult;
     ExprDatetimeValue ldt;
     String toTz;
-    String fromTz;
 
     try {
       ZonedDateTime zdtWithZoneOffset = ZonedDateTime.parse(dateTime.stringValue(), formatDT);
