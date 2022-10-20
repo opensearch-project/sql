95c5e5cb625f571ce2a76bac131a4b09f346f4b2
MitchellGale-BitQuill <mitchellg@bitquilltech.com>
2022-10-19 17:07:30 -0700

    Removed unused DATETIMEFORMATTERS.
    Signed-off-by: MitchellGale-BitQuill <mitchellg@bitquilltech.com>

--- core/src/main/java/org/opensearch/sql/utils/DateTimeFormatters.java
+++ core/src/main/java/org/opensearch/sql/utils/DateTimeFormatters.java
@@ -133,38 +133,6 @@ public class DateTimeFormatters {
           .toFormatter(Locale.ROOT)
           .withResolverStyle(ResolverStyle.STRICT);
 
-  public static final DateTimeFormatter DATE_FORMATTER_VARIABLE_NANOS_OPTIONAL =
-      new DateTimeFormatterBuilder()
-          .appendPattern("[uuuu-MM-dd][HH:mm:ss][uuuu-MM-dd HH:mm:ss]")
-          .appendFraction(
-              ChronoField.NANO_OF_SECOND,
-              MIN_FRACTION_SECONDS,
-              MAX_FRACTION_SECONDS,
-              true)
-          .toFormatter(Locale.ROOT)
-          .withResolverStyle(ResolverStyle.STRICT);
-
-  public static final DateTimeFormatter TIME_FORMATTER_VARIABLE_NANOS_OPTIONAL =
-      new DateTimeFormatterBuilder()
-          .appendPattern("[uuuu-MM-dd HH:mm:ss][HH:mm:ss]")
-          .appendFraction(
-              ChronoField.NANO_OF_SECOND,
-              MIN_FRACTION_SECONDS,
-              MAX_FRACTION_SECONDS,
-              true)
-          .toFormatter(Locale.ROOT)
-          .withResolverStyle(ResolverStyle.STRICT);
-
-  public static final DateTimeFormatter TIME_FORMATTER_VARIABLE_NANOS =
-      new DateTimeFormatterBuilder()
-          .appendPattern("HH:mm:ss")
-          .appendFraction(
-              ChronoField.NANO_OF_SECOND,
-              MIN_FRACTION_SECONDS,
-              MAX_FRACTION_SECONDS,
-              true)
-          .toFormatter();
-
   // YYMMDD
   public static final DateTimeFormatter DATE_FORMATTER_SHORT_YEAR =
       new DateTimeFormatterBuilder()
