73787b700f73bd686a96600a9ab688680485dc6a
Chen Dai <daichen@amazon.com>
2022-11-01 16:01:32 -0700

    Add stream context and window trigger (#958)
    * Add stream context and window trigger
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    * Update Javadoc with more details
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    Signed-off-by: Chen Dai <daichen@amazon.com>

---
+++ core/src/main/java/org/opensearch/sql/planner/streaming/StreamContext.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.sql.planner.streaming;
+
+import lombok.Data;
+
+/**
+ * Stream context required by stream processing components and can be
+ * stored and restored between executions.
+ */
+@Data
+public class StreamContext {
+
+  /** Current watermark timestamp. */
+  private long watermark;
+}
