e8f3205dafe737a5e3a172bec4a090080b2e58d2
Chen Dai <daichen@amazon.com>
2022-11-01 16:00:04 -0700

    Add watermark generator (#959)
    * Add watermark generator
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    * Update Javadoc and UT for readability
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    Signed-off-by: Chen Dai <daichen@amazon.com>

---
+++ core/src/main/java/org/opensearch/sql/planner/streaming/watermark/WatermarkGenerator.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.sql.planner.streaming.watermark;
+
+/**
+ * A watermark generator generates watermark timestamp based on some strategy which is defined
+ * in implementation class.
+ */
+public interface WatermarkGenerator {
+
+  /**
+   * Generate watermark timestamp on the given event timestamp.
+   *
+   * @param timestamp event timestamp in millisecond
+   * @return watermark timestamp in millisecond
+   */
+  long generate(long timestamp);
+
+}
