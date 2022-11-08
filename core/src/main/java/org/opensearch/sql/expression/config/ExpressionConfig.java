d8543e3ea6f08ca0e1bec1f6a6a1a2cdfb2223e3
Chen Dai <daichen@amazon.com>
2022-11-08 12:57:33 -0800

    Clean up unused imports
    Signed-off-by: Chen Dai <daichen@amazon.com>

--- core/src/main/java/org/opensearch/sql/expression/config/ExpressionConfig.java
+++ core/src/main/java/org/opensearch/sql/expression/config/ExpressionConfig.java
@@ -6,19 +6,7 @@
 
 package org.opensearch.sql.expression.config;
 
-import org.opensearch.sql.expression.aggregation.AggregatorFunction;
-import org.opensearch.sql.expression.datetime.DateTimeFunction;
-import org.opensearch.sql.expression.datetime.IntervalClause;
 import org.opensearch.sql.expression.function.BuiltinFunctionRepository;
-import org.opensearch.sql.expression.function.OpenSearchFunctions;
-import org.opensearch.sql.expression.operator.arthmetic.ArithmeticFunction;
-import org.opensearch.sql.expression.operator.arthmetic.MathematicalFunction;
-import org.opensearch.sql.expression.operator.convert.TypeCastOperator;
-import org.opensearch.sql.expression.operator.predicate.BinaryPredicateOperator;
-import org.opensearch.sql.expression.operator.predicate.UnaryPredicateOperator;
-import org.opensearch.sql.expression.system.SystemFunctions;
-import org.opensearch.sql.expression.text.TextFunction;
-import org.opensearch.sql.expression.window.WindowFunctions;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
