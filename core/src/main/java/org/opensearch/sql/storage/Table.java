64a3794cb015430bc30199885f1d91c5b34d5654
Chen Dai <daichen@amazon.com>
2022-12-07 11:29:11 -0800

    Improve pushdown optimization and logical to physical transformation (#1091)
    * Add new table scan builder and optimizer rules
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    * Fix jacoco test coverage
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    * Update javadoc with more details
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    * Fix highlight pushdown issue
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    * Rename new class more properly
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    * Fix default sort by doc issue
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    * Rename visit method and javadoc
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    * Move table scan builder and optimize rule to read package
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    * Fix sort push down issue
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    * Move sortByFields to parent scan builder
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    * Add back old test
    
    Signed-off-by: Chen Dai <daichen@amazon.com>
    
    Signed-off-by: Chen Dai <daichen@amazon.com>

--- core/src/main/java/org/opensearch/sql/storage/Table.java
+++ core/src/main/java/org/opensearch/sql/storage/Table.java
@@ -11,6 +11,7 @@ import org.opensearch.sql.data.type.ExprType;
 import org.opensearch.sql.executor.streaming.StreamingSource;
 import org.opensearch.sql.planner.logical.LogicalPlan;
 import org.opensearch.sql.planner.physical.PhysicalPlan;
+import org.opensearch.sql.storage.read.TableScanBuilder;
 
 /**
  * Table.
@@ -45,7 +46,9 @@ public interface Table {
    *
    * @param plan logical plan
    * @return physical plan
+   * @deprecated because of new {@link TableScanBuilder} implementation
    */
+  @Deprecated(since = "2.5.0")
   PhysicalPlan implement(LogicalPlan plan);
 
   /**
@@ -54,9 +57,20 @@ public interface Table {
    *
    * @param plan logical plan.
    * @return logical plan.
+   * @deprecated because of new {@link TableScanBuilder} implementation
    */
+  @Deprecated(since = "2.5.0")
   default LogicalPlan optimize(LogicalPlan plan) {
     return plan;
+  }
+
+  /**
+   * Create table scan builder for logical to physical transformation.
+   *
+   * @return table scan builder
+   */
+  default TableScanBuilder createScanBuilder() {
+    return null; // TODO: Enforce all subclasses to implement this later
   }
 
   /**
