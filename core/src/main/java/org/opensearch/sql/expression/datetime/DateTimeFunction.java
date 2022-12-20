94b6beca3b46d359ec7cbeaf81c6bf1ac9371ce2
MitchellGale-BitQuill <104795536+MitchellGale-BitQuill@users.noreply.github.com>
2022-12-20 11:17:48 -0800

    Adding UTC_DATE, UTC_TIME, UTC_TIMESTAMP (#1193)
    * Updated UTC TIME Functions for like now FunctionProperties.
    
    Signed-off-by: MitchellGale-BitQuill <mitchellg@bitquilltech.com>
    
    * Finished updates to tests.
    
    Signed-off-by: MitchellGale-BitQuill <mitchellg@bitquilltech.com>
    
    * Removed unused import.
    
    Signed-off-by: MitchellGale-BitQuill <mitchellg@bitquilltech.com>
    
    * Addressed PR comments.
    
    Signed-off-by: MitchellGale-BitQuill <mitchellg@bitquilltech.com>
    
    * Removed extra import.
    
    Signed-off-by: MitchellGale-BitQuill <mitchellg@bitquilltech.com>
    
    * removed `DateTimeFunction` used within class.
    
    Signed-off-by: MitchellGale-BitQuill <mitchellg@bitquilltech.com>
    
    Signed-off-by: MitchellGale-BitQuill <mitchellg@bitquilltech.com>

--- core/src/main/java/org/opensearch/sql/expression/datetime/DateTimeFunction.java
+++ core/src/main/java/org/opensearch/sql/expression/datetime/DateTimeFunction.java
@@ -63,6 +63,7 @@ import org.opensearch.sql.expression.function.BuiltinFunctionRepository;
 import org.opensearch.sql.expression.function.DefaultFunctionResolver;
 import org.opensearch.sql.expression.function.FunctionDSL;
 import org.opensearch.sql.expression.function.FunctionName;
+import org.opensearch.sql.expression.function.FunctionProperties;
 import org.opensearch.sql.expression.function.FunctionResolver;
 import org.opensearch.sql.utils.DateTimeUtils;
 
@@ -126,6 +127,9 @@ public class DateTimeFunction {
     repository.register(time());
     repository.register(time_to_sec());
     repository.register(timestamp());
+    repository.register(utc_date());
+    repository.register(utc_time());
+    repository.register(utc_timestamp());
     repository.register(date_format());
     repository.register(to_days());
     repository.register(unix_timestamp());
@@ -564,6 +568,33 @@ public class DateTimeFunction {
         impl(nullMissingHandling(DateTimeFunction::unixTimeStampOf), DOUBLE, TIMESTAMP),
         impl(nullMissingHandling(DateTimeFunction::unixTimeStampOf), DOUBLE, DOUBLE)
     );
+  }
+
+  /**
+   * UTC_DATE(). return the current UTC Date in format yyyy-MM-dd
+   */
+  private DefaultFunctionResolver utc_date() {
+    return define(BuiltinFunctionName.UTC_DATE.getName(),
+        implWithProperties(functionProperties
+            -> exprUtcDate(functionProperties), DATE));
+  }
+
+  /**
+   * UTC_TIME(). return the current UTC Time in format HH:mm:ss
+   */
+  private DefaultFunctionResolver utc_time() {
+    return define(BuiltinFunctionName.UTC_TIME.getName(),
+        implWithProperties(functionProperties
+            -> exprUtcTime(functionProperties), TIME));
+  }
+
+  /**
+   * UTC_TIMESTAMP(). return the current UTC TimeStamp in format yyyy-MM-dd HH:mm:ss
+   */
+  private DefaultFunctionResolver utc_timestamp() {
+    return define(BuiltinFunctionName.UTC_TIMESTAMP.getName(),
+        implWithProperties(functionProperties
+            -> exprUtcTimeStamp(functionProperties), DATETIME));
   }
 
   /**
@@ -1061,6 +1092,38 @@ public class DateTimeFunction {
    */
   private ExprValue exprTimeToSec(ExprValue time) {
     return new ExprLongValue(time.timeValue().toSecondOfDay());
+  }
+
+  /**
+   * UTC_DATE implementation for ExprValue.
+   *
+   * @param functionProperties FunctionProperties.
+   * @return ExprValue.
+   */
+  private ExprValue exprUtcDate(FunctionProperties functionProperties) {
+    return new ExprDateValue(exprUtcTimeStamp(functionProperties).dateValue());
+  }
+
+  /**
+   * UTC_TIME implementation for ExprValue.
+   *
+   * @param functionProperties FunctionProperties.
+   * @return ExprValue.
+   */
+  private ExprValue exprUtcTime(FunctionProperties functionProperties) {
+    return new ExprTimeValue(exprUtcTimeStamp(functionProperties).timeValue());
+  }
+
+  /**
+   * UTC_TIMESTAMP implementation for ExprValue.
+   *
+   * @param functionProperties FunctionProperties.
+   * @return ExprValue.
+   */
+  private ExprValue exprUtcTimeStamp(FunctionProperties functionProperties) {
+    var zdt = ZonedDateTime.now(functionProperties.getQueryStartClock())
+        .withZoneSameInstant(ZoneId.of("UTC"));
+    return new ExprDatetimeValue(zdt.toLocalDateTime());
   }
 
   /**
