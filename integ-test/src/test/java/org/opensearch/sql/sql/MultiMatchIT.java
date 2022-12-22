77c82dae8e0ca9935b68ea3751b25626c0e36fc4
MitchellGale-BitQuill <mitchellg@bitquilltech.com>
2022-12-21 16:09:17 -0800

    Reverting changes in multimatchIT
    Signed-off-by: MitchellGale-BitQuill <mitchellg@bitquilltech.com>

--- integ-test/src/test/java/org/opensearch/sql/sql/MultiMatchIT.java
+++ integ-test/src/test/java/org/opensearch/sql/sql/MultiMatchIT.java
@@ -6,6 +6,8 @@
 package org.opensearch.sql.sql;
 
 import static org.opensearch.sql.legacy.TestsConstants.TEST_INDEX_BEER;
+import static org.opensearch.sql.util.MatcherUtils.rows;
+import static org.opensearch.sql.util.MatcherUtils.verifyDataRows;
 
 import java.io.IOException;
 import org.json.JSONObject;
@@ -35,30 +37,30 @@ public class MultiMatchIT extends SQLIntegTestCase {
   }
 
   @Test
-  public void test_all_params() throws IOException {
+  public void test_all_params() {
     String query = "SELECT Id FROM " + TEST_INDEX_BEER
         + " WHERE multi_match(['Body', Tags], 'taste beer', operator='and', analyzer=english,"
         + "auto_generate_synonyms_phrase_query=true, boost = 0.77, cutoff_frequency=0.33,"
         + "fuzziness = 'AUTO:1,5', fuzzy_transpositions = false, lenient = true, max_expansions = 25,"
         + "minimum_should_match = '2<-25% 9<-3', prefix_length = 7, tie_breaker = 0.3,"
         + "type = most_fields, slop = 2, zero_terms_query = 'ALL');";
-    var result = new JSONObject(executeQuery(query, "jdbc"));
+    JSONObject result = executeJdbcRequest(query);
     assertEquals(10, result.getInt("total"));
   }
 
   @Test
-  public void verify_wildcard_test() throws IOException {
+  public void verify_wildcard_test() {
     String query1 = "SELECT Id FROM " + TEST_INDEX_BEER
         + " WHERE multi_match(['Tags'], 'taste')";
-    var result1 = new JSONObject(executeQuery(query1, "jdbc"));
+    JSONObject result1 = executeJdbcRequest(query1);
     String query2 = "SELECT Id FROM " + TEST_INDEX_BEER
         + " WHERE multi_match(['T*'], 'taste')";
-    var result2 = new JSONObject(executeQuery(query2, "jdbc"));
+    JSONObject result2 = executeJdbcRequest(query2);
     assertNotEquals(result2.getInt("total"), result1.getInt("total"));
 
     String query = "SELECT Id FROM " + TEST_INDEX_BEER
         + " WHERE multi_match(['*Date'], '2014-01-22');";
-    var result = new JSONObject(executeQuery(query, "jdbc"));
+    JSONObject result = executeJdbcRequest(query);
     assertEquals(10, result.getInt("total"));
   }
 
