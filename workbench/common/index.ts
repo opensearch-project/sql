9d4a8418bbfbead54cf848d247b5b6af513300d7
YANGDB <yang.db.dev@gmail.com>
2023-01-13 11:37:52 -0800

    port repo split to main (#1232)
    * remove sub-folders which upgraded as new repositories
     - sql-cli
     - sql-odbc
     - sql-jdbc
     - dashboards-query-workbench
     - add additional workflow run triggers
     - move the BI tests to jdbc/odbc
     - move the sql-cli outside and clone for the doctest build
    
    Signed-off-by: YANGDB <yang.db.dev@gmail.com>
    
    * Merge remote-tracking branch 'upstream/main'
    
    Signed-off-by: YANGDB <yang.db.dev@gmail.com>
    
    * add sql-jdbc dependendcy to gradle
    
    Signed-off-by: YANGDB <yang.db.dev@gmail.com>
    
    * Disallow unquoted literals in `LIKE` clause in `DESCRIBE` statement (#1181)
    
    * Allow quoted literals only in `DESCRIBE` and `SHOW` clauses. Tests.
    
    Signed-off-by: Yury-Fridlyand <yuryf@bitquilltech.com>
    
    * Fix doctest after rebase.
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    * Fix doctest after rebase. - Typo fix.
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    * Update syntax section.
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    Signed-off-by: Yury-Fridlyand <yuryf@bitquilltech.com>
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    Creation and updating 2.x branch (#6)
    
    * Create the 2.x branch
    * Add  workflow_dispatch: to github CLI
    
    Signed-off-by: YANGDB <yang.db.dev@gmail.com>
    
    * remove sub-folders which upgraded as new repositories
     - sql-cli
     - sql-odbc
     - sql-jdbc
     - dashboards-query-workbench
    
    Signed-off-by: YANG-DB <yang-db-dev@gmail.com>
    
    * add additional workflow run triggers
    
    Signed-off-by: YANGDB <yang.db.dev@gmail.com>
    
    * Merge remote-tracking branch 'upstream/main'
     - move the BI tests to jdbc/odbc
     - move the sql-cli outside and clone for the doctest build
    
    Signed-off-by: YANGDB <yang.db.dev@gmail.com>
    
    * Disallow unquoted literals in `LIKE` clause in `DESCRIBE` statement (#1181) (#3)
    
    * Allow quoted literals only in `DESCRIBE` and `SHOW` clauses. Tests.
    
    Signed-off-by: Yury-Fridlyand <yuryf@bitquilltech.com>
    
    * Fix doctest after rebase.
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    * Fix doctest after rebase. - Typo fix.
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    * Update syntax section.
    
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    Signed-off-by: Yury-Fridlyand <yuryf@bitquilltech.com>
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    Signed-off-by: Yury-Fridlyand <yuryf@bitquilltech.com>
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    Co-authored-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    
    Signed-off-by: YANG-DB <yang-db-dev@gmail.com>
    Signed-off-by: YANGDB <yang.db.dev@gmail.com>
    Signed-off-by: Yury-Fridlyand <yuryf@bitquilltech.com>
    Signed-off-by: Yury-Fridlyand <yury.fridlyand@improving.com>
    Co-authored-by: lior perry <lioperry@amazon.com>
    Co-authored-by: yang-db <yang-db-dev@gmail.com>
    Co-authored-by: Yury-Fridlyand <yury.fridlyand@improving.com>

--- workbench/common/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright OpenSearch Contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-
-export const PLUGIN_ID = 'queryWorkbenchDashboards';
-export const PLUGIN_NAME = 'Query Workbench';
