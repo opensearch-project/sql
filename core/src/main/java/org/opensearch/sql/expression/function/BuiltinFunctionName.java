4e2422e5795ee7f3ebf6adef78adfbcb46ead165
Margarit Hakobyan <margarit.hakobyan@improving.com>
2023-01-24 16:05:50 -0800

    Minor cleanup
    Signed-off-by: Margarit Hakobyan <margarit.hakobyan@improving.com>

--- core/src/main/java/org/opensearch/sql/expression/function/BuiltinFunctionName.java
+++ core/src/main/java/org/opensearch/sql/expression/function/BuiltinFunctionName.java
@@ -258,7 +258,6 @@ public enum BuiltinFunctionName {
     ALL_NATIVE_FUNCTIONS = builder.build();
   }
 
-
   private static final Map<String, BuiltinFunctionName> AGGREGATION_FUNC_MAPPING =
       new ImmutableMap.Builder<String, BuiltinFunctionName>()
           .put("max", BuiltinFunctionName.MAX)
