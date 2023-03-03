56b32fa2ea91af419729fd7100535973097bfc02
Vamsi Manohar <reddyvam@amazon.com>
2023-03-02 20:06:06 -0800

    Added unit tests for sts assume role credentials provider
    Signed-off-by: Vamsi Manohar <reddyvam@amazon.com>

--- prometheus/src/test/java/org/opensearch/sql/prometheus/authinterceptors/AwsSigningInterceptorTest.java
+++ prometheus/src/test/java/org/opensearch/sql/prometheus/authinterceptors/AwsSigningInterceptorTest.java
@@ -11,8 +11,11 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSSessionCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.STSAssumeRoleSessionCredentialsProvider;
+import com.amazonaws.auth.STSSessionCredentialsProvider;
 import lombok.SneakyThrows;
 import okhttp3.Interceptor;
 import okhttp3.Request;
@@ -33,6 +36,9 @@ public class AwsSigningInterceptorTest {
   @Captor
   ArgumentCaptor<Request> requestArgumentCaptor;
 
+  @Mock
+  private STSAssumeRoleSessionCredentialsProvider stsAssumeRoleSessionCredentialsProvider;
+
   @Test
   void testConstructors() {
     Assertions.assertThrows(NullPointerException.class, () ->
@@ -52,8 +58,8 @@ public class AwsSigningInterceptorTest {
         .url("http://localhost:9090")
         .build());
     AwsSigningInterceptor awsSigningInterceptor
-        =
-        new AwsSigningInterceptor(getStaticAWSCredentialsProvider("testAccessKey", "testSecretKey"),
+        = new AwsSigningInterceptor(
+            getStaticAWSCredentialsProvider("testAccessKey", "testSecretKey"),
             "us-east-1", "aps");
     awsSigningInterceptor.intercept(chain);
     verify(chain).proceed(requestArgumentCaptor.capture());
@@ -64,9 +70,50 @@ public class AwsSigningInterceptorTest {
   }
 
 
+  @Test
+  @SneakyThrows
+  void testSTSCredentialsProviderInterceptor() {
+    when(chain.request()).thenReturn(new Request.Builder()
+        .url("http://localhost:9090")
+        .build());
+    when(stsAssumeRoleSessionCredentialsProvider.getCredentials())
+        .thenReturn(getAWSSessionCredentials());
+    AwsSigningInterceptor awsSigningInterceptor
+        = new AwsSigningInterceptor(stsAssumeRoleSessionCredentialsProvider,
+            "us-east-1", "aps");
+    awsSigningInterceptor.intercept(chain);
+    verify(chain).proceed(requestArgumentCaptor.capture());
+    Request request = requestArgumentCaptor.getValue();
+    Assertions.assertNotNull(request.headers("Authorization"));
+    Assertions.assertNotNull(request.headers("x-amz-date"));
+    Assertions.assertNotNull(request.headers("host"));
+    Assertions.assertEquals("session_token",
+        request.headers("x-amz-security-token").get(0));
+  }
+
+
   private AWSCredentialsProvider getStaticAWSCredentialsProvider(String accessKey,
                                                                  String secretKey) {
     return new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey));
+  }
+
+  private AWSSessionCredentials getAWSSessionCredentials() {
+    return new AWSSessionCredentials() {
+      @Override
+      public String getSessionToken() {
+        return "session_token";
+      }
+
+      @Override
+      public String getAWSAccessKeyId() {
+        return "access_key";
+      }
+
+      @Override
+      public String getAWSSecretKey() {
+        return "secret_key";
+      }
+    };
   }
 
 }
