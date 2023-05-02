8d7df838ab371a1618de6e418b3d4f709486b550
Peng Huo <penghuo@gmail.com>
2023-05-02 16:40:05 -0700

    add scalafmt (#1601)
    * add scalafmt
    * add flint ci workflow
    
    Signed-off-by: Peng Huo <penghuo@gmail.com>

--- flint/flint-spark-integration/src/main/scala/org/opensearch/flint/spark/FlintSparkExtensions.scala
+++ flint/flint-spark-integration/src/main/scala/org/opensearch/flint/spark/FlintSparkExtensions.scala
@@ -9,6 +9,5 @@ import org.apache.spark.sql.SparkSessionExtensions
 
 class FlintSparkExtensions extends (SparkSessionExtensions => Unit) {
 
-  override def apply(v1: SparkSessionExtensions): Unit = {
-  }
+  override def apply(v1: SparkSessionExtensions): Unit = {}
 }
