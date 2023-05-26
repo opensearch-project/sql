252f54c688223433d8d74c7b05079a3d16045e16
Peng Huo <penghuo@gmail.com>
2023-05-25 17:28:44 -0700

    Add Streaming write support (#1656)
    Signed-off-by: Peng Huo <penghuo@gmail.com>

--- flint/flint-spark-integration/src/main/scala/org/apache/spark/sql/flint/FlintTable.scala
+++ flint/flint-spark-integration/src/main/scala/org/apache/spark/sql/flint/FlintTable.scala
@@ -9,7 +9,7 @@ import java.util
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.connector.catalog.{SupportsRead, SupportsWrite, Table, TableCapability}
-import org.apache.spark.sql.connector.catalog.TableCapability.{BATCH_READ, BATCH_WRITE, TRUNCATE}
+import org.apache.spark.sql.connector.catalog.TableCapability.{BATCH_READ, BATCH_WRITE, STREAMING_WRITE, TRUNCATE}
 import org.apache.spark.sql.connector.read.ScanBuilder
 import org.apache.spark.sql.connector.write.{LogicalWriteInfo, WriteBuilder}
 import org.apache.spark.sql.types.StructType
@@ -44,7 +44,7 @@ case class FlintTable(
   }
 
   override def capabilities(): util.Set[TableCapability] =
-    util.EnumSet.of(BATCH_READ, BATCH_WRITE, TRUNCATE)
+    util.EnumSet.of(BATCH_READ, BATCH_WRITE, TRUNCATE, STREAMING_WRITE)
 
   override def newScanBuilder(options: CaseInsensitiveStringMap): ScanBuilder = {
     FlintScanBuilder(name, sparkSession, schema, options.asCaseSensitiveMap())
