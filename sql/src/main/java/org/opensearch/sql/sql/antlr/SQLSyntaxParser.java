95a2cd8a410d279f049f813b316f73d8d20a80ef
Matthew Wells <matthew.wells@improving.com>
2023-05-29 15:34:30 -0700

    Create new anonymizer for new engine (#266)
    * Created anonymizer listener for anonymizing SQL queries through the new engine
    Signed-off-by: Matthew Wells <matthew.wells@improving.com>

--- sql/src/main/java/org/opensearch/sql/sql/antlr/SQLSyntaxParser.java
+++ sql/src/main/java/org/opensearch/sql/sql/antlr/SQLSyntaxParser.java
@@ -8,6 +8,8 @@ package org.opensearch.sql.sql.antlr;
 
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.opensearch.sql.common.antlr.CaseInsensitiveCharStream;
 import org.opensearch.sql.common.antlr.Parser;
 import org.opensearch.sql.common.antlr.SyntaxAnalysisErrorListener;
@@ -18,6 +20,12 @@ import org.opensearch.sql.sql.antlr.parser.OpenSearchSQLParser;
  * SQL syntax parser which encapsulates an ANTLR parser.
  */
 public class SQLSyntaxParser implements Parser {
+  private SyntaxAnalysisErrorListener syntaxAnalysisErrorListener;
+  private static final Logger LOG = LogManager.getLogger(SQLSyntaxParser.class);
+
+  public SQLSyntaxParser() {
+    this.syntaxAnalysisErrorListener = new SyntaxAnalysisErrorListener();
+  }
 
   /**
    * Parse a SQL query by ANTLR parser.
@@ -26,10 +34,16 @@ public class SQLSyntaxParser implements Parser {
    */
   @Override
   public ParseTree parse(String query) {
+    AnonymizerListener anonymizer = new AnonymizerListener();
+
     OpenSearchSQLLexer lexer = new OpenSearchSQLLexer(new CaseInsensitiveCharStream(query));
     OpenSearchSQLParser parser = new OpenSearchSQLParser(new CommonTokenStream(lexer));
-    parser.addErrorListener(new SyntaxAnalysisErrorListener());
-    return parser.root();
-  }
+    parser.addErrorListener(syntaxAnalysisErrorListener);
+    parser.addParseListener(anonymizer);
 
+    ParseTree parseTree = parser.root();
+    LOG.info("New Engine Request Query: {}", anonymizer.getAnonymizedQueryString());
+
+    return parseTree;
+  }
 }
