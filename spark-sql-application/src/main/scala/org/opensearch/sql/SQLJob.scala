e234a516277864fcd156b27587ef8080d1b3497b
Rupal Mahajan <maharup@amazon.com>
2023-06-08 06:48:50 +0000

    Remove temp table
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>

--- spark-sql-application/src/main/scala/org/opensearch/sql/SQLJob.scala
+++ spark-sql-application/src/main/scala/org/opensearch/sql/SQLJob.scala
@@ -13,7 +13,7 @@ object SQLJob {
     val aos_host = args(2)
     val aos_region = args(3)
 
-     val aos = Map(
+    val aos = Map(
       "host" -> aos_host, 
       "port" -> "-1", 
       "scheme" -> "https", 
@@ -24,63 +24,37 @@ object SQLJob {
     val spark = SparkSession.builder().appName("SQLJob").getOrCreate()
 
     try {
-      // Create a tableschema for the DataFrame
-      val tableschema = StructType(Seq(
-        StructField("name", StringType, nullable = false),
-        StructField("age", IntegerType, nullable = false),
-        StructField("city", StringType, nullable = false)
-      ))
-
-      // Create a sequence of Row objects representing the data
-      val tabledata = Seq(
-        Row("Tina", 29, "Bellevue"),
-        Row("Jane", 25, "London"),
-        Row("Mike", 35, "Paris")
-      )
-
-      // Create a DataFrame from the tableschema and data
-      val df = spark.createDataFrame(spark.sparkContext.parallelize(tabledata), tableschema)
-
-      // Register the DataFrame as a temporary table/view
-      df.createOrReplaceTempView("my_table")  // Replace "my_table" with your desired name
-
       // Execute SQL query
       val result: DataFrame = spark.sql(query)
-      val resultJsonBlob = result.select(to_json(collect_list(struct(result.columns.map(col): _*)))).first().getString(0)
+      val resultJson = result.select(to_json(collect_list(struct(result.columns.map(col): _*)))).first().getString(0)
 
       // Convert the schema to a DataFrame
-      // Get the schema of the DataFrame
-      val resultschema = result.schema
-      val resultschemaRows = resultschema.fields.map { field =>
+      val schema = result.schema
+      val schemaRow = schema.fields.map { field =>
         Row(field.name, field.dataType.typeName)
       }
-      val resultschemaDF = spark.createDataFrame(spark.sparkContext.parallelize(resultschemaRows), StructType(Seq(
+      val schemaDF = spark.createDataFrame(spark.sparkContext.parallelize(schemaRow), StructType(Seq(
         StructField("column_name", StringType, nullable = false),
         StructField("data_type", StringType, nullable = false)
       )))
-      val schemaJsonBlob = resultschemaDF.select(to_json(collect_list(struct(resultschemaDF.columns.map(col): _*)))).first().getString(0)
+      val schemaJson = schemaDF.select(to_json(collect_list(struct(schemaDF.columns.map(col): _*)))).first().getString(0)
 
-      // Create a step DataFrame from the tableschema and data
-      val envVarValue = sys.env.getOrElse("EMR_STEP_ID", "")
-
-      val stepRows = Seq(
-        Row(envVarValue)
+      // Create a DataFrame with stepId, schema and result
+      val dataRow = Seq(
+        Row(sys.env.getOrElse("EMR_STEP_ID", ""))
       )
-
-      val stepDF = spark.createDataFrame(spark.sparkContext.parallelize(stepRows), StructType(Seq(
-        StructField("step_id", StringType, nullable = false)
+      val dataDF = spark.createDataFrame(spark.sparkContext.parallelize(dataRow), StructType(Seq(
+        StructField("stepId", StringType, nullable = false)
       )))
 
-      // Add JSON blob column to df1
-      val stepWithSchema = stepDF.withColumn("schema", lit(schemaJsonBlob)).withColumn("result",lit(resultJsonBlob))
-      stepWithSchema.show(false)
+      val data = dataDF.withColumn("schema", lit(schemaJson)).withColumn("result",lit(resultJson))
 
-     stepWithSchema.write
+      // Write data to OpenSearch index
+      data.write
         .format("flint")
         .options(aos)
         .mode("append")
-       .save(index)
-     println("Result saved to OpenSearch index successfully.")
+        .save(index)
 
     } finally {
       // Stop SparkSession
