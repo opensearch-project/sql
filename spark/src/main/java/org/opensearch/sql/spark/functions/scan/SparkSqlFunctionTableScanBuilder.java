a816a58dc888d6596b6f4c174d1d03bdff3dc70d
Rupal Mahajan <maharup@amazon.com>
2023-07-05 16:50:09 -0700

    Add spark connector (#1780)
    * Create Spark Connector
    
    Signed-off-by: Vamsi Manohar <reddyvam@amazon.com>
    
    * Add spark client and engine
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Remove vars
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Spark connector draft
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * nit
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Fix checkstyle errors
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * nit
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Fix license header
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Add spark storage test
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Update comments
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Fix checkstyle in comments
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Update tests
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Address PR comments
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Refactor class name
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Address PR comment
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    ---------
    
    Signed-off-by: Vamsi Manohar <reddyvam@amazon.com>
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    Co-authored-by: Vamsi Manohar <reddyvam@amazon.com>

---
+++ spark/src/main/java/org/opensearch/sql/spark/functions/scan/SparkSqlFunctionTableScanBuilder.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.sql.spark.functions.scan;
+
+import lombok.AllArgsConstructor;
+import org.opensearch.sql.planner.logical.LogicalProject;
+import org.opensearch.sql.spark.client.SparkClient;
+import org.opensearch.sql.spark.request.SparkQueryRequest;
+import org.opensearch.sql.storage.TableScanOperator;
+import org.opensearch.sql.storage.read.TableScanBuilder;
+
+/**
+ * TableScanBuilder for sql function of spark connector.
+ */
+@AllArgsConstructor
+public class SparkSqlFunctionTableScanBuilder extends TableScanBuilder {
+
+  private final SparkClient sparkClient;
+
+  private final SparkQueryRequest sparkQueryRequest;
+
+  @Override
+  public TableScanOperator build() {
+    //TODO: return SqlFunctionTableScanOperator
+    return null;
+  }
+
+  @Override
+  public boolean pushDownProject(LogicalProject project) {
+    return true;
+  }
+}
