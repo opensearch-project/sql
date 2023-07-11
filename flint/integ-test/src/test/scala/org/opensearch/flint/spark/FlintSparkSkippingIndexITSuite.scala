e10d160f2dcfc397e21721b3355e1545e3892494
Chen Dai <daichen@amazon.com>
2023-07-11 16:59:19 -0700

    Fix IT failure
    Signed-off-by: Chen Dai <daichen@amazon.com>

--- flint/integ-test/src/test/scala/org/opensearch/flint/spark/FlintSparkSkippingIndexITSuite.scala
+++ flint/integ-test/src/test/scala/org/opensearch/flint/spark/FlintSparkSkippingIndexITSuite.scala
@@ -175,6 +175,19 @@ class FlintSparkSkippingIndexITSuite
     indexData should have size 2
   }
 
+  test("should fail to manual refresh an incremental refreshing index") {
+    flint
+      .skippingIndex()
+      .onTable(testTable)
+      .addPartitions("year", "month")
+      .create()
+    flint.refreshIndex(testIndex, INCREMENTAL)
+
+    assertThrows[IllegalStateException] {
+      flint.refreshIndex(testIndex, FULL)
+    }
+  }
+
   test("incremental refresh skipping index successfully") {
     // Create Flint index and wait for complete
     flint
@@ -199,19 +212,6 @@ class FlintSparkSkippingIndexITSuite
         .collect()
         .toSet
     indexData should have size 2
-  }
-
-  test("should fail to manual refresh an incremental refreshing index") {
-    flint
-      .skippingIndex()
-      .onTable(testTable)
-      .addPartitions("year", "month")
-      .create()
-    flint.refreshIndex(testIndex, INCREMENTAL)
-
-    assertThrows[IllegalStateException] {
-      flint.refreshIndex(testIndex, FULL)
-    }
   }
 
   test("can have only 1 skipping index on a table") {
