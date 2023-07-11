c88b5947d066366cc66facaa1c3326be53543d9f
Andrew Carbonetto <andrewc@bitquilltech.com>
2023-07-10 23:32:53 -0700

    Add _routing to SQL includes list (#277) (#1771)
    * Add _routing to SQL includes list
    
    Signed-off-by: Andrew Carbonetto <andrewc@bitquilltech.com>
    
    * Update IT index
    
    Signed-off-by: Andrew Carbonetto <andrewc@bitquilltech.com>
    
    * Update doctest
    
    Signed-off-by: acarbonetto <andrewc@bitquilltech.com>
    
    * Add WHERE clause IT tests
    
    Signed-off-by: acarbonetto <andrewc@bitquilltech.com>
    
    * Fix IT test
    
    Signed-off-by: acarbonetto <andrewc@bitquilltech.com>
    
    * Update documentation for _routing
    
    Signed-off-by: acarbonetto <andrewc@bitquilltech.com>
    
    ---------
    
    Signed-off-by: Andrew Carbonetto <andrewc@bitquilltech.com>
    Signed-off-by: acarbonetto <andrewc@bitquilltech.com>
    (cherry picked from commit 876a9c80a00c5ffbc416ea3d62d11b9f005123ce)

--- opensearch/src/main/java/org/opensearch/sql/opensearch/storage/OpenSearchIndex.java
+++ opensearch/src/main/java/org/opensearch/sql/opensearch/storage/OpenSearchIndex.java
@@ -45,12 +45,15 @@ public class OpenSearchIndex implements Table {
   public static final String METADATA_FIELD_MAXSCORE = "_maxscore";
   public static final String METADATA_FIELD_SORT = "_sort";
 
+  public static final String METADATA_FIELD_ROUTING = "_routing";
+
   public static final java.util.Map<String, ExprType> METADATAFIELD_TYPE_MAP = Map.of(
       METADATA_FIELD_ID, ExprCoreType.STRING,
       METADATA_FIELD_INDEX, ExprCoreType.STRING,
       METADATA_FIELD_SCORE, ExprCoreType.FLOAT,
       METADATA_FIELD_MAXSCORE, ExprCoreType.FLOAT,
-      METADATA_FIELD_SORT, ExprCoreType.LONG
+      METADATA_FIELD_SORT, ExprCoreType.LONG,
+      METADATA_FIELD_ROUTING, ExprCoreType.STRING
   );
 
   /** OpenSearch client connection. */
