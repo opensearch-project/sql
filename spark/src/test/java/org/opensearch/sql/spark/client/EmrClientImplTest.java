0140e3795011e34e9d073e8864acac7436b1a9f2
Rupal Mahajan <maharup@amazon.com>
2023-07-11 09:51:39 -0700

    Add EMR client to spark connector (#1790)
    * Create Spark Connector
    
    Signed-off-by: Vamsi Manohar <reddyvam@amazon.com>
    
    * Add spark client and engine
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Remove vars
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Spark connector draft
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * nit
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Fix checkstyle errors
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * nit
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Fix license header
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Add spark storage test
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Update comments
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Fix checkstyle in comments
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Update tests
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Add emr client
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Set default values for flint args
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Validate emr auth type
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Add default constants for flint
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Update unit tests
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Address PR comments
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * tests draft
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Refactor class name
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Rename classes and update tests
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Update scan operator test
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Address PR comment
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Fix Connection pool shut down issue
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Update emr unit tests
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Update doc and tests
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * nit
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Update EMR clinet impl tests
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Address PR comments
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Make spark & flint jars configurable
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Address comments
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Add spark application id in logs
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * nit
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    * Delete result when not required
    
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    
    ---------
    
    Signed-off-by: Vamsi Manohar <reddyvam@amazon.com>
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>
    Co-authored-by: Vamsi Manohar <reddyvam@amazon.com>
    (cherry picked from commit 392a72000224f36a75222b22398357933d18c015)

---
+++ spark/src/test/java/org/opensearch/sql/spark/client/EmrClientImplTest.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.sql.spark.client;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+import static org.opensearch.sql.spark.constants.TestConstants.EMR_CLUSTER_ID;
+import static org.opensearch.sql.spark.constants.TestConstants.QUERY;
+import static org.opensearch.sql.spark.utils.TestUtils.getJson;
+
+import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduce;
+import com.amazonaws.services.elasticmapreduce.model.AddJobFlowStepsResult;
+import com.amazonaws.services.elasticmapreduce.model.DescribeStepResult;
+import com.amazonaws.services.elasticmapreduce.model.Step;
+import com.amazonaws.services.elasticmapreduce.model.StepStatus;
+import lombok.SneakyThrows;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.opensearch.sql.spark.helper.FlintHelper;
+import org.opensearch.sql.spark.response.SparkResponse;
+
+@ExtendWith(MockitoExtension.class)
+public class EmrClientImplTest {
+
+  @Mock
+  private AmazonElasticMapReduce emr;
+  @Mock
+  private FlintHelper flint;
+  @Mock
+  private SparkResponse sparkResponse;
+
+  @Test
+  @SneakyThrows
+  void testRunEmrApplication() {
+    AddJobFlowStepsResult addStepsResult = new AddJobFlowStepsResult().withStepIds(EMR_CLUSTER_ID);
+    when(emr.addJobFlowSteps(any())).thenReturn(addStepsResult);
+
+    StepStatus stepStatus = new StepStatus();
+    stepStatus.setState("COMPLETED");
+    Step step = new Step();
+    step.setStatus(stepStatus);
+    DescribeStepResult describeStepResult = new DescribeStepResult();
+    describeStepResult.setStep(step);
+    when(emr.describeStep(any())).thenReturn(describeStepResult);
+
+    EmrClientImpl emrClientImpl = new EmrClientImpl(
+        emr, EMR_CLUSTER_ID, flint, sparkResponse, null);
+    emrClientImpl.runEmrApplication(QUERY);
+  }
+
+  @Test
+  @SneakyThrows
+  void testRunEmrApplicationFailed() {
+    AddJobFlowStepsResult addStepsResult = new AddJobFlowStepsResult().withStepIds(EMR_CLUSTER_ID);
+    when(emr.addJobFlowSteps(any())).thenReturn(addStepsResult);
+
+    StepStatus stepStatus = new StepStatus();
+    stepStatus.setState("FAILED");
+    Step step = new Step();
+    step.setStatus(stepStatus);
+    DescribeStepResult describeStepResult = new DescribeStepResult();
+    describeStepResult.setStep(step);
+    when(emr.describeStep(any())).thenReturn(describeStepResult);
+
+    EmrClientImpl emrClientImpl = new EmrClientImpl(
+        emr, EMR_CLUSTER_ID, flint, sparkResponse, null);
+    RuntimeException exception = Assertions.assertThrows(RuntimeException.class,
+        () -> emrClientImpl.runEmrApplication(QUERY));
+    Assertions.assertEquals("Spark SQL application failed.",
+        exception.getMessage());
+  }
+
+  @Test
+  @SneakyThrows
+  void testRunEmrApplicationCancelled() {
+    AddJobFlowStepsResult addStepsResult = new AddJobFlowStepsResult().withStepIds(EMR_CLUSTER_ID);
+    when(emr.addJobFlowSteps(any())).thenReturn(addStepsResult);
+
+    StepStatus stepStatus = new StepStatus();
+    stepStatus.setState("CANCELLED");
+    Step step = new Step();
+    step.setStatus(stepStatus);
+    DescribeStepResult describeStepResult = new DescribeStepResult();
+    describeStepResult.setStep(step);
+    when(emr.describeStep(any())).thenReturn(describeStepResult);
+
+    EmrClientImpl emrClientImpl = new EmrClientImpl(
+        emr, EMR_CLUSTER_ID, flint, sparkResponse, null);
+    RuntimeException exception = Assertions.assertThrows(RuntimeException.class,
+        () -> emrClientImpl.runEmrApplication(QUERY));
+    Assertions.assertEquals("Spark SQL application failed.",
+        exception.getMessage());
+  }
+
+  @Test
+  @SneakyThrows
+  void testRunEmrApplicationRunnning() {
+    AddJobFlowStepsResult addStepsResult = new AddJobFlowStepsResult().withStepIds(EMR_CLUSTER_ID);
+    when(emr.addJobFlowSteps(any())).thenReturn(addStepsResult);
+
+    StepStatus runningStatus = new StepStatus();
+    runningStatus.setState("RUNNING");
+    Step runningStep = new Step();
+    runningStep.setStatus(runningStatus);
+    DescribeStepResult runningDescribeStepResult = new DescribeStepResult();
+    runningDescribeStepResult.setStep(runningStep);
+
+    StepStatus completedStatus = new StepStatus();
+    completedStatus.setState("COMPLETED");
+    Step completedStep = new Step();
+    completedStep.setStatus(completedStatus);
+    DescribeStepResult completedDescribeStepResult = new DescribeStepResult();
+    completedDescribeStepResult.setStep(completedStep);
+
+    when(emr.describeStep(any())).thenReturn(runningDescribeStepResult)
+        .thenReturn(completedDescribeStepResult);
+
+    EmrClientImpl emrClientImpl = new EmrClientImpl(
+        emr, EMR_CLUSTER_ID, flint, sparkResponse, null);
+    emrClientImpl.runEmrApplication(QUERY);
+  }
+
+  @Test
+  @SneakyThrows
+  void testSql() {
+    AddJobFlowStepsResult addStepsResult = new AddJobFlowStepsResult().withStepIds(EMR_CLUSTER_ID);
+    when(emr.addJobFlowSteps(any())).thenReturn(addStepsResult);
+
+    StepStatus runningStatus = new StepStatus();
+    runningStatus.setState("RUNNING");
+    Step runningStep = new Step();
+    runningStep.setStatus(runningStatus);
+    DescribeStepResult runningDescribeStepResult = new DescribeStepResult();
+    runningDescribeStepResult.setStep(runningStep);
+
+    StepStatus completedStatus = new StepStatus();
+    completedStatus.setState("COMPLETED");
+    Step completedStep = new Step();
+    completedStep.setStatus(completedStatus);
+    DescribeStepResult completedDescribeStepResult = new DescribeStepResult();
+    completedDescribeStepResult.setStep(completedStep);
+
+    when(emr.describeStep(any())).thenReturn(runningDescribeStepResult)
+        .thenReturn(completedDescribeStepResult);
+    when(sparkResponse.getResultFromOpensearchIndex())
+        .thenReturn(new JSONObject(getJson("select_query_response.json")));
+
+    EmrClientImpl emrClientImpl = new EmrClientImpl(
+        emr, EMR_CLUSTER_ID, flint, sparkResponse, null);
+    emrClientImpl.sql(QUERY);
+
+  }
+}
