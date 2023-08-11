0e7aed38259b5af45f48f94fb4af694f86817a54
Mitchell Gale <Mitchell.Gale@improving.com>
2023-08-11 12:11:33 -0700

    Spotless apply on protocol
    Signed-off-by: Mitchell Gale <Mitchell.Gale@improving.com>

--- protocol/src/main/java/org/opensearch/sql/protocol/response/format/RawResponseFormatter.java
+++ protocol/src/main/java/org/opensearch/sql/protocol/response/format/RawResponseFormatter.java
@@ -3,16 +3,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-
 package org.opensearch.sql.protocol.response.format;
 
-/**
- * Response formatter to format response to csv or raw format.
- */
-//@RequiredArgsConstructor
+/** Response formatter to format response to csv or raw format. */
+// @RequiredArgsConstructor
 public class RawResponseFormatter extends FlatResponseFormatter {
   public RawResponseFormatter() {
     super("|", false);
   }
-
 }
--- protocol/src/test/java/org/opensearch/sql/protocol/response/QueryResultTest.java
+++ protocol/src/test/java/org/opensearch/sql/protocol/response/QueryResultTest.java
@@ -3,7 +3,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-
 package org.opensearch.sql.protocol.response;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
@@ -23,86 +22,77 @@ import org.opensearch.sql.executor.pagination.Cursor;
 
 class QueryResultTest {
 
-  private ExecutionEngine.Schema schema = new ExecutionEngine.Schema(ImmutableList.of(
-      new ExecutionEngine.Schema.Column("name", null, STRING),
-      new ExecutionEngine.Schema.Column("age", null, INTEGER)));
-
+  private ExecutionEngine.Schema schema =
+      new ExecutionEngine.Schema(
+          ImmutableList.of(
+              new ExecutionEngine.Schema.Column("name", null, STRING),
+              new ExecutionEngine.Schema.Column("age", null, INTEGER)));
 
   @Test
   void size() {
-    QueryResult response = new QueryResult(
-        schema,
-        Arrays.asList(
-            tupleValue(ImmutableMap.of("name", "John", "age", 20)),
-            tupleValue(ImmutableMap.of("name", "Allen", "age", 30)),
-            tupleValue(ImmutableMap.of("name", "Smith", "age", 40))
-        ), Cursor.None);
+    QueryResult response =
+        new QueryResult(
+            schema,
+            Arrays.asList(
+                tupleValue(ImmutableMap.of("name", "John", "age", 20)),
+                tupleValue(ImmutableMap.of("name", "Allen", "age", 30)),
+                tupleValue(ImmutableMap.of("name", "Smith", "age", 40))),
+            Cursor.None);
     assertEquals(3, response.size());
   }
 
   @Test
   void columnNameTypes() {
-    QueryResult response = new QueryResult(
-        schema,
-        Collections.singletonList(
-            tupleValue(ImmutableMap.of("name", "John", "age", 20))
-        ), Cursor.None);
+    QueryResult response =
+        new QueryResult(
+            schema,
+            Collections.singletonList(tupleValue(ImmutableMap.of("name", "John", "age", 20))),
+            Cursor.None);
 
-    assertEquals(
-        ImmutableMap.of("name", "string", "age", "integer"),
-        response.columnNameTypes()
-    );
+    assertEquals(ImmutableMap.of("name", "string", "age", "integer"), response.columnNameTypes());
   }
 
   @Test
   void columnNameTypesWithAlias() {
-    ExecutionEngine.Schema schema = new ExecutionEngine.Schema(ImmutableList.of(
-        new ExecutionEngine.Schema.Column("name", "n", STRING)));
-    QueryResult response = new QueryResult(
-        schema,
-        Collections.singletonList(tupleValue(ImmutableMap.of("n", "John"))),
-        Cursor.None);
+    ExecutionEngine.Schema schema =
+        new ExecutionEngine.Schema(
+            ImmutableList.of(new ExecutionEngine.Schema.Column("name", "n", STRING)));
+    QueryResult response =
+        new QueryResult(
+            schema,
+            Collections.singletonList(tupleValue(ImmutableMap.of("n", "John"))),
+            Cursor.None);
 
-    assertEquals(
-        ImmutableMap.of("n", "string"),
-        response.columnNameTypes()
-    );
+    assertEquals(ImmutableMap.of("n", "string"), response.columnNameTypes());
   }
 
   @Test
   void columnNameTypesFromEmptyExprValues() {
-    QueryResult response = new QueryResult(
-        schema,
-        Collections.emptyList(), Cursor.None);
-    assertEquals(
-        ImmutableMap.of("name", "string", "age", "integer"),
-        response.columnNameTypes()
-    );
+    QueryResult response = new QueryResult(schema, Collections.emptyList(), Cursor.None);
+    assertEquals(ImmutableMap.of("name", "string", "age", "integer"), response.columnNameTypes());
   }
 
   @Test
   void columnNameTypesFromExprValuesWithMissing() {
-    QueryResult response = new QueryResult(
-        schema,
-        Arrays.asList(
-            tupleValue(ImmutableMap.of("name", "John")),
-            tupleValue(ImmutableMap.of("name", "John", "age", 20))
-        ));
+    QueryResult response =
+        new QueryResult(
+            schema,
+            Arrays.asList(
+                tupleValue(ImmutableMap.of("name", "John")),
+                tupleValue(ImmutableMap.of("name", "John", "age", 20))));
 
-    assertEquals(
-        ImmutableMap.of("name", "string", "age", "integer"),
-        response.columnNameTypes()
-    );
+    assertEquals(ImmutableMap.of("name", "string", "age", "integer"), response.columnNameTypes());
   }
 
   @Test
   void iterate() {
-    QueryResult response = new QueryResult(
-        schema,
-        Arrays.asList(
-            tupleValue(ImmutableMap.of("name", "John", "age", 20)),
-            tupleValue(ImmutableMap.of("name", "Allen", "age", 30))
-        ), Cursor.None);
+    QueryResult response =
+        new QueryResult(
+            schema,
+            Arrays.asList(
+                tupleValue(ImmutableMap.of("name", "John", "age", 20)),
+                tupleValue(ImmutableMap.of("name", "Allen", "age", 30))),
+            Cursor.None);
 
     int i = 0;
     for (Object[] objects : response) {
@@ -116,5 +106,4 @@ class QueryResultTest {
       i++;
     }
   }
-
 }
