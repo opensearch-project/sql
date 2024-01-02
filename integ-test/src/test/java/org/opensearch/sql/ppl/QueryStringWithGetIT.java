4e3f53aaa9ca6a044dc518da43f155750d486e5c
YANGDB <yang.db.dev@gmail.com>
2024-01-01 23:28:47 -0800

    fix failed tests
    Signed-off-by: YANGDB <yang.db.dev@gmail.com>

--- integ-test/src/test/java/org/opensearch/sql/ppl/QueryStringWithGetIT.java
+++ integ-test/src/test/java/org/opensearch/sql/ppl/QueryStringWithGetIT.java
@@ -5,7 +5,10 @@
 
 package org.opensearch.sql.ppl;
 
+import static org.opensearch.sql.legacy.TestsConstants.TEST_INDEX_BEER;
+
 import java.io.IOException;
+import org.json.JSONObject;
 import org.opensearch.rest.RestRequest;
 
 /** Run QueryStringIT tests using http GET request */
@@ -14,5 +17,15 @@ public class QueryStringWithGetIT extends QueryStringIT {
   public void init() throws IOException {
     super.init();
     this.method = RestRequest.Method.GET;
+  }
+
+  @Override
+  public void mandatory_params_test() throws IOException {
+    String query =
+        "source="
+            + TEST_INDEX_BEER
+            + " | where query_string(['Tags' ^ 1.5, Title, 'Body' 4.2], 'taste')";
+    JSONObject result = executeQuery(query);
+    assertEquals(16, result.getInt("total"));
   }
 }
--- integ-test/src/test/java/org/opensearch/sql/ppl/SimpleQueryStringWithGetIT.java
+++ integ-test/src/test/java/org/opensearch/sql/ppl/SimpleQueryStringWithGetIT.java
@@ -5,7 +5,10 @@
 
 package org.opensearch.sql.ppl;
 
+import static org.opensearch.sql.legacy.TestsConstants.TEST_INDEX_BEER;
+
 import java.io.IOException;
+import org.junit.Test;
 import org.opensearch.rest.RestRequest;
 
 /** Run SimpleQueryStringIT tests using http GET request */
@@ -14,5 +17,16 @@ public class SimpleQueryStringWithGetIT extends SimpleQueryStringIT {
   public void init() throws IOException {
     super.init();
     this.method = RestRequest.Method.GET;
+  }
+
+  @Test
+  public void test_simple_query_string() throws IOException {
+    String query =
+        "SOURCE="
+            + TEST_INDEX_BEER
+            + " | WHERE simple_query_string(['Tags' ^ 1.5, Title, 'Body' 4.2], 'taste') |"
+            + " fields Id";
+    var result = executeQuery(query);
+    assertEquals(16, result.getInt("total"));
   }
 }
