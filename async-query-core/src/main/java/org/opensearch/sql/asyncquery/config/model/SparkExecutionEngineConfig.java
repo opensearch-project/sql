b5a20b6afb3d6086e02e67541e35fabce950c090
Tomoyuki Morita <moritato@amazon.com>
2024-04-16 23:52:32 +0000

    Scaffold async-query-core module

---
+++ async-query-core/src/main/java/org/opensearch/sql/asyncquery/config/model/SparkExecutionEngineConfig.java
@@ -0,0 +1,21 @@
+package org.opensearch.sql.asyncquery.config.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * POJO for spark Execution Engine Config. Interface between {@link
+ * org.opensearch.sql.spark.asyncquery.AsyncQueryExecutorService} and {@link
+ * SparkExecutionEngineConfigSupplier}
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SparkExecutionEngineConfig {
+  private String applicationId;
+  private String region;
+  private String executionRoleARN;
+  private String sparkSubmitParameters;
+  private String clusterName;
+}
