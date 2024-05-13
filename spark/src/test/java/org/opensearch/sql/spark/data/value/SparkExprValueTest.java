8a9d38c95114ab33e2746dc10ee445fc1bd025be
Tomoyuki MORITA <moritato@amazon.com>
2024-05-13 13:36:05 -0700

    [Backport 2.x] Delete Spark datasource (#2638) (#2667)
    * Delete Spark datasource (#2638)
    
    * Delete Spark datasource
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>
    
    * Fix build error
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>
    
    * Delete spark_connector.rst
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>
    
    * Add missing test
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>
    
    ---------
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>
    (cherry picked from commit de7b367ec21f1eeba5698893fc713e86f783bdfb)
    Signed-off-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    * Fix test failure in DefaultSparkSqlFunctionResponseHandleTest
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>
    
    * Reformat
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>
    
    ---------
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>
    Signed-off-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- spark/src/test/java/org/opensearch/sql/spark/data/value/SparkExprValueTest.java
+++ spark/src/test/java/org/opensearch/sql/spark/data/value/SparkExprValueTest.java
@@ -11,18 +11,30 @@ import org.junit.jupiter.api.Test;
 import org.opensearch.sql.spark.data.type.SparkDataType;
 
 class SparkExprValueTest {
+  private final SparkDataType sparkDataType = new SparkDataType("char");
+
   @Test
-  public void type() {
-    assertEquals(
-        new SparkDataType("char"), new SparkExprValue(new SparkDataType("char"), "str").type());
+  public void getters() {
+    SparkExprValue sparkExprValue = new SparkExprValue(sparkDataType, "str");
+
+    assertEquals(sparkDataType, sparkExprValue.type());
+    assertEquals("str", sparkExprValue.value());
   }
 
   @Test
   public void unsupportedCompare() {
-    SparkDataType type = new SparkDataType("char");
+    SparkExprValue sparkExprValue = new SparkExprValue(sparkDataType, "str");
 
-    assertThrows(
-        UnsupportedOperationException.class,
-        () -> new SparkExprValue(type, "str").compare(new SparkExprValue(type, "str")));
+    assertThrows(UnsupportedOperationException.class, () -> sparkExprValue.compare(sparkExprValue));
+  }
+
+  @Test
+  public void testEquals() {
+    SparkExprValue sparkExprValue1 = new SparkExprValue(sparkDataType, "str");
+    SparkExprValue sparkExprValue2 = new SparkExprValue(sparkDataType, "str");
+    SparkExprValue sparkExprValue3 = new SparkExprValue(sparkDataType, "other");
+
+    assertTrue(sparkExprValue1.equal(sparkExprValue2));
+    assertFalse(sparkExprValue1.equal(sparkExprValue3));
   }
 }
