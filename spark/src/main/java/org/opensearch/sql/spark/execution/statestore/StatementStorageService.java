1985459b7979ca6d1d9cae0b2c04851e6657f5af
Tomoyuki MORITA <moritato@amazon.com>
2024-05-15 11:26:36 -0700

    Extract SessionStorageService and StatementStorageService (#2665)
    * Extract SessionStorageService and StatementStorageService
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>
    
    * Reformat
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>
    
    * Add copyright comment
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>
    
    * Add comments and remove unused methods
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>
    
    * Remove unneeded imports
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>
    
    * Fix code format issue
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>
    
    ---------
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>

---
+++ spark/src/main/java/org/opensearch/sql/spark/execution/statestore/StatementStorageService.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.sql.spark.execution.statestore;
+
+import java.util.Optional;
+import org.opensearch.sql.spark.execution.statement.StatementModel;
+import org.opensearch.sql.spark.execution.statement.StatementState;
+
+/**
+ * Interface for accessing {@link StatementModel} data storage. {@link StatementModel} is an
+ * abstraction over the query request within a Session.
+ */
+public interface StatementStorageService {
+
+  StatementModel createStatement(StatementModel statementModel, String datasourceName);
+
+  StatementModel updateStatementState(
+      StatementModel oldStatementModel, StatementState statementState, String datasourceName);
+
+  Optional<StatementModel> getStatement(String id, String datasourceName);
+}
