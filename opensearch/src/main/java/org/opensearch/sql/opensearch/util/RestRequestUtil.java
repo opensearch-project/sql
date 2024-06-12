c8fdabf8da7dcec8cd8ede7cdc890a045777ce0c
Frank Dattalo <fddattal@amazon.com>
2024-06-12 11:17:02 -0700

    Added Setting to Toggle Data Source Management Code Paths
    Signed-off-by: Frank Dattalo <fddattal@amazon.com>

---
+++ opensearch/src/main/java/org/opensearch/sql/opensearch/util/RestRequestUtil.java
@@ -0,0 +1,25 @@
+package org.opensearch.sql.opensearch.util;
+
+import lombok.NonNull;
+import org.opensearch.client.node.NodeClient;
+import org.opensearch.rest.RestChannel;
+import org.opensearch.rest.RestRequest;
+
+/** RestRequestUtil is a utility class for common operations on OpenSearch RestRequest's. */
+public class RestRequestUtil {
+
+  private RestRequestUtil() {
+    // utility class
+  }
+
+  /**
+   * Utility method for consuming all the request parameters. Doing this will ensure that the
+   * BaseRestHandler doesn't fail the request with an unconsumed parameter exception.
+   *
+   * @see org.opensearch.rest.BaseRestHandler#handleRequest(RestRequest, RestChannel, NodeClient)
+   * @param request - The request to consume all parameters on
+   */
+  public static void consumeAllRequestParameters(@NonNull RestRequest request) {
+    request.params().keySet().forEach(request::param);
+  }
+}
