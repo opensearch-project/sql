084a3c814638d3c7ceb163dc9f87c8f534194d78
Tomoyuki Morita <moritato@amazon.com>
2024-06-12 08:11:20 -0700

    Fix copyrights
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>

--- async-query/src/main/java/org/opensearch/sql/spark/config/SparkExecutionEngineConfigSupplierImpl.java
+++ async-query/src/main/java/org/opensearch/sql/spark/config/SparkExecutionEngineConfigSupplierImpl.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.opensearch.sql.spark.config;
 
 import static org.opensearch.sql.common.setting.Settings.Key.CLUSTER_NAME;
