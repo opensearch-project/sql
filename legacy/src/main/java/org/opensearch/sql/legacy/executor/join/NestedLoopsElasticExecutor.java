3185eda162871dae1af1ee47bcc140a1474ee0f8
Rupal Mahajan <maharup@amazon.com>
2024-06-19 10:53:39 -0700

    Update comment
    Signed-off-by: Rupal Mahajan <maharup@amazon.com>

--- legacy/src/main/java/org/opensearch/sql/legacy/executor/join/NestedLoopsElasticExecutor.java
+++ legacy/src/main/java/org/opensearch/sql/legacy/executor/join/NestedLoopsElasticExecutor.java
@@ -110,8 +110,10 @@ public class NestedLoopsElasticExecutor extends ElasticJoinExecutor {
           }
           /* Fetching next result page.
             Using scroll api - only scrollId from previous response is required for scroll request.
-            Using pit with search_after - we need to recreate search request along with pitId and sort fields from previous response.
+            Using pit with search_after - we need to recreate search request along with pitId and
+            sort fields from previous response.
             Here we are finding required size for recreating search request with pit and search after.
+            Conditions for size are similar as firstFetch().
             In case of scroll, this size will be ignored and size from first request will be used.
           */
           Integer hintLimit = nestedLoopsRequest.getFirstTable().getHintLimit();
