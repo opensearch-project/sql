8eae36f69da0a95acb9f453c261396699904113d
Tomoyuki MORITA <moritato@amazon.com>
2024-06-27 09:42:33 -0700

    Push down OpenSearch specific exception handling (#2778)
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>

--- async-query/src/main/java/org/opensearch/sql/spark/execution/statestore/OpenSearchStatementStorageService.java
+++ async-query/src/main/java/org/opensearch/sql/spark/execution/statestore/OpenSearchStatementStorageService.java
@@ -7,6 +7,10 @@ package org.opensearch.sql.spark.execution.statestore;
 
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.opensearch.index.engine.DocumentMissingException;
+import org.opensearch.index.engine.VersionConflictEngineException;
 import org.opensearch.sql.spark.asyncquery.model.AsyncQueryRequestContext;
 import org.opensearch.sql.spark.execution.statement.StatementModel;
 import org.opensearch.sql.spark.execution.statement.StatementState;
@@ -14,6 +18,7 @@ import org.opensearch.sql.spark.execution.xcontent.StatementModelXContentSeriali
 
 @RequiredArgsConstructor
 public class OpenSearchStatementStorageService implements StatementStorageService {
+  private static final Logger LOG = LogManager.getLogger();
 
   private final StateStore stateStore;
   private final StatementModelXContentSerializer serializer;
@@ -21,11 +26,17 @@ public class OpenSearchStatementStorageService implements StatementStorageServic
   @Override
   public StatementModel createStatement(
       StatementModel statementModel, AsyncQueryRequestContext asyncQueryRequestContext) {
-    return stateStore.create(
-        statementModel.getId(),
-        statementModel,
-        StatementModel::copy,
-        OpenSearchStateStoreUtil.getIndexName(statementModel.getDatasourceName()));
+    try {
+      return stateStore.create(
+          statementModel.getId(),
+          statementModel,
+          StatementModel::copy,
+          OpenSearchStateStoreUtil.getIndexName(statementModel.getDatasourceName()));
+    } catch (VersionConflictEngineException e) {
+      String errorMsg = "statement already exist. " + statementModel.getStatementId();
+      LOG.error(errorMsg);
+      throw new IllegalStateException(errorMsg);
+    }
   }
 
   @Override
@@ -37,10 +48,29 @@ public class OpenSearchStatementStorageService implements StatementStorageServic
   @Override
   public StatementModel updateStatementState(
       StatementModel oldStatementModel, StatementState statementState) {
-    return stateStore.updateState(
-        oldStatementModel,
-        statementState,
-        StatementModel::copyWithState,
-        OpenSearchStateStoreUtil.getIndexName(oldStatementModel.getDatasourceName()));
+    try {
+      return stateStore.updateState(
+          oldStatementModel,
+          statementState,
+          StatementModel::copyWithState,
+          OpenSearchStateStoreUtil.getIndexName(oldStatementModel.getDatasourceName()));
+    } catch (DocumentMissingException e) {
+      String errorMsg =
+          String.format(
+              "cancel statement failed. no statement found. statement: %s.",
+              oldStatementModel.getStatementId());
+      LOG.error(errorMsg);
+      throw new IllegalStateException(errorMsg);
+    } catch (VersionConflictEngineException e) {
+      StatementModel statementModel =
+          getStatement(oldStatementModel.getId(), oldStatementModel.getDatasourceName())
+              .orElse(oldStatementModel);
+      String errorMsg =
+          String.format(
+              "cancel statement failed. current statementState: %s " + "statement: %s.",
+              statementModel.getStatementState(), statementModel.getStatementId());
+      LOG.error(errorMsg);
+      throw new IllegalStateException(errorMsg);
+    }
   }
 }
