3617afe6e1cb35268735def6be2e468b20405886
Tomoyuki MORITA <moritato@amazon.com>
2024-07-12 15:10:18 -0700

    Introduce SparkParameterComposerCollection (#2774)
    * Introduce SparkParameterComposerCollection
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>
    
    * Fix comments
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>
    
    * Fix integ test
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>
    
    ---------
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>

---
+++ async-query/src/main/java/org/opensearch/sql/spark/config/SparkExecutionEngineConfigClusterSettingLoader.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.sql.spark.config;
+
+import static org.opensearch.sql.common.setting.Settings.Key.SPARK_EXECUTION_ENGINE_CONFIG;
+
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.opensearch.sql.common.setting.Settings;
+
+/** Load SparkExecutionEngineConfigClusterSetting from settings with privilege check. */
+@RequiredArgsConstructor
+public class SparkExecutionEngineConfigClusterSettingLoader {
+  private final Settings settings;
+
+  public Optional<SparkExecutionEngineConfigClusterSetting> load() {
+    String sparkExecutionEngineConfigSettingString =
+        this.settings.getSettingValue(SPARK_EXECUTION_ENGINE_CONFIG);
+    if (!StringUtils.isBlank(sparkExecutionEngineConfigSettingString)) {
+      return Optional.of(
+          AccessController.doPrivileged(
+              (PrivilegedAction<SparkExecutionEngineConfigClusterSetting>)
+                  () ->
+                      SparkExecutionEngineConfigClusterSetting.toSparkExecutionEngineConfig(
+                          sparkExecutionEngineConfigSettingString)));
+    } else {
+      return Optional.empty();
+    }
+  }
+}
