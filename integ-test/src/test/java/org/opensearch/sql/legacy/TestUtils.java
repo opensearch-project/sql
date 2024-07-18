0de937fe864c2cc1c46acda889b675a100a5545c
panguixin <panguixin@bytedance.com>
2024-07-18 19:24:45 +0800

    add IT
    Signed-off-by: panguixin <panguixin@bytedance.com>

--- integ-test/src/test/java/org/opensearch/sql/legacy/TestUtils.java
+++ integ-test/src/test/java/org/opensearch/sql/legacy/TestUtils.java
@@ -245,6 +245,11 @@ public class TestUtils {
     return getMappingFile(mappingFile);
   }
 
+  public static String getGeopointIndexMapping() {
+    String mappingFile = "geopoint_index_mapping.json";
+    return getMappingFile(mappingFile);
+  }
+
   public static void loadBulk(Client client, String jsonPath, String defaultIndex)
       throws Exception {
     System.out.println(String.format("Loading file %s into opensearch cluster", jsonPath));
