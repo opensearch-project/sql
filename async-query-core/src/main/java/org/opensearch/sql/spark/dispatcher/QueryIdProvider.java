2f031a5d6ddda855d7bfbcb2c669597807b1baa0
Tomoyuki MORITA <moritato@amazon.com>
2024-07-31 15:08:35 -0700

    Add AsyncQueryRequestContext to QueryIdProvider parameter (#2870)
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>

--- async-query-core/src/main/java/org/opensearch/sql/spark/dispatcher/QueryIdProvider.java
+++ async-query-core/src/main/java/org/opensearch/sql/spark/dispatcher/QueryIdProvider.java
@@ -5,9 +5,11 @@
 
 package org.opensearch.sql.spark.dispatcher;
 
+import org.opensearch.sql.spark.asyncquery.model.AsyncQueryRequestContext;
 import org.opensearch.sql.spark.dispatcher.model.DispatchQueryRequest;
 
 /** Interface for extension point to specify queryId. Called when new query is executed. */
 public interface QueryIdProvider {
-  String getQueryId(DispatchQueryRequest dispatchQueryRequest);
+  String getQueryId(
+      DispatchQueryRequest dispatchQueryRequest, AsyncQueryRequestContext asyncQueryRequestContext);
 }
