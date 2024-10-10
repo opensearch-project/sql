c9b61e7d88352ca17a36ec67e7a7a7af36638e58
Simeon Widdis <sawiddis@amazon.com>
2024-10-10 10:46:28 -0700

    Add reproducer for malformed cursor handling
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>

--- integ-test/src/test/java/org/opensearch/sql/sql/PaginationIT.java
+++ integ-test/src/test/java/org/opensearch/sql/sql/PaginationIT.java
@@ -253,6 +253,16 @@ public class PaginationIT extends SQLIntegTestCase {
     assertEquals(aliasFilteredResponse.getInt("size"), 4);
   }
 
+  @Test
+  public void testMalformedCursorGracefullyHandled() throws IOException {
+    ResponseException ex = assertThrows(
+            "Expected query with malformed cursor to raise error, but didn't",
+            ResponseException.class,
+            () -> executeCursorQuery("d:a11b4db33f")
+    );
+    assertEquals(ex.getResponse().getStatusLine().getStatusCode(), 400);
+  }
+
   private String executeFetchQuery(String query, int fetchSize, String requestType, String filter)
       throws IOException {
     String endpoint = "/_plugins/_sql?format=" + requestType;
