e184e28b4d5f557f3069d02275facab94e379892
Simeon Widdis <sawiddis@amazon.com>
2024-10-17 15:55:51 -0700

    Add failing tests
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>

---
+++ integ-test/src/test/java/org/opensearch/sql/legacy/MalformedQueryIT.java
@@ -0,0 +1,60 @@
+package org.opensearch.sql.legacy;
+
+import static org.opensearch.sql.legacy.TestUtils.performRequest;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Locale;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.opensearch.client.ResponseException;
+
+/** Tests for clean handling of various types of invalid queries */
+public class MalformedQueryIT extends SQLIntegTestCase {
+  @Override
+  protected void init() throws Exception {
+    loadIndex(Index.BANK);
+    loadIndex(Index.BANK_TWO);
+  }
+
+  public void testJoinWithInvalidCondition() throws IOException, ParseException {
+    ResponseException result =
+        assertThrows(
+            "Expected Join query with malformed 'ON' to raise error, but didn't",
+            ResponseException.class,
+            () ->
+                executeQuery(
+                    String.format(
+                        Locale.ROOT,
+                        "SELECT a.firstname, b.age FROM %s AS a INNER JOIN %s AS b %%"
+                            + " a.account_number=b.account_number",
+                        TestsConstants.TEST_INDEX_BANK,
+                        TestsConstants.TEST_INDEX_BANK_TWO)));
+    var errMsg = new JSONObject(EntityUtils.toString(result.getResponse().getEntity()));
+    System.err.println("Full response: " + errMsg);
+
+    Assert.assertEquals("SqlParseException", errMsg.getJSONObject("error").getString("type"));
+    Assert.assertEquals(400, errMsg.getInt("status"));
+  }
+
+  public void testWildcardInSubquery() throws IOException, ParseException {
+    ResponseException result =
+        assertThrows(
+            "Expected wildcard subquery to raise error, but didn't",
+            ResponseException.class,
+            () ->
+                executeQuery(
+                    String.format(
+                        Locale.ROOT,
+                        "SELECT a.age FROM %s AS a WHERE a.age IN (SELECT b.age FROM * AS b)",
+                        TestsConstants.TEST_INDEX_BANK)));
+    var errMsg = new JSONObject(EntityUtils.toString(result.getResponse().getEntity()));
+    System.err.println("Full response: " + errMsg);
+
+    Assert.assertEquals(
+        "IllegalArgumentException", errMsg.getJSONObject("error").getString("type"));
+    Assert.assertEquals(400, errMsg.getInt("status"));
+  }
+}
