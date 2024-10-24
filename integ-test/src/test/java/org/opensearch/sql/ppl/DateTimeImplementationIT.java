549bd1ac552c85f7cd3be59d51d9dd55a1ae1e98
Lantao Jin <ltjin@amazon.com>
2024-10-24 17:02:54 +0800

    correct IT
    Signed-off-by: Lantao Jin <ltjin@amazon.com>

--- integ-test/src/test/java/org/opensearch/sql/ppl/DateTimeImplementationIT.java
+++ integ-test/src/test/java/org/opensearch/sql/ppl/DateTimeImplementationIT.java
@@ -6,8 +6,10 @@
 package org.opensearch.sql.ppl;
 
 import static org.opensearch.sql.legacy.TestsConstants.TEST_INDEX_DATE;
+import static org.opensearch.sql.legacy.TestsConstants.TEST_INDEX_DATE_FORMATS;
 import static org.opensearch.sql.util.MatcherUtils.rows;
 import static org.opensearch.sql.util.MatcherUtils.schema;
+import static org.opensearch.sql.util.MatcherUtils.verifyDataRows;
 import static org.opensearch.sql.util.MatcherUtils.verifySchema;
 import static org.opensearch.sql.util.MatcherUtils.verifySome;
 
@@ -20,6 +22,7 @@ public class DateTimeImplementationIT extends PPLIntegTestCase {
   @Override
   public void init() throws IOException {
     loadIndex(Index.DATE);
+    loadIndex(Index.DATE_FORMATS);
   }
 
   @Test
@@ -175,5 +178,16 @@ public class DateTimeImplementationIT extends PPLIntegTestCase {
                 TEST_INDEX_DATE));
     verifySchema(result, schema("f", null, "timestamp"));
     verifySome(result.getJSONArray("datarows"), rows(new Object[] {null}));
+  }
+
+  @Test
+  public void testSpanDatetimeWithCustomFormat() throws IOException {
+    JSONObject result =
+        executeQuery(
+            String.format(
+                "source=%s | eval a = 1 | stats count() as cnt by span(yyyy-MM-dd, 1d) as span",
+                TEST_INDEX_DATE_FORMATS));
+    verifySchema(result, schema("cnt", null, "integer"), schema("span", null, "date"));
+    verifyDataRows(result, rows(2, "1984-04-12"));
   }
 }
