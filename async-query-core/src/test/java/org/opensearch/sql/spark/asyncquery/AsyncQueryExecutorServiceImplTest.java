f593d0486696e945fc267a1235c67e50ed749091
Tomoyuki Morita <moritato@amazon.com>
2024-10-29 16:28:03 -0700

    Fix code style
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>

--- async-query-core/src/test/java/org/opensearch/sql/spark/asyncquery/AsyncQueryExecutorServiceImplTest.java
+++ async-query-core/src/test/java/org/opensearch/sql/spark/asyncquery/AsyncQueryExecutorServiceImplTest.java
@@ -28,8 +28,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.opensearch.sql.spark.asyncquery.exceptions.AsyncQueryNotFoundException;
@@ -61,8 +59,6 @@ public class AsyncQueryExecutorServiceImplTest {
   @Mock private SparkExecutionEngineConfigSupplier sparkExecutionEngineConfigSupplier;
   @Mock private SparkSubmitParameterModifier sparkSubmitParameterModifier;
   @Mock private AsyncQueryRequestContext asyncQueryRequestContext;
-
-  @Captor private ArgumentCaptor<AsyncQueryJobMetadata> asyncQueryJobMetadataCaptor;
 
   @BeforeEach
   void setUp() {
