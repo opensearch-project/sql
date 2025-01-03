fe0c363b645cd2141d09be1a56e85e1833dd797e
Andy Kwok <andy.kwok@improving.com>
2025-01-03 15:33:30 -0800

    Unit-test 1
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>

--- opensearch/src/test/java/org/opensearch/sql/opensearch/planner/physical/OpenSearchEvalOperatorTest.java
+++ opensearch/src/test/java/org/opensearch/sql/opensearch/planner/physical/OpenSearchEvalOperatorTest.java
@@ -17,12 +17,19 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 import org.opensearch.client.node.NodeClient;
+import org.opensearch.sql.data.model.ExprLongValue;
 import org.opensearch.sql.data.model.ExprNullValue;
+import org.opensearch.sql.data.model.ExprTupleValue;
+import org.opensearch.sql.data.model.ExprValue;
 import org.opensearch.sql.data.type.ExprCoreType;
+import org.opensearch.sql.data.type.ExprType;
+import org.opensearch.sql.expression.DSL;
 import org.opensearch.sql.expression.Expression;
+import org.opensearch.sql.expression.FunctionExpression;
 import org.opensearch.sql.expression.LiteralExpression;
 import org.opensearch.sql.expression.NamedExpression;
 import org.opensearch.sql.expression.ReferenceExpression;
+import org.opensearch.sql.expression.env.Environment;
 import org.opensearch.sql.opensearch.data.type.OpenSearchTextType;
 import org.opensearch.sql.opensearch.data.value.OpenSearchExprTextValue;
 import org.opensearch.sql.opensearch.executor.protector.OpenSearchExecutionProtector;
@@ -32,57 +39,57 @@ import org.opensearch.sql.planner.physical.PhysicalPlanNodeVisitor;
 import org.opensearch.sql.planner.physical.ProjectOperator;
 
 import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 
 /**
- * To assert the original bahviour of eval operator.
+ * To assert the original behaviour of eval operator.
  */
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
 @RunWith(MockitoJUnitRunner.Silent.class)
 public class OpenSearchEvalOperatorTest {
+
   @Mock private PhysicalPlan input;
 
   @Mock(answer = Answers.RETURNS_DEEP_STUBS)
   private NodeClient nodeClient;
 
-  @Test
-  public void testOpenSearchEvalAccept() {
-    PhysicalPlanNodeVisitor physicalPlanNodeVisitor =
-              new PhysicalPlanNodeVisitor<Integer, Object>() {};
-    List<Pair<ReferenceExpression, Expression>> ipAddress = List.of(ImmutablePair.of(
-            new ReferenceExpression("ipAddress", OpenSearchTextType.of()),
-            new LiteralExpression(ExprNullValue.of())));
-    OpenSearchEvalOperator evalOperator = new OpenSearchEvalOperator(input, ipAddress, nodeClient);
-    assertNull(evalOperator.accept(physicalPlanNodeVisitor, null));
-  }
-
   // Test-case: Make sure the original eval works
   // Test-case: Make sure geoIpClient being called and assert the same mocked value being returned.
 
-  // To have some expression like a+2 == c to make sure the original eval behaviour stay.
+  /**
+   * The test-case aim to assert OpenSearchEvalOperator behaviour when evaluating generic expression,
+   * which is not OpenSearch Engine specific. (Ex: ABS(age) )
+   */
   @Test
   public void testEvalOperatorOnGenericOperations() {
 
-    ProjectOperator projectOperator = new ProjectOperator(input,
-            List.of(
-                    new NamedExpression("firstname", new ReferenceExpression("firstname", List.of("firstname"), OpenSearchTextType.of())),
-                    new NamedExpression("age", new ReferenceExpression("age", List.of("age"), ExprCoreType.LONG)),
-                    new NamedExpression("firstname", new ReferenceExpression("email", List.of("email"), OpenSearchTextType.of()))),
-            Collections.emptyList());
+    ExprTupleValue valueMap = new ExprTupleValue(new LinkedHashMap<>(Map.of(
+            "firstname", new OpenSearchExprTextValue("Amber"),
+            "age", new ExprLongValue(32),
+            "email", new OpenSearchExprTextValue("amberduke@pyrami.com"))));
 
-    List<Pair<ReferenceExpression, Expression>> ipAddress = List.of(ImmutablePair.of(
-            new ReferenceExpression("ipAddress", OpenSearchTextType.of()),
-            new LiteralExpression(ExprNullValue.of())));
-    OpenSearchEvalOperator evalOperator = new OpenSearchEvalOperator(projectOperator, ipAddress, nodeClient);
-    assertNull(evalOperator.next());
+    when(input.next()).thenReturn(valueMap);
 
+    List<Pair<ReferenceExpression, Expression>> ipAddress = List.of(
+            ImmutablePair.of(
+                    new ReferenceExpression("ageInAbs", OpenSearchTextType.of()),
+                    DSL.abs(DSL.abs(new ReferenceExpression("age", ExprCoreType.LONG)))));
+    OpenSearchEvalOperator evalOperator = new OpenSearchEvalOperator(input, ipAddress, nodeClient);
+
+    // Make sure generic Expression function as expected when wrapped with OpenSearchEvalOperator.
+    assertSame(32, evalOperator.next().keyValue("ageInAbs").integerValue());
   }
+
 }
