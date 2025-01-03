6dbf37bb19fd3e49dc94a6c237ca940bfbe068b7
Kenrick Yap <14yapkc1@gmail.com>
2025-01-03 10:43:05 -0800

    added implementation
    Signed-off-by: Kenrick Yap <14yapkc1@gmail.com>

--- core/src/main/java/org/opensearch/sql/expression/json/JsonFunctions.java
+++ core/src/main/java/org/opensearch/sql/expression/json/JsonFunctions.java
@@ -5,10 +5,20 @@
 
 package org.opensearch.sql.expression.json;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.experimental.UtilityClass;
+import org.opensearch.sql.data.model.ExprValue;
+import org.opensearch.sql.data.model.ExprValueUtils;
+import org.opensearch.sql.exception.SemanticCheckException;
+import org.opensearch.sql.expression.function.BuiltinFunctionName;
+import org.opensearch.sql.expression.function.BuiltinFunctionRepository;
+import org.opensearch.sql.expression.function.DefaultFunctionResolver;
+
 import static org.opensearch.sql.data.type.ExprCoreType.BOOLEAN;
 import static org.opensearch.sql.data.type.ExprCoreType.STRING;
 import static org.opensearch.sql.expression.function.FunctionDSL.define;
 import static org.opensearch.sql.expression.function.FunctionDSL.impl;
+import static org.opensearch.sql.expression.function.FunctionDSL.nullMissingHandling;
 
 import lombok.experimental.UtilityClass;
 import org.opensearch.sql.expression.function.BuiltinFunctionName;
