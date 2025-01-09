3407d4a4d92473b6c2be0154c325b49b6f26751a
Kenrick Yap <kenrick.yap@improving.com>
2025-01-09 10:58:21 -0800

    removed disabled import
    Signed-off-by: Kenrick Yap <kenrick.yap@improving.com>

--- core/src/test/java/org/opensearch/sql/expression/datetime/DateTimeFunctionTest.java
+++ core/src/test/java/org/opensearch/sql/expression/datetime/DateTimeFunctionTest.java
@@ -27,7 +27,6 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Stream;
 import lombok.AllArgsConstructor;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
--- core/src/test/java/org/opensearch/sql/expression/datetime/YearweekTest.java
+++ core/src/test/java/org/opensearch/sql/expression/datetime/YearweekTest.java
@@ -14,7 +14,6 @@ import static org.opensearch.sql.data.type.ExprCoreType.INTEGER;
 
 import java.time.LocalDate;
 import java.util.stream.Stream;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
