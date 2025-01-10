148803408b69d3bbb93616764d0ce62906f160cd
Andy Kwok <andy.kwok@improving.com>
2025-01-09 16:46:02 -0800

    Update java doc
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>

--- integ-test/src/test/java/org/opensearch/sql/geo/PplIpEnrichmentIT.java
+++ integ-test/src/test/java/org/opensearch/sql/geo/PplIpEnrichmentIT.java
@@ -16,7 +16,6 @@ import org.junit.jupiter.api.Test;
 import org.opensearch.client.Request;
 import org.opensearch.client.RequestOptions;
 import org.opensearch.client.Response;
-import org.opensearch.client.ResponseException;
 import org.opensearch.common.xcontent.XContentFactory;
 import org.opensearch.common.xcontent.XContentType;
 import org.opensearch.core.xcontent.XContentBuilder;
@@ -25,25 +24,34 @@ import org.opensearch.sql.ppl.PPLIntegTestCase;
 import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
 import static org.opensearch.sql.legacy.TestUtils.getResponseBody;
-import static org.opensearch.sql.legacy.TestsConstants.TEST_INDEX_ACCOUNT;
-import static org.opensearch.sql.legacy.TestsConstants.TEST_INDEX_BANK;
-import static org.opensearch.sql.legacy.TestsConstants.TEST_INDEX_DOG;
 import static org.opensearch.sql.legacy.TestsConstants.TEST_INDEX_GEOIP;
 import static org.opensearch.sql.util.MatcherUtils.columnName;
 import static org.opensearch.sql.util.MatcherUtils.rows;
 import static org.opensearch.sql.util.MatcherUtils.verifyColumn;
 import static org.opensearch.sql.util.MatcherUtils.verifyDataRows;
 
-/** Cross Cluster Search tests to be executed with security plugin. */
+/**
+ * IP enrichment PPL request with OpenSearch Geo-sptial plugin
+ */
 public class PplIpEnrichmentIT extends PPLIntegTestCase {
 
   private static boolean initialized = false;
+
+  private static Map<String, Object> MANIFEST_LOCATION = Map.of(
+          "endpoint",
+          "https://raw.githubusercontent.com/opensearch-project/geospatial/main/src/test/resources/ip2geo/server/city/manifest.json"
+  );
+
+  private static String DATASOURCE_NAME = "dummycityindex";
+
+  private static String PLUGIN_NAME = "opensearch-geospatial";
+
+  private static String GEO_SPATIAL_DATASOURCE_PATH = "/_plugins/geospatial/ip2geo/datasource/";
 
   @SneakyThrows
   @BeforeEach
@@ -57,6 +65,9 @@ public class PplIpEnrichmentIT extends PPLIntegTestCase {
   @Override
   protected void init() throws Exception {
     loadIndex(Index.GEOIP);
+    // Create a new dataSource
+    createDatasource(DATASOURCE_NAME, MANIFEST_LOCATION);
+    waitForDatasourceToBeAvailable(DATASOURCE_NAME, Duration.ofSeconds(10));
   }
 
 
@@ -69,31 +80,12 @@ public class PplIpEnrichmentIT extends PPLIntegTestCase {
     request.setOptions(restOptionsBuilder);
     Response response = client().performRequest(request);
     Assert.assertEquals(200, response.getStatusLine().getStatusCode());
-    Assert.assertTrue(getResponseBody(response, true).contains("opensearch-geospatial"));
+    Assert.assertTrue(getResponseBody(response, true).contains(PLUGIN_NAME));
   }
 
   @SneakyThrows
   @Test
   public void testGeoIpEnrichment() {
-    // Disable the denyList
-    updateClusterSetting(Map.of("plugins.geospatial.ip2geo.datasource.endpoint.denylist", Collections.emptyList()));
-    Map<String, Object> datasourceProperties = Map.of(
-            "endpoint",
-            "https://raw.githubusercontent.com/opensearch-project/geospatial/main/src/test/resources/ip2geo/server/city/manifest.json"
-    );
-
-    // Create a new dataSource
-    createDatasource("dummycityindex", datasourceProperties);
-    // Create Wait till setup is completed
-    waitForDatasourceToBeAvailable("dummycityindex", Duration.ofSeconds(10));
-
-    // Make sure test-data loaded correctly.
-    JSONObject result = executeQuery(String.format("search source=%s", TEST_INDEX_GEOIP));
-    verifyColumn(result, columnName("name"), columnName("ip"));
-    verifyDataRows(result,
-            rows("Test user - USA", "10.1.1.1"),
-            rows("Test user - Canada", "127.1.1.1"));
-
     JSONObject resultGeoIp = executeQuery(
             String.format("search source=%s | eval enrichmentResult = geoip(\\\"%s\\\",%s)",
                     TEST_INDEX_GEOIP, "dummycityindex", "ip"));
@@ -105,36 +97,39 @@ public class PplIpEnrichmentIT extends PPLIntegTestCase {
 
   }
 
-
-  protected Response updateClusterSetting(final Map<String, Object> properties) throws IOException {
-    XContentBuilder builder = XContentFactory.jsonBuilder().startObject();
-    builder.startObject("transient");
-    for (Map.Entry<String, Object> config : properties.entrySet()) {
-      builder.field(config.getKey(), config.getValue());
-    }
-    builder.endObject();
-    builder.endObject();
-
-    Request request = new Request("PUT", "/_cluster/settings");
-    request.setJsonEntity(builder.toString());
-    return client().performRequest(request);
-  }
-
-  protected Response createDatasource(final String name, Map<String, Object> properties) throws IOException {
+  /**
+   * Helper method to send a PUT request to create a dummy dataSource with provided endpoint for integration test.
+   * @param name Name of the dataSource
+   * @param properties Request payload in Json format
+   * @return Response for the create dataSource request.
+   * @throws IOException In case of network failure
+   */
+  private Response createDatasource(final String name, Map<String, Object> properties) throws IOException {
     XContentBuilder builder = XContentFactory.jsonBuilder().startObject();
     for (Map.Entry<String, Object> config : properties.entrySet()) {
       builder.field(config.getKey(), config.getValue());
     }
     builder.endObject();
-
-    Request request = new Request("PUT", "/_plugins/geospatial/ip2geo/datasource/" + name);
+    Request request = new Request("PUT", GEO_SPATIAL_DATASOURCE_PATH + name);
     request.setJsonEntity(builder.toString());
     return client().performRequest(request);
   }
 
-  protected void waitForDatasourceToBeAvailable(final String name, final Duration timeout) throws Exception {
+  /**
+   * Helper method check the status of dataSource creation within the specific timeframe.
+   * @param name The name of the dataSource to assert
+   * @param timeout The timeout value in seconds
+   * @throws Exception Exception
+   */
+  private void waitForDatasourceToBeAvailable(final String name, final Duration timeout) throws Exception {
     Instant start = Instant.now();
-    while ("AVAILABLE".equals(getDatasourceState(name)) == false) {
+    Request request = new Request("GET", GEO_SPATIAL_DATASOURCE_PATH + name);
+    Response response = client().performRequest(request);
+    var responseInMap = createParser(XContentType.JSON.xContent(), EntityUtils.toString(response.getEntity())).map();
+    var datasources = (List<Map<String, Object>>) responseInMap.get("datasources");
+    String state = (String) datasources.getFirst().get("state");
+
+    while (!"AVAILABLE".equals(state)) {
       if (Duration.between(start, Instant.now()).compareTo(timeout) > 0) {
         throw new RuntimeException(
                 String.format(
@@ -148,18 +143,4 @@ public class PplIpEnrichmentIT extends PPLIntegTestCase {
       Thread.sleep(1000);
     }
   }
-
-  private String getDatasourceState(final String name) throws Exception {
-    List<Map<String, Object>> datasources = (List<Map<String, Object>>) getDatasource(name).get("datasources");
-    return (String) datasources.get(0).get("state");
-  }
-
-  protected Map<String, Object> getDatasource(final String name) throws Exception {
-    Request request = new Request("GET", "_plugins/geospatial/ip2geo/datasource/" + name);
-    Response response = client().performRequest(request);
-    return createParser(XContentType.JSON.xContent(), EntityUtils.toString(response.getEntity())).map();
-  }
-
-
-
 }
