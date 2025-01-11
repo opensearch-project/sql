15a196b975a0430055b5bc23fde17d75a83e4763
Andy Kwok <andy.kwok@improving.com>
2025-01-10 16:44:41 -0800

    Test coverage
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>

--- core/src/main/java/org/opensearch/sql/expression/ip/IPFunctions.java
+++ core/src/main/java/org/opensearch/sql/expression/ip/IPFunctions.java
@@ -28,25 +28,12 @@ public class IPFunctions {
 
   public void register(BuiltinFunctionRepository repository) {
     repository.register(cidrmatch());
-    repository.register(geoIp());
   }
 
   private DefaultFunctionResolver cidrmatch() {
     return define(
         BuiltinFunctionName.CIDRMATCH.getName(),
         impl(nullMissingHandling(IPFunctions::exprCidrMatch), BOOLEAN, IP, STRING));
-  }
-
-  private DefaultFunctionResolver geoIp() {
-    return define(
-        BuiltinFunctionName.GEOIP.getName(),
-        impl(nullMissingHandling(IPFunctions::exprGeoIp), BOOLEAN, STRING, STRING),
-        impl(
-            nullMissingHandling(IPFunctions::exprGeoIpWithOption),
-            BOOLEAN,
-            STRING,
-            STRING,
-            STRING));
   }
 
   /**
@@ -71,12 +58,4 @@ public class IPFunctions {
         : ExprValueUtils.LITERAL_TRUE;
   }
 
-  private ExprValue exprGeoIpWithOption(
-      ExprValue dataSource, ExprValue ipAddress, ExprValue option) {
-    return ExprValueUtils.booleanValue(true);
-  }
-
-  private ExprValue exprGeoIp(ExprValue dataSource, ExprValue ipAddress) {
-    return ExprValueUtils.booleanValue(true);
-  }
 }
--- core/src/test/java/org/opensearch/sql/expression/ip/GeoIPFunctionTest.java
+++ core/src/test/java/org/opensearch/sql/expression/ip/GeoIPFunctionTest.java
@@ -5,8 +5,10 @@
 
 package org.opensearch.sql.expression.ip;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.opensearch.sql.data.type.ExprCoreType.BOOLEAN;
 import static org.opensearch.sql.data.type.ExprCoreType.STRING;
 
 import org.junit.jupiter.api.Test;
@@ -16,6 +18,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.opensearch.sql.data.model.ExprValue;
 import org.opensearch.sql.expression.DSL;
 import org.opensearch.sql.expression.Expression;
+import org.opensearch.sql.expression.FunctionExpression;
 import org.opensearch.sql.expression.env.Environment;
 
 @ExtendWith(MockitoExtension.class)
@@ -33,5 +36,11 @@ public class GeoIPFunctionTest {
                 DSL.geoip(DSL.literal("HARDCODED_DATASOURCE_NAME"), DSL.ref("ip_address", STRING))
                     .valueOf(env));
     assertTrue(exception.getMessage().matches(".*no default implementation available"));
+  }
+
+  @Test
+  public void testGeoipFnctionSignature() {
+    var geoip = DSL.geoip(DSL.literal("HARDCODED_DATASOURCE_NAME"), DSL.ref("ip_address", STRING));
+    assertEquals(BOOLEAN, geoip.type());
   }
 }
