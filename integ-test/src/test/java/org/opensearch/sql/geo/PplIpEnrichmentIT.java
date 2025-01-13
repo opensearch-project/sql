f4f7c844a612cb5aabf2d35a9a0662b2fe5853bb
Andy Kwok <andy.kwok@improving.com>
2025-01-13 12:31:33 -0800

    Refactor Gradle file for GeoSpatial build target
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    Remove redundent wrapper
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    Github CI
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    Update spotless
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    Update gradle def
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>
    
    Exclude geo IT from main integ-test
    
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>

---
+++ integ-test/src/test/java/org/opensearch/sql/geo/PplIpEnrichmentIT.java
@@ -0,0 +1,49 @@
+/*
+ *
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package org.opensearch.sql.geo;
+
+import static org.opensearch.sql.legacy.TestUtils.getResponseBody;
+
+import java.io.IOException;
+import lombok.SneakyThrows;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.opensearch.client.Request;
+import org.opensearch.client.RequestOptions;
+import org.opensearch.client.Response;
+import org.opensearch.sql.ppl.PPLIntegTestCase;
+
+/** IP enrichment PPL request with OpenSearch Geo-sptial plugin */
+public class PplIpEnrichmentIT extends PPLIntegTestCase {
+
+  private static boolean initialized = false;
+
+  private static String PLUGIN_NAME = "opensearch-geospatial";
+
+  @SneakyThrows
+  @BeforeEach
+  public void initialize() {
+    if (!initialized) {
+      setUpIndices();
+      initialized = true;
+    }
+  }
+
+  @Test
+  public void testGeoPluginInstallation() throws IOException {
+
+    Request request = new Request("GET", "/_cat/plugins?v");
+    RequestOptions.Builder restOptionsBuilder = RequestOptions.DEFAULT.toBuilder();
+    restOptionsBuilder.addHeader("Content-Type", "application/json");
+    request.setOptions(restOptionsBuilder);
+    Response response = client().performRequest(request);
+    Assert.assertEquals(200, response.getStatusLine().getStatusCode());
+    Assert.assertTrue(getResponseBody(response, true).contains(PLUGIN_NAME));
+  }
+}
