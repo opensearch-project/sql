5b1f6de41bd10d00d96b2d88527360123fdce22b
Andrew Carbonetto <andrew.carbonetto@improving.com>
2025-01-15 09:12:53 -0800

    json_valid: null and missing should return false
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>

--- core/src/main/java/org/opensearch/sql/expression/json/JsonFunctions.java
+++ core/src/main/java/org/opensearch/sql/expression/json/JsonFunctions.java
@@ -16,7 +16,6 @@ import static org.opensearch.sql.data.type.ExprCoreType.STRING;
 import static org.opensearch.sql.data.type.ExprCoreType.UNDEFINED;
 import static org.opensearch.sql.expression.function.FunctionDSL.define;
 import static org.opensearch.sql.expression.function.FunctionDSL.impl;
-import static org.opensearch.sql.expression.function.FunctionDSL.nullMissingHandling;
 
 import lombok.experimental.UtilityClass;
 import org.opensearch.sql.expression.function.BuiltinFunctionName;
