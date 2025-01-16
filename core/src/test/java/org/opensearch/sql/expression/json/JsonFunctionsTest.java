e8b6df3e6ab0ce616b0ba1c9868dca19044cdfad
Andrew Carbonetto <andrew.carbonetto@improving.com>
2025-01-16 15:34:17 -0800

    Add test for missing/null
    Signed-off-by: Andrew Carbonetto <andrew.carbonetto@improving.com>

--- core/src/test/java/org/opensearch/sql/expression/json/JsonFunctionsTest.java
+++ core/src/test/java/org/opensearch/sql/expression/json/JsonFunctionsTest.java
@@ -74,6 +74,7 @@ public class JsonFunctionsTest {
             // test json scalars are valid
             "\"abc\"",
             "1234",
+            "12.34",
             "true",
             "false",
             "null",
@@ -155,9 +156,15 @@ public class JsonFunctionsTest {
 
     assertEquals(new ExprIntegerValue(1234), DSL.json_function(DSL.literal("1234")).valueOf());
 
+    assertEquals(new ExprDoubleValue(12.34), DSL.json_function(DSL.literal("12.34")).valueOf());
+
     assertEquals(LITERAL_TRUE, DSL.json_function(DSL.literal("true")).valueOf());
 
     assertEquals(LITERAL_NULL, DSL.json_function(DSL.literal("null")).valueOf());
+
+    assertEquals(LITERAL_NULL, DSL.json_function(DSL.literal(LITERAL_NULL)).valueOf());
+
+    assertEquals(LITERAL_MISSING, DSL.json_function(DSL.literal(LITERAL_MISSING)).valueOf());
 
     assertEquals(LITERAL_NULL, DSL.json_function(DSL.literal("")).valueOf());
 
