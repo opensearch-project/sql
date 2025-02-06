94a4c8a6e689dc841d60edbaed24d65467b4401d
currantw <taylor.curran@improving.com>
2025-02-06 09:53:52 -0800

    Review comments - add `testStructNestedDeep` test case
    Signed-off-by: currantw <taylor.curran@improving.com>

--- core/src/test/java/org/opensearch/sql/planner/logical/LogicalFlattenTest.java
+++ core/src/test/java/org/opensearch/sql/planner/logical/LogicalFlattenTest.java
@@ -8,6 +8,7 @@ package org.opensearch.sql.planner.logical;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.opensearch.sql.data.type.ExprCoreType.BOOLEAN;
 import static org.opensearch.sql.data.type.ExprCoreType.DOUBLE;
 import static org.opensearch.sql.data.type.ExprCoreType.INTEGER;
 import static org.opensearch.sql.data.type.ExprCoreType.STRING;
@@ -21,7 +22,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.opensearch.sql.analysis.AnalyzerTestBase;
 import org.opensearch.sql.analysis.symbol.Namespace;
 import org.opensearch.sql.ast.dsl.AstDSL;
-import org.opensearch.sql.ast.tree.Flatten;
 import org.opensearch.sql.data.type.ExprType;
 import org.opensearch.sql.exception.SemanticCheckException;
 import org.opensearch.sql.expression.DSL;
@@ -29,6 +29,7 @@ import org.opensearch.sql.expression.DSL;
 @ExtendWith(MockitoExtension.class)
 class LogicalFlattenTest extends AnalyzerTestBase {
 
+  // Test constants
   private static final String TABLE_NAME = "schema";
 
   @Override
@@ -41,8 +42,10 @@ class LogicalFlattenTest extends AnalyzerTestBase {
     mapping.put("struct", STRUCT);
     mapping.put("struct.integer", INTEGER);
     mapping.put("struct.double", DOUBLE);
-    mapping.put("struct.nested_struct", STRUCT);
-    mapping.put("struct.nested_struct.string", STRING);
+    mapping.put("struct.struct_nested", STRUCT);
+    mapping.put("struct.struct_nested.string", STRING);
+    mapping.put("struct.struct_nested.struct_nested_deep", STRUCT);
+    mapping.put("struct.struct_nested.struct_nested_deep.boolean", BOOLEAN);
 
     mapping.put("duplicate", STRUCT);
     mapping.put("duplicate.integer_value", INTEGER);
@@ -52,73 +55,90 @@ class LogicalFlattenTest extends AnalyzerTestBase {
 
   @Test
   void testStructEmpty() {
-    LogicalPlan expectedLogicalPlan =
-        LogicalPlanDSL.flatten(
-            LogicalPlanDSL.relation(TABLE_NAME, table), DSL.ref("struct_empty", STRUCT));
-    LogicalPlan actualLogicalPlan =
-        analyze(AstDSL.flatten(AstDSL.relation(TABLE_NAME), AstDSL.field("struct_empty")));
+    executeFlatten("struct_empty");
 
-    assertEquals(expectedLogicalPlan, actualLogicalPlan);
+    assertTypeDefined("struct_empty", STRUCT);
   }
 
   @Test
   void testStruct() {
-    LogicalPlan expectedLogicalPlan =
-        LogicalPlanDSL.flatten(
-            LogicalPlanDSL.relation(TABLE_NAME, table), DSL.ref("struct", STRUCT));
-    LogicalPlan actualLogicalPlan =
-        analyze(AstDSL.flatten(AstDSL.relation(TABLE_NAME), AstDSL.field("struct")));
-
-    assertEquals(expectedLogicalPlan, actualLogicalPlan);
-
-    assertTypeDefined("integer", INTEGER);
-    assertTypeDefined("double", DOUBLE);
-    assertTypeDefined("nested_struct", STRUCT);
-    assertTypeDefined("nested_struct.string", STRING);
-  }
-
-  @Test
-  void testStructNested() {
-    LogicalPlan expectedLogicalPlan =
-        LogicalPlanDSL.flatten(
-            LogicalPlanDSL.relation(TABLE_NAME, table), DSL.ref("struct.nested_struct", STRUCT));
-    LogicalPlan actualLogicalPlan =
-        analyze(AstDSL.flatten(AstDSL.relation(TABLE_NAME), AstDSL.field("struct.nested_struct")));
-
-    assertEquals(expectedLogicalPlan, actualLogicalPlan);
+    executeFlatten("struct");
 
     assertTypeDefined("struct", STRUCT);
     assertTypeDefined("struct.integer", INTEGER);
     assertTypeDefined("struct.double", DOUBLE);
+    assertTypeDefined("struct.struct_nested", STRUCT);
+    assertTypeDefined("struct.struct_nested.string", STRING);
+
+    assertTypeDefined("struct", STRUCT);
+    assertTypeDefined("integer", INTEGER);
+    assertTypeDefined("double", DOUBLE);
+    assertTypeDefined("struct_nested", STRUCT);
+    assertTypeDefined("struct_nested.string", STRING);
+  }
+
+  @Test
+  void testStructNested() {
+    executeFlatten("struct.struct_nested");
+
+    assertTypeDefined("struct", STRUCT);
+    assertTypeDefined("struct.struct_nested", STRUCT);
+    assertTypeDefined("struct.struct_nested.string", STRING);
+
     assertTypeDefined("struct.string", STRING);
   }
 
   @Test
-  void testInvalidName() {
-    Flatten actualUnresolvedPlan =
-        AstDSL.flatten(AstDSL.relation("schema"), AstDSL.field("invalid"));
+  void testStructNestedDeep() {
+    executeFlatten("struct.struct_nested.struct_nested_deep");
 
-    String msg =
-        assertThrows(SemanticCheckException.class, () -> analyze(actualUnresolvedPlan))
-            .getMessage();
-    assertEquals("can't resolve Symbol(namespace=FIELD_NAME, name=invalid) in type env", msg);
+    assertTypeDefined("struct", STRUCT);
+    assertTypeDefined("struct.struct_nested", STRUCT);
+    assertTypeDefined("struct.struct_nested.struct_nested_deep", STRUCT);
+    assertTypeDefined("struct.struct_nested.struct_nested_deep.boolean", BOOLEAN);
+
+    assertTypeDefined("struct.struct_nested.boolean", BOOLEAN);
+  }
+
+  @Test
+  void testInvalidName() {
+    Exception ex;
+
+    ex = assertThrows(SemanticCheckException.class, () -> executeFlatten("invalid"));
+    assertEquals(
+        "can't resolve Symbol(namespace=FIELD_NAME, name=invalid) in type env", ex.getMessage());
+
+    ex = assertThrows(SemanticCheckException.class, () -> executeFlatten(".invalid"));
+    assertEquals(
+        "can't resolve Symbol(namespace=FIELD_NAME, name=.struct) in type env", ex.getMessage());
+
+    ex = assertThrows(SemanticCheckException.class, () -> executeFlatten("invalid."));
+    assertEquals(
+        "can't resolve Symbol(namespace=FIELD_NAME, name=struct.) in type env", ex.getMessage());
   }
 
   @Test
   void testInvalidDuplicate() {
-    Flatten actualUnresolvedPlan =
-        AstDSL.flatten(AstDSL.relation("schema"), AstDSL.field("duplicate"));
-
     String msg =
-        assertThrows(IllegalArgumentException.class, () -> analyze(actualUnresolvedPlan))
+        assertThrows(IllegalArgumentException.class, () -> executeFlatten("duplicate"))
             .getMessage();
     assertEquals("Flatten command cannot overwrite field 'integer_value'", msg);
   }
 
   /**
-   * Asserts that the given field name is defined in the type environment and corresponds to the
-   * given type.
+   * Builds the actual and expected logical plans by flattening the field with the given name, and
+   * tests whether they are equal.
    */
+  private void executeFlatten(String fieldName) {
+    LogicalPlan expected =
+        LogicalPlanDSL.flatten(
+            LogicalPlanDSL.relation(TABLE_NAME, table), DSL.ref(fieldName, STRUCT));
+    LogicalPlan actual =
+        analyze(AstDSL.flatten(AstDSL.relation(TABLE_NAME), AstDSL.field(fieldName)));
+    assertEquals(expected, actual);
+  }
+
+  /** Asserts that the given field name is defined in the type environment with the given type. */
   private void assertTypeDefined(String fieldName, ExprType fieldType) {
     Map<String, ExprType> fieldsMap =
         analysisContext.peek().lookupAllTupleFields(Namespace.FIELD_NAME);
