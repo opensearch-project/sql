908fd1f3913cfbb591e9ae9769d877b1793ca732
qianheng <qianheng@amazon.com>
2025-02-21 10:53:19 +0800

    [Calcite Engine] Push down project and filter operator into index scan (#3327)
    * Support Filter and Project pushdown
    
    Signed-off-by: Heng Qian <qianheng@amazon.com>
    
    * Support Filter and Project pushdown v2
    
    Signed-off-by: Heng Qian <qianheng@amazon.com>
    
    * Address comments
    
    Signed-off-by: Heng Qian <qianheng@amazon.com>
    
    * Add original license for PredicateAnalyzer
    
    Signed-off-by: Heng Qian <qianheng@amazon.com>
    
    ---------
    
    Signed-off-by: Heng Qian <qianheng@amazon.com>

--- core/src/main/java/org/opensearch/sql/calcite/CalcitePlanContext.java
+++ core/src/main/java/org/opensearch/sql/calcite/CalcitePlanContext.java
@@ -7,7 +7,6 @@ package org.opensearch.sql.calcite;
 
 import java.util.function.BiFunction;
 import lombok.Getter;
-import org.apache.calcite.jdbc.CalciteConnection;
 import org.apache.calcite.rex.RexNode;
 import org.apache.calcite.tools.FrameworkConfig;
 import org.apache.calcite.tools.RelBuilder;
@@ -16,15 +15,13 @@ import org.opensearch.sql.ast.expression.UnresolvedExpression;
 public class CalcitePlanContext {
 
   public FrameworkConfig config;
-  public CalciteConnection connection;
   public final RelBuilder relBuilder;
   public final ExtendedRexBuilder rexBuilder;
 
   @Getter private boolean isResolvingJoinCondition = false;
 
-  public CalcitePlanContext(FrameworkConfig config, CalciteConnection connection) {
+  public CalcitePlanContext(FrameworkConfig config) {
     this.config = config;
-    this.connection = connection;
     this.relBuilder = RelBuilder.create(config);
     this.rexBuilder = new ExtendedRexBuilder(relBuilder.getRexBuilder());
   }
@@ -40,6 +37,6 @@ public class CalcitePlanContext {
 
   // for testing only
   public static CalcitePlanContext create(FrameworkConfig config) {
-    return new CalcitePlanContext(config, null);
+    return new CalcitePlanContext(config);
   }
 }
