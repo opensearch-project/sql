ce264dd3b8c0a467ea6dbf2c6a05ed3a5289f63a
xinyual <xinyual@amazon.com>
2025-02-24 15:13:55 +0800

    add transfer for log
    Signed-off-by: xinyual <xinyual@amazon.com>

--- integ-test/src/test/java/org/opensearch/sql/calcite/CalcitePPLBasicIT.java
+++ integ-test/src/test/java/org/opensearch/sql/calcite/CalcitePPLBasicIT.java
@@ -18,7 +18,7 @@ public class CalcitePPLBasicIT extends CalcitePPLIntegTestCase {
   public void init() throws IOException {
     super.init();
     Request request1 = new Request("PUT", "/test/_doc/1?refresh=true");
-    request1.setJsonEntity("{\"name\": \"hello\", \"age\": 20}");
+    request1.setJsonEntity("{\"name\": null, \"age\": 20}");
     client().performRequest(request1);
     Request request2 = new Request("PUT", "/test/_doc/2?refresh=true");
     request2.setJsonEntity("{\"name\": \"world\", \"age\": 30}");
@@ -1064,4 +1064,7 @@ public class CalcitePPLBasicIT extends CalcitePPLIntegTestCase {
                     + "}",
             actual);
   }
+
+
+
 }
