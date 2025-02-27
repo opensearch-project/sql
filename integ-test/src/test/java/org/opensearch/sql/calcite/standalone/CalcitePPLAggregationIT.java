c7dd3f80c17f0a79d5e1e3506eb35ba376c3b6b8
Lantao Jin <ltjin@amazon.com>
2025-02-27 10:33:50 +0800

    Make basic aggregation working (part 2) : adding tests (#3355)
    * Add more aggregation tests
    
    Signed-off-by: Lantao Jin <ltjin@amazon.com>
    
    * delete unrelavant code
    
    Signed-off-by: Lantao Jin <ltjin@amazon.com>
    
    ---------
    
    Signed-off-by: Lantao Jin <ltjin@amazon.com>

--- integ-test/src/test/java/org/opensearch/sql/calcite/standalone/CalcitePPLAggregationIT.java
+++ integ-test/src/test/java/org/opensearch/sql/calcite/standalone/CalcitePPLAggregationIT.java
@@ -159,6 +159,60 @@ public class CalcitePPLAggregationIT extends CalcitePPLIntegTestCase {
   }
 
   @Test
+  public void testMultipleAggregatesWithAliasesByClause() {
+    String actual =
+        execute(
+            String.format(
+                "source=%s | stats avg(balance) as avg, max(balance) as max, min(balance) as min,"
+                    + " count() as cnt by gender",
+                TEST_INDEX_BANK));
+    assertEquals(
+        "{\n"
+            + "  \"schema\": [\n"
+            + "    {\n"
+            + "      \"name\": \"gender\",\n"
+            + "      \"type\": \"string\"\n"
+            + "    },\n"
+            + "    {\n"
+            + "      \"name\": \"avg\",\n"
+            + "      \"type\": \"double\"\n"
+            + "    },\n"
+            + "    {\n"
+            + "      \"name\": \"max\",\n"
+            + "      \"type\": \"long\"\n"
+            + "    },\n"
+            + "    {\n"
+            + "      \"name\": \"min\",\n"
+            + "      \"type\": \"long\"\n"
+            + "    },\n"
+            + "    {\n"
+            + "      \"name\": \"cnt\",\n"
+            + "      \"type\": \"long\"\n"
+            + "    }\n"
+            + "  ],\n"
+            + "  \"datarows\": [\n"
+            + "    [\n"
+            + "      \"F\",\n"
+            + "      40488.0,\n"
+            + "      48086,\n"
+            + "      32838,\n"
+            + "      3\n"
+            + "    ],\n"
+            + "    [\n"
+            + "      \"M\",\n"
+            + "      16377.25,\n"
+            + "      39225,\n"
+            + "      4180,\n"
+            + "      4\n"
+            + "    ]\n"
+            + "  ],\n"
+            + "  \"total\": 2,\n"
+            + "  \"size\": 2\n"
+            + "}",
+        actual);
+  }
+
+  @Test
   public void testAvgByField() {
     String actual =
         execute(String.format("source=%s | stats avg(balance) by gender", TEST_INDEX_BANK));
@@ -268,8 +322,21 @@ public class CalcitePPLAggregationIT extends CalcitePPLIntegTestCase {
         actual);
   }
 
-  // TODO fallback to V2 because missing conversion LogicalAggregate[convention: NONE -> ENUMERABLE]
+  /**
+   * TODO Calcite doesn't support group by window, but it support Tumble table function. See
+   * `SqlToRelConverterTest`
+   */
   @Ignore
+  public void testAvgByTimeSpanAndFields() {
+    String actual =
+        execute(
+            String.format(
+                "source=%s | stats avg(balance) by span(birthdate, 1 day) as age_balance",
+                TEST_INDEX_BANK));
+    assertEquals("", actual);
+  }
+
+  @Test
   public void testCountDistinct() {
     String actual =
         execute(
@@ -278,26 +345,189 @@ public class CalcitePPLAggregationIT extends CalcitePPLIntegTestCase {
         "{\n"
             + "  \"schema\": [\n"
             + "    {\n"
-            + "      \"name\": \"distinct_count(state)\",\n"
-            + "      \"type\": \"integer\"\n"
-            + "    },\n"
-            + "    {\n"
             + "      \"name\": \"gender\",\n"
             + "      \"type\": \"string\"\n"
+            + "    },\n"
+            + "    {\n"
+            + "      \"name\": \"distinct_count(state)\",\n"
+            + "      \"type\": \"long\"\n"
             + "    }\n"
             + "  ],\n"
             + "  \"datarows\": [\n"
             + "    [\n"
-            + "      3,\n"
-            + "      \"f\"\n"
+            + "      \"F\",\n"
+            + "      3\n"
             + "    ],\n"
             + "    [\n"
-            + "      4,\n"
-            + "      \"m\"\n"
+            + "      \"M\",\n"
+            + "      4\n"
             + "    ]\n"
             + "  ],\n"
             + "  \"total\": 2,\n"
             + "  \"size\": 2\n"
+            + "}",
+        actual);
+  }
+
+  @Test
+  public void testCountDistinctWithAlias() {
+    String actual =
+        execute(
+            String.format(
+                "source=%s | stats distinct_count(state) as dc by gender", TEST_INDEX_BANK));
+    assertEquals(
+        "{\n"
+            + "  \"schema\": [\n"
+            + "    {\n"
+            + "      \"name\": \"gender\",\n"
+            + "      \"type\": \"string\"\n"
+            + "    },\n"
+            + "    {\n"
+            + "      \"name\": \"dc\",\n"
+            + "      \"type\": \"long\"\n"
+            + "    }\n"
+            + "  ],\n"
+            + "  \"datarows\": [\n"
+            + "    [\n"
+            + "      \"F\",\n"
+            + "      3\n"
+            + "    ],\n"
+            + "    [\n"
+            + "      \"M\",\n"
+            + "      4\n"
+            + "    ]\n"
+            + "  ],\n"
+            + "  \"total\": 2,\n"
+            + "  \"size\": 2\n"
+            + "}",
+        actual);
+  }
+
+  @Ignore
+  public void testApproxCountDistinct() {
+    String actual =
+        execute(
+            String.format(
+                "source=%s | stats distinct_count_approx(state) by gender", TEST_INDEX_BANK));
+  }
+
+  @Test
+  public void testStddevSampStddevPop() {
+    String actual =
+        execute(
+            String.format(
+                "source=%s | stats stddev_samp(balance) as ss, stddev_pop(balance) as sp by gender",
+                TEST_INDEX_BANK));
+    assertEquals(
+        "{\n"
+            + "  \"schema\": [\n"
+            + "    {\n"
+            + "      \"name\": \"gender\",\n"
+            + "      \"type\": \"string\"\n"
+            + "    },\n"
+            + "    {\n"
+            + "      \"name\": \"ss\",\n"
+            + "      \"type\": \"double\"\n"
+            + "    },\n"
+            + "    {\n"
+            + "      \"name\": \"sp\",\n"
+            + "      \"type\": \"double\"\n"
+            + "    }\n"
+            + "  ],\n"
+            + "  \"datarows\": [\n"
+            + "    [\n"
+            + "      \"F\",\n"
+            + "      7624.132999889233,\n"
+            + "      6225.078526947806\n"
+            + "    ],\n"
+            + "    [\n"
+            + "      \"M\",\n"
+            + "      16177.114233282358,\n"
+            + "      14009.791885945344\n"
+            + "    ]\n"
+            + "  ],\n"
+            + "  \"total\": 2,\n"
+            + "  \"size\": 2\n"
+            + "}",
+        actual);
+  }
+
+  @Test
+  public void testAggWithEval() {
+    String actual =
+        execute(
+            String.format(
+                "source=%s | eval a = 1, b = a | stats avg(a) as avg_a by b", TEST_INDEX_BANK));
+    assertEquals(
+        "{\n"
+            + "  \"schema\": [\n"
+            + "    {\n"
+            + "      \"name\": \"b\",\n"
+            + "      \"type\": \"integer\"\n"
+            + "    },\n"
+            + "    {\n"
+            + "      \"name\": \"avg_a\",\n"
+            + "      \"type\": \"double\"\n"
+            + "    }\n"
+            + "  ],\n"
+            + "  \"datarows\": [\n"
+            + "    [\n"
+            + "      1,\n"
+            + "      1.0\n"
+            + "    ]\n"
+            + "  ],\n"
+            + "  \"total\": 1,\n"
+            + "  \"size\": 1\n"
+            + "}",
+        actual);
+  }
+
+  @Test
+  public void testAggWithBackticksAlias() {
+    String actual =
+        execute(String.format("source=%s | stats sum(`balance`) as `sum_b`", TEST_INDEX_BANK));
+    assertEquals(
+        "{\n"
+            + "  \"schema\": [\n"
+            + "    {\n"
+            + "      \"name\": \"sum_b\",\n"
+            + "      \"type\": \"long\"\n"
+            + "    }\n"
+            + "  ],\n"
+            + "  \"datarows\": [\n"
+            + "    [\n"
+            + "      186973\n"
+            + "    ]\n"
+            + "  ],\n"
+            + "  \"total\": 1,\n"
+            + "  \"size\": 1\n"
+            + "}",
+        actual);
+  }
+
+  @Test
+  public void testSimpleTwoLevelStats() {
+    String actual =
+        execute(
+            String.format(
+                "source=%s | stats avg(balance) as avg_by_gender by gender | stats"
+                    + " avg(avg_by_gender) as avg_avg",
+                TEST_INDEX_BANK));
+    assertEquals(
+        "{\n"
+            + "  \"schema\": [\n"
+            + "    {\n"
+            + "      \"name\": \"avg_avg\",\n"
+            + "      \"type\": \"double\"\n"
+            + "    }\n"
+            + "  ],\n"
+            + "  \"datarows\": [\n"
+            + "    [\n"
+            + "      28432.625\n"
+            + "    ]\n"
+            + "  ],\n"
+            + "  \"total\": 1,\n"
+            + "  \"size\": 1\n"
             + "}",
         actual);
   }
