0b28fab61fdae37406688fce9fd2d01f4b7b3872
xinyual <xinyual@amazon.com>
2025-02-28 11:10:45 +0800

    add license and move it
    Signed-off-by: xinyual <xinyual@amazon.com>

--- core/src/main/java/org/opensearch/sql/calcite/udf/mathUDF/CRC32Function.java
+++ core/src/main/java/org/opensearch/sql/calcite/udf/mathUDF/CRC32Function.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.opensearch.sql.calcite.udf.mathUDF;
 
 import java.util.zip.CRC32;
--- core/src/main/java/org/opensearch/sql/calcite/utils/UserDefineFunctionUtils.java
+++ core/src/main/java/org/opensearch/sql/calcite/utils/UserDefineFunctionUtils.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.opensearch.sql.calcite.utils;
 
 import static org.apache.calcite.sql.type.SqlTypeUtil.createArrayType;
