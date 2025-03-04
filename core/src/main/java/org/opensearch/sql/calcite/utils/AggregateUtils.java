6668aa843572e728cbd84cb9237e40c99116fa1f
Xinyuan Lu <xinyual@amazon.com>
2025-03-04 20:07:05 +0800

    Add udf interface (#3374)
    * add udf/udaf interface and take/sqrt function
    
    Signed-off-by: xinyual <xinyual@amazon.com>
    
    * add UT
    
    Signed-off-by: xinyual <xinyual@amazon.com>
    
    * add POW, Atan, Atan2 and corresponding UT
    
    Signed-off-by: xinyual <xinyual@amazon.com>
    
    * apply spotless
    
    Signed-off-by: xinyual <xinyual@amazon.com>
    
    * fix table for join it
    
    Signed-off-by: xinyual <xinyual@amazon.com>
    
    * add java doc
    
    Signed-off-by: xinyual <xinyual@amazon.com>
    
    * apply spotless
    
    Signed-off-by: xinyual <xinyual@amazon.com>
    
    ---------
    
    Signed-off-by: xinyual <xinyual@amazon.com>

--- core/src/main/java/org/opensearch/sql/calcite/utils/AggregateUtils.java
+++ core/src/main/java/org/opensearch/sql/calcite/utils/AggregateUtils.java
@@ -5,7 +5,10 @@
 
 package org.opensearch.sql.calcite.utils;
 
+import static org.opensearch.sql.calcite.utils.UserDefineFunctionUtils.TransferUserDefinedAggFunction;
+
 import com.google.common.collect.ImmutableList;
+import java.util.List;
 import org.apache.calcite.rel.RelCollations;
 import org.apache.calcite.rel.core.AggregateCall;
 import org.apache.calcite.rex.RexInputRef;
@@ -15,12 +18,13 @@ import org.apache.calcite.sql.fun.SqlStdOperatorTable;
 import org.apache.calcite.tools.RelBuilder;
 import org.opensearch.sql.ast.expression.AggregateFunction;
 import org.opensearch.sql.calcite.CalcitePlanContext;
+import org.opensearch.sql.calcite.udf.udaf.TakeAggFunction;
 import org.opensearch.sql.expression.function.BuiltinFunctionName;
 
 public interface AggregateUtils {
 
   static RelBuilder.AggCall translate(
-      AggregateFunction agg, RexNode field, CalcitePlanContext context) {
+      AggregateFunction agg, RexNode field, CalcitePlanContext context, List<RexNode> argList) {
     if (BuiltinFunctionName.ofAggregation(agg.getFuncName()).isEmpty())
       throw new IllegalStateException("Unexpected value: " + agg.getFuncName());
 
@@ -50,6 +54,14 @@ public interface AggregateUtils {
         //            case PERCENTILE_APPROX:
         //                return
         // context.relBuilder.aggregateCall(SqlStdOperatorTable.PERCENTILE_CONT, field);
+      case TAKE:
+        return TransferUserDefinedAggFunction(
+            TakeAggFunction.class,
+            "TAKE",
+            UserDefineFunctionUtils.getReturnTypeInferenceForArray(),
+            List.of(field),
+            argList,
+            context.relBuilder);
       case PERCENTILE_APPROX:
         throw new UnsupportedOperationException("PERCENTILE_APPROX is not supported in PPL");
         //            case APPROX_COUNT_DISTINCT:
