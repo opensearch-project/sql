4329c13c8ac852f4a8bf85b3132bf0c2b33da595
Heng Qian <qianheng@amazon.com>
2025-03-13 18:24:54 +0800

    Add anonymizer for lookup
    Signed-off-by: Heng Qian <qianheng@amazon.com>

--- ppl/src/test/java/org/opensearch/sql/ppl/utils/PPLQueryDataAnonymizerTest.java
+++ ppl/src/test/java/org/opensearch/sql/ppl/utils/PPLQueryDataAnonymizerTest.java
@@ -217,6 +217,23 @@ public class PPLQueryDataAnonymizerTest {
   }
 
   @Test
+  public void testLookup() {
+    assertEquals(
+        "source=EMP | lookup DEPT DEPTNO replace LOC",
+        anonymize("source=EMP | lookup DEPT DEPTNO replace LOC"));
+    assertEquals(
+        "source=EMP | lookup DEPT DEPTNO replace LOC as JOB",
+        anonymize("source=EMP | lookup DEPT DEPTNO replace LOC as JOB"));
+    assertEquals(
+        "source=EMP | lookup DEPT DEPTNO append LOC",
+        anonymize("source=EMP | lookup DEPT DEPTNO append LOC"));
+    assertEquals(
+        "source=EMP | lookup DEPT DEPTNO append LOC as JOB",
+        anonymize("source=EMP | lookup DEPT DEPTNO append LOC as JOB"));
+    assertEquals("source=EMP | lookup DEPT DEPTNO", anonymize("source=EMP | lookup DEPT DEPTNO"));
+  }
+
+  @Test
   public void testInSubquery() {
     assertEquals(
         "source=t | where (id) in [ source=s | fields + uid ] | fields + id",
