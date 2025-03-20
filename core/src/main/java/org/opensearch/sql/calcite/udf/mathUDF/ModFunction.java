d318e4c64384f8088975ae36e16c26e4bdb3fea2
Xinyuan Lu <xinyual@amazon.com>
2025-03-20 10:55:45 +0800

    Add math udf (#3390)
    * add math udfs
    
    Signed-off-by: xinyual <xinyual@amazon.com>
    
    * add log argument
    
    Signed-off-by: xinyual <xinyual@amazon.com>
    
    * Add math function unit tests
    - Additionally implement user-defined ConvFunction
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Add integration tests for Calcite math functions
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Rename CalcitePPLMathFunctionsIT to CalcitePPLBuiltinFunctionIT
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * add license
    
    Signed-off-by: xinyual <xinyual@amazon.com>
    
    * apply spot
    
    Signed-off-by: xinyual <xinyual@amazon.com>
    
    * Update the implementation of CONV function to align with v2's behavior
    - Rename UserDefineFunctionUtils to UserDefinedFunctionUtils
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Improve code style:
    - enforce uniform parameter number check
    - comment on differences from calcite's implementation if necessary
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Simplify Calcite PPL math function unit tests
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Alter MOD and SQRT UDF to conform to documented behaviors
    - return null with invalid (zero, negative) arguments
    - return wider type for mod
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Complicate math integration tests
    - edge cases for UDF
    - combine operations or clauses
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Handle NULL return in ASIN, ACOS, SQRT and POW by convert returned Double.NaN and Float.NaN to null
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Apply spotless on math UDFs and their tests
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Remove unnecessary Double cast in SQRT UDF
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Convert returned Double.NaN and Float.NaN from math UDFs to LITERAL_NULL
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Correct math UDF integration tests
    - remove comparision between string and integers
    - correct thrown error types
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Update MOD UDF
    - add alias % to MOD
    - return negative when the dividend is negative
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Modify substring ITs
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * apply spot
    
    Signed-off-by: xinyual <xinyual@amazon.com>
    
    * Replace containsMessage with verifyErrorMessageContains in math ITs
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Correct MOD return types
    - additionally enrich math ITs with fields calculations
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * fix UT
    
    Signed-off-by: xinyual <xinyual@amazon.com>
    
    ---------
    
    Signed-off-by: xinyual <xinyual@amazon.com>
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    Co-authored-by: xinyual <xinyual@amazon.com>
    Co-authored-by: Yuanchun Shen <yuanchu@amazon.com>
    Signed-off-by: xinyual <xinyual@amazon.com>

---
+++ core/src/main/java/org/opensearch/sql/calcite/udf/mathUDF/ModFunction.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.sql.calcite.udf.mathUDF;
+
+import java.math.BigDecimal;
+import org.opensearch.sql.calcite.udf.UserDefinedFunction;
+
+/**
+ * Calculate the remainder of x divided by y<br>
+ * The supported signature of mod function is<br>
+ * (x: INTEGER/LONG/FLOAT/DOUBLE, y: INTEGER/LONG/FLOAT/DOUBLE)<br>
+ * -> wider type between types of x and y
+ */
+public class ModFunction implements UserDefinedFunction {
+
+  @Override
+  public Object eval(Object... args) {
+    if (args.length != 2) {
+      throw new IllegalArgumentException(
+          String.format("MOD function requires exactly two arguments, but got %d", args.length));
+    }
+
+    Object arg0 = args[0];
+    Object arg1 = args[1];
+    if (!(arg0 instanceof Number num0) || !(arg1 instanceof Number num1)) {
+      throw new IllegalArgumentException(
+          String.format(
+              "MOD function requires two numeric arguments, but got %s and %s",
+              arg0.getClass().getSimpleName(), arg1.getClass().getSimpleName()));
+    }
+
+    // TODO: This precision check is arbitrary.
+    if (Math.abs(num1.doubleValue()) < 0.0000001) {
+      return null;
+    }
+
+    if (isIntegral(num0) && isIntegral(num1)) {
+      long l0 = num0.longValue();
+      long l1 = num1.longValue();
+      // It returns negative values when l0 is negative
+      long result = l0 % l1;
+      // Return the wider type between l0 and l1
+      if (num0 instanceof Long || num1 instanceof Long) {
+        return result;
+      }
+      return (int) result;
+    }
+
+    BigDecimal b0 = new BigDecimal(num0.toString());
+    BigDecimal b1 = new BigDecimal(num1.toString());
+    BigDecimal result = b0.remainder(b1);
+    if (num0 instanceof Double || num1 instanceof Double) {
+      return result.doubleValue();
+    }
+    return result.floatValue();
+  }
+
+  private boolean isIntegral(Number n) {
+    return n instanceof Byte || n instanceof Short || n instanceof Integer || n instanceof Long;
+  }
+}
