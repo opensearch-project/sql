fd37cdc637db86c92f8f1fb30f5e5f29235f336f
Lantao Jin <ltjin@amazon.com>
2025-03-20 10:27:43 +0800

    fix conflicts
    Signed-off-by: Lantao Jin <ltjin@amazon.com>

--- core/src/main/java/org/opensearch/sql/calcite/CalciteRexNodeVisitor.java
+++ core/src/main/java/org/opensearch/sql/calcite/CalciteRexNodeVisitor.java
@@ -20,6 +20,7 @@ import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
 import org.apache.calcite.rex.RexBuilder;
 import org.apache.calcite.rex.RexNode;
+import org.apache.calcite.sql.SqlIntervalQualifier;
 import org.apache.calcite.sql.SqlOperator;
 import org.apache.calcite.sql.fun.SqlStdOperatorTable;
 import org.apache.calcite.sql.type.SqlTypeName;
