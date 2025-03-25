c7454315d98aa90c64cbdcc440c08554d0e50643
xinyual <xinyual@amazon.com>
2025-03-25 13:59:29 +0800

    apply spot
    Signed-off-by: xinyual <xinyual@amazon.com>

--- plugin/src/test/java/org/opensearch/sql/plugin/transport/TransportPPLQueryResponseTest.java
+++ plugin/src/test/java/org/opensearch/sql/plugin/transport/TransportPPLQueryResponseTest.java
@@ -15,7 +15,6 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
