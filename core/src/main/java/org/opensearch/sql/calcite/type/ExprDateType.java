c13b6de37d30d670a766951d6baf3b9928e158a1
Heng Qian <qianheng@amazon.com>
2025-03-26 19:29:38 +0800

    Refine code
    Signed-off-by: Heng Qian <qianheng@amazon.com>

--- core/src/main/java/org/opensearch/sql/calcite/type/ExprDateType.java
+++ core/src/main/java/org/opensearch/sql/calcite/type/ExprDateType.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.opensearch.sql.calcite.type;
 
 import static org.opensearch.sql.calcite.utils.OpenSearchTypeFactory.ExprUDT.EXPR_DATE;
