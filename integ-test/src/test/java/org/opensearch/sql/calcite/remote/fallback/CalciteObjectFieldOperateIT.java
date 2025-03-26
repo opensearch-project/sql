69f8803307b3a54dab4af52cf80dd8cb0816d04e
qianheng <qianheng@amazon.com>
2025-03-26 11:23:02 +0800

    [Calcite engine] Support nested field (#3476)
    * [Calcite engine] Support nested field
    
    Signed-off-by: Heng Qian <qianheng@amazon.com>
    
    * Fix merging
    
    Signed-off-by: Heng Qian <qianheng@amazon.com>
    
    ---------
    
    Signed-off-by: Heng Qian <qianheng@amazon.com>
    Signed-off-by: xinyual <xinyual@amazon.com>

--- integ-test/src/test/java/org/opensearch/sql/calcite/remote/fallback/CalciteObjectFieldOperateIT.java
+++ integ-test/src/test/java/org/opensearch/sql/calcite/remote/fallback/CalciteObjectFieldOperateIT.java
@@ -5,10 +5,8 @@
 
 package org.opensearch.sql.calcite.remote.fallback;
 
-import org.junit.Ignore;
 import org.opensearch.sql.ppl.ObjectFieldOperateIT;
 
-@Ignore("https://github.com/opensearch-project/sql/issues/3452")
 public class CalciteObjectFieldOperateIT extends ObjectFieldOperateIT {
   @Override
   public void init() throws Exception {
