3e1774cd09df41b1dadb091ca607ef981b52baf2
xinyual <xinyual@amazon.com>
2025-04-01 12:19:38 +0800

    add TO DO
    Signed-off-by: xinyual <xinyual@amazon.com>

--- core/src/main/java/org/opensearch/sql/calcite/udf/UserDefinedFunction.java
+++ core/src/main/java/org/opensearch/sql/calcite/udf/UserDefinedFunction.java
@@ -5,6 +5,9 @@
 
 package org.opensearch.sql.calcite.udf;
 
+/**
+ * TO DO. support init with constant arguments https://github.com/opensearch-project/sql/issues/3490
+ */
 public interface UserDefinedFunction {
   Object eval(Object... args);
 }
