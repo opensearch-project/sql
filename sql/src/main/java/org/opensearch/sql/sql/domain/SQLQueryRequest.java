25c4b4fdd10af305bf88af951349a00d577d750e
Lantao Jin <ltjin@amazon.com>
2025-04-08 17:29:27 +0800

    coverage
    Signed-off-by: Lantao Jin <ltjin@amazon.com>

--- sql/src/main/java/org/opensearch/sql/sql/domain/SQLQueryRequest.java
+++ sql/src/main/java/org/opensearch/sql/sql/domain/SQLQueryRequest.java
@@ -100,8 +100,9 @@ public class SQLQueryRequest {
 
     return (validCursor || validQuery) // It's a valid cursor or a valid query
         && isOnlySupportedFieldInPayload() // and request must contain supported fields only
-        && (isSupportedFormat()
-            || isSupportedFormatForExplain()); // and request must be a supported format
+        && (isExplainRequest()
+            ? isSupportedExplainFormat()
+            : isSupportedFormat()); // and request must be a supported format
   }
 
   private boolean isCursor() {
@@ -148,12 +149,12 @@ public class SQLQueryRequest {
     return Stream.of("csv", "jdbc", "raw").anyMatch(format::equalsIgnoreCase);
   }
 
-  private boolean isSupportedFormatForExplain() {
-    return Stream.of("jdbc", "simple", "standard", "codegen").anyMatch(format::equalsIgnoreCase);
+  private boolean isSupportedExplainFormat() {
+    return Stream.of("simple", "standard", "codegen").anyMatch(format::equalsIgnoreCase);
   }
 
   private String getFormat(Map<String, String> params) {
-    return params.getOrDefault(QUERY_PARAMS_FORMAT, "jdbc");
+    return params.getOrDefault(QUERY_PARAMS_FORMAT, isExplainRequest() ? "standard" : "jdbc");
   }
 
   private boolean shouldSanitize(Map<String, String> params) {
