e8215b85a1d8ad5c22e8e3f64a069caa540e8b58
Yuanchun Shen <yuanchu@amazon.com>
2025-04-22 18:13:43 +0800

    Implement cidrmatch UDF
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>

--- core/src/main/java/org/opensearch/sql/expression/function/PPLBuiltinOperators.java
+++ core/src/main/java/org/opensearch/sql/expression/function/PPLBuiltinOperators.java
@@ -16,11 +16,13 @@ import org.apache.calcite.rex.RexCall;
 import org.apache.calcite.sql.SqlOperator;
 import org.apache.calcite.sql.util.ReflectiveSqlOperatorTable;
 import org.apache.calcite.util.BuiltInMethod;
+import org.opensearch.sql.expression.function.udf.ip.CidrMatchFunction;
 
 /** Defines functions and operators that are implemented only by PPL */
 public class PPLBuiltinOperators extends ReflectiveSqlOperatorTable {
 
   public static final SqlOperator SPAN = new SpanFunctionImpl().toUDF("SPAN");
+  public static final SqlOperator CIDRMATCH = new CidrMatchFunction().toUDF("CIDRMATCH");
 
   /**
    * Invoking an implementor registered in {@link RexImpTable}, need to use reflection since they're
---
+++ core/src/main/java/org/opensearch/sql/expression/function/udf/ip/CidrMatchFunction.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.sql.expression.function.udf.ip;
+
+import java.util.List;
+import org.apache.calcite.adapter.enumerable.NotNullImplementor;
+import org.apache.calcite.adapter.enumerable.NullPolicy;
+import org.apache.calcite.adapter.enumerable.RexToLixTranslator;
+import org.apache.calcite.linq4j.tree.Expression;
+import org.apache.calcite.linq4j.tree.Expressions;
+import org.apache.calcite.rex.RexCall;
+import org.apache.calcite.sql.type.ReturnTypes;
+import org.apache.calcite.sql.type.SqlReturnTypeInference;
+import org.opensearch.sql.data.model.ExprIpValue;
+import org.opensearch.sql.data.model.ExprValue;
+import org.opensearch.sql.data.model.ExprValueUtils;
+import org.opensearch.sql.expression.function.ImplementorUDF;
+import org.opensearch.sql.expression.ip.IPFunctions;
+
+public class CidrMatchFunction extends ImplementorUDF {
+  public CidrMatchFunction() {
+    super(new CidrMatchImplementor(), NullPolicy.ANY);
+  }
+
+  @Override
+  public SqlReturnTypeInference getReturnTypeInference() {
+    return ReturnTypes.BOOLEAN_FORCE_NULLABLE;
+  }
+
+  public static class CidrMatchImplementor implements NotNullImplementor {
+    @Override
+    public Expression implement(
+        RexToLixTranslator translator, RexCall call, List<Expression> translatedOperands) {
+      return Expressions.call(CidrMatchImplementor.class, "cidrMatch", translatedOperands);
+    }
+
+    public static Object cidrMatch(ExprIpValue ip, String cidr) {
+      ExprValue cidrValue = ExprValueUtils.stringValue(cidr);
+      return IPFunctions.exprCidrMatch(ip, cidrValue).valueForCalcite();
+    }
+
+    public static Object cidrMatch(String ip, String cidr) {
+      ExprIpValue ipValue = (ExprIpValue) ExprValueUtils.ipValue(ip);
+      return cidrMatch(ipValue, cidr);
+    }
+  }
+}
