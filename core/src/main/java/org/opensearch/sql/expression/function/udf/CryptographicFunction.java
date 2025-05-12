9f8f3ad320ec4839a5a704a38b85c213c2ee00ca
Yuanchun Shen <yuanchu@amazon.com>
2025-05-13 00:34:37 +0800

    Implement cryptographic hash UDFs (#3574)
    * Implement cryptographic functions
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Implement cryptographic UDFs
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Wrap message digests with ThreadLocal to enforce thread safety
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Create documentation for cryptographic hash PPL functions
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Use calcite implementations of md5 and sha1
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Test field values in cryptographic UDFs
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Refactor: update sha2 implementations with DigestUtils
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    ---------
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    Signed-off-by: xinyual <xinyual@amazon.com>

---
+++ core/src/main/java/org/opensearch/sql/expression/function/udf/CryptographicFunction.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.sql.expression.function.udf;
+
+import java.util.List;
+import org.apache.calcite.adapter.enumerable.NotNullImplementor;
+import org.apache.calcite.adapter.enumerable.NullPolicy;
+import org.apache.calcite.adapter.enumerable.RexToLixTranslator;
+import org.apache.calcite.linq4j.tree.Expression;
+import org.apache.calcite.linq4j.tree.Expressions;
+import org.apache.calcite.rex.RexCall;
+import org.apache.calcite.sql.type.ReturnTypes;
+import org.apache.calcite.sql.type.SqlReturnTypeInference;
+import org.apache.calcite.sql.type.SqlTypeTransforms;
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.codec.digest.MessageDigestAlgorithms;
+import org.opensearch.sql.expression.function.ImplementorUDF;
+
+public class CryptographicFunction extends ImplementorUDF {
+  private CryptographicFunction(NotNullImplementor implementor, NullPolicy nullPolicy) {
+    super(implementor, nullPolicy);
+  }
+
+  public static CryptographicFunction sha2() {
+    return new CryptographicFunction(new Sha2Implementor(), NullPolicy.ANY);
+  }
+
+  @Override
+  public SqlReturnTypeInference getReturnTypeInference() {
+    return ReturnTypes.VARCHAR.andThen(SqlTypeTransforms.FORCE_NULLABLE);
+  }
+
+  public static class Sha2Implementor implements NotNullImplementor {
+    @Override
+    public Expression implement(
+        RexToLixTranslator translator, RexCall call, List<Expression> translatedOperands) {
+      return Expressions.call(Sha2Implementor.class, "getDigest", translatedOperands);
+    }
+
+    public static String getDigest(String input, int algorithm) {
+      return switch (algorithm) {
+        case 224 -> Hex.encodeHexString(
+            DigestUtils.getDigest(MessageDigestAlgorithms.SHA_224).digest(input.getBytes()));
+        case 256 -> DigestUtils.sha256Hex(input);
+        case 384 -> DigestUtils.sha384Hex(input);
+        case 512 -> DigestUtils.sha512Hex(input);
+        default -> throw new IllegalArgumentException(
+            String.format(
+                "Unsupported SHA2 algorithm: %d. Only 224, 256, 384, and 512 are supported.",
+                algorithm));
+      };
+    }
+  }
+}
