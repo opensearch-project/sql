bf19c6e3a1f6cf45182a14dc2db4405f7c503884
Yuanchun Shen <yuanchu@amazon.com>
2025-05-19 13:46:47 +0800

    Refactor: create a isLogicalSortLimit method to enhance readability
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>

--- opensearch/src/main/java/org/opensearch/sql/opensearch/planner/physical/OpenSearchLimitIndexScanRule.java
+++ opensearch/src/main/java/org/opensearch/sql/opensearch/planner/physical/OpenSearchLimitIndexScanRule.java
@@ -30,10 +30,7 @@ public class OpenSearchLimitIndexScanRule extends RelRule<OpenSearchLimitIndexSc
     final LogicalSort sort = call.rel(0);
     final CalciteLogicalIndexScan scan = call.rel(1);
 
-    // The LogicalSort is a LIMIT that should be pushed down when its fetch field is not null and
-    // its collation is empty.
-    // For example: `sort name | head 5` should not be pushed down because it has a field collation.
-    if (sort.fetch != null && sort.getCollation().getFieldCollations().isEmpty()) {
+    if (isLogicalSortLimit(sort)) {
       Integer limitValue = extractLimitValue(sort.fetch);
       Integer offsetValue = extractOffsetValue(sort.offset);
       if (limitValue != null && offsetValue != null) {
@@ -45,6 +42,18 @@ public class OpenSearchLimitIndexScanRule extends RelRule<OpenSearchLimitIndexSc
     }
   }
 
+  /**
+   * The LogicalSort is a LIMIT that should be pushed down when its fetch field is not null and its
+   * collation is empty. For example: <code>sort name | head 5</code> should not be pushed down
+   * because it has a field collation.
+   *
+   * @param sort The LogicalSort to check.
+   * @return True if the LogicalSort is a LIMIT that can be pushed down, false otherwise.
+   */
+  private static boolean isLogicalSortLimit(LogicalSort sort) {
+    return sort.fetch != null && sort.getCollation().getFieldCollations().isEmpty();
+  }
+
   private static Integer extractLimitValue(RexNode fetch) {
     if (fetch instanceof RexLiteral) {
       return ((RexLiteral) fetch).getValueAs(Integer.class);
@@ -53,8 +62,8 @@ public class OpenSearchLimitIndexScanRule extends RelRule<OpenSearchLimitIndexSc
   }
 
   /**
-   * Extracts the offset value from the given `RexNode`. If the offset is `null`, it defaults to 0.
-   * For example:
+   * Extracts the offset value from the given <code>RexNode</code>. If the offset is <code>null
+   * </code>, it defaults to 0. For example:
    *
    * <ul>
    *   <li><code>source=people | head 1</code> will have a <code>null</code> offset, which is
@@ -62,8 +71,8 @@ public class OpenSearchLimitIndexScanRule extends RelRule<OpenSearchLimitIndexSc
    *   <li><code>source=people | head 1 from 2</code> will have an offset of 2.
    * </ul>
    *
-   * @param offset The `RexNode` representing the offset.
-   * @return The extracted offset value, or `null` if it cannot be determined.
+   * @param offset The <code>RexNode</code> representing the offset.
+   * @return The extracted offset value, or <code>null</code> if it cannot be determined.
    */
   private static Integer extractOffsetValue(RexNode offset) {
     if (Objects.isNull(offset)) {
