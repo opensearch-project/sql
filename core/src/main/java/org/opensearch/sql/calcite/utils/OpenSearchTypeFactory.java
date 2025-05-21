eecf32cf4f780cdd81dcf18c92b73990aec7a061
Lantao Jin <ltjin@amazon.com>
2025-05-21 09:52:12 +0800

    Support `describe` command with Calcite (#3624)
    * Support describe command with Calcite
    
    Signed-off-by: Lantao Jin <ltjin@amazon.com>
    
    * add java doc and class rename
    
    Signed-off-by: Lantao Jin <ltjin@amazon.com>
    
    * refactor to remove the useless Queryable interface
    
    Signed-off-by: Lantao Jin <ltjin@amazon.com>
    
    ---------
    
    Signed-off-by: Lantao Jin <ltjin@amazon.com>
    Signed-off-by: xinyual <xinyual@amazon.com>

--- core/src/main/java/org/opensearch/sql/calcite/utils/OpenSearchTypeFactory.java
+++ core/src/main/java/org/opensearch/sql/calcite/utils/OpenSearchTypeFactory.java
@@ -32,6 +32,7 @@ import static org.opensearch.sql.lang.PPLLangSpec.PPL_SPEC;
 import java.lang.reflect.Type;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -270,7 +271,7 @@ public class OpenSearchTypeFactory extends JavaTypeFactoryImpl {
   public static RelDataType convertSchema(Table table) {
     List<String> fieldNameList = new ArrayList<>();
     List<RelDataType> typeList = new ArrayList<>();
-    Map<String, ExprType> fieldTypes = table.getFieldTypes();
+    Map<String, ExprType> fieldTypes = new LinkedHashMap<>(table.getFieldTypes());
     fieldTypes.putAll(table.getReservedFieldTypes());
     for (Entry<String, ExprType> entry : fieldTypes.entrySet()) {
       fieldNameList.add(entry.getKey());
