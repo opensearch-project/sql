abcaaaab3b36a8a9bcf350ff4751298706404598
Peng Huo <penghuo@gmail.com>
2025-05-27 18:19:09 -0700

    Support match_only_text field type (#3663)
    * Support match_only_text field type
    
    Signed-off-by: Peng Huo <penghuo@gmail.com>
    
    * Update doc
    
    Signed-off-by: Peng Huo <penghuo@gmail.com>
    
    * Update code format
    
    Signed-off-by: Peng Huo <penghuo@gmail.com>
    
    * Update
    
    Signed-off-by: Peng Huo <penghuo@gmail.com>
    
    * add plugins.calcite.fallback.allowed : false
    
    Signed-off-by: Peng Huo <penghuo@gmail.com>
    
    ---------
    
    Signed-off-by: Peng Huo <penghuo@gmail.com>
    Signed-off-by: xinyual <xinyual@amazon.com>

--- opensearch/src/main/java/org/opensearch/sql/opensearch/data/type/OpenSearchDataType.java
+++ opensearch/src/main/java/org/opensearch/sql/opensearch/data/type/OpenSearchDataType.java
@@ -25,6 +25,7 @@ public class OpenSearchDataType implements ExprType, Serializable {
   public enum MappingType {
     Invalid(null, ExprCoreType.UNKNOWN),
     Text("text", ExprCoreType.UNKNOWN),
+    MatchOnlyText("match_only_text", ExprCoreType.UNKNOWN),
     Keyword("keyword", ExprCoreType.STRING),
     Ip("ip", ExprCoreType.IP),
     GeoPoint("geo_point", ExprCoreType.UNKNOWN),
@@ -172,6 +173,7 @@ public class OpenSearchDataType implements ExprType, Serializable {
         OpenSearchDataType objectDataType = res.cloneEmpty();
         objectDataType.properties = properties;
         return objectDataType;
+      case MatchOnlyText:
       case Text:
         // TODO update these 2 below #1038 https://github.com/opensearch-project/sql/issues/1038
         Map<String, OpenSearchDataType> fields =
