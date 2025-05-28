d36e42794f4f8ed155553c8349f4105418da16e4
Lantao Jin <ltjin@amazon.com>
2025-05-28 08:00:46 +0800

    Support `top`, `rare` commands with Calcite (#3647)
    * Support top, rare commands with Calcite
    
    Signed-off-by: Lantao Jin <ltjin@amazon.com>
    
    * remove useless code
    
    Signed-off-by: Lantao Jin <ltjin@amazon.com>
    
    * address comments
    
    Signed-off-by: Lantao Jin <ltjin@amazon.com>
    
    ---------
    
    Signed-off-by: Lantao Jin <ltjin@amazon.com>
    Signed-off-by: xinyual <xinyual@amazon.com>

--- integ-test/src/test/java/org/opensearch/sql/ppl/TopCommandIT.java
+++ integ-test/src/test/java/org/opensearch/sql/ppl/TopCommandIT.java
@@ -7,7 +7,9 @@ package org.opensearch.sql.ppl;
 
 import static org.opensearch.sql.legacy.TestsConstants.TEST_INDEX_ACCOUNT;
 import static org.opensearch.sql.util.MatcherUtils.rows;
+import static org.opensearch.sql.util.MatcherUtils.schema;
 import static org.opensearch.sql.util.MatcherUtils.verifyDataRows;
+import static org.opensearch.sql.util.MatcherUtils.verifySchemaInOrder;
 
 import java.io.IOException;
 import org.json.JSONObject;
@@ -25,19 +27,35 @@ public class TopCommandIT extends PPLIntegTestCase {
   @Test
   public void testTopWithoutGroup() throws IOException {
     JSONObject result = executeQuery(String.format("source=%s | top gender", TEST_INDEX_ACCOUNT));
-    verifyDataRows(result, rows("M"), rows("F"));
+    if (isCalciteEnabled()) {
+      verifySchemaInOrder(result, schema("gender", "string"), schema("count", "bigint"));
+      verifyDataRows(result, rows("M", 507), rows("F", 493));
+    } else {
+      verifyDataRows(result, rows("M"), rows("F"));
+    }
   }
 
   @Test
   public void testTopNWithoutGroup() throws IOException {
     JSONObject result = executeQuery(String.format("source=%s | top 1 gender", TEST_INDEX_ACCOUNT));
-    verifyDataRows(result, rows("M"));
+    if (isCalciteEnabled()) {
+      verifySchemaInOrder(result, schema("gender", "string"), schema("count", "bigint"));
+      verifyDataRows(result, rows("M", 507));
+    } else {
+      verifyDataRows(result, rows("M"));
+    }
   }
 
   @Test
   public void testTopNWithGroup() throws IOException {
     JSONObject result =
         executeQuery(String.format("source=%s | top 1 state by gender", TEST_INDEX_ACCOUNT));
-    verifyDataRows(result, rows("F", "TX"), rows("M", "MD"));
+    if (isCalciteEnabled()) {
+      verifySchemaInOrder(
+          result, schema("gender", "string"), schema("state", "string"), schema("count", "bigint"));
+      verifyDataRows(result, rows("F", "TX", 17), rows("M", "MD", 18));
+    } else {
+      verifyDataRows(result, rows("F", "TX"), rows("M", "MD"));
+    }
   }
 }
