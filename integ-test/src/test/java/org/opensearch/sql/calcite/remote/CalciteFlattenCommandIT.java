5e2f1f1a78bb3f3eb79241fd215765ac7ccacde3
Yuanchun Shen <yuanchu@amazon.com>
2025-06-08 14:19:47 +0800

    Update flatten IT
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>

--- integ-test/src/test/java/org/opensearch/sql/calcite/remote/CalciteFlattenCommandIT.java
+++ integ-test/src/test/java/org/opensearch/sql/calcite/remote/CalciteFlattenCommandIT.java
@@ -9,10 +9,12 @@ package org.opensearch.sql.calcite.remote;
 
 import static org.opensearch.sql.legacy.SQLIntegTestCase.Index.NESTED_WITHOUT_ARRAYS;
 import static org.opensearch.sql.legacy.TestsConstants.TEST_INDEX_NESTED_TYPE_WITHOUT_ARRAYS;
+import static org.opensearch.sql.util.MatcherUtils.rows;
 import static org.opensearch.sql.util.MatcherUtils.schema;
-import static org.opensearch.sql.util.MatcherUtils.verifyNumOfRows;
+import static org.opensearch.sql.util.MatcherUtils.verifyDataRows;
 import static org.opensearch.sql.util.MatcherUtils.verifySchema;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.opensearch.sql.ppl.PPLIntegTestCase;
@@ -27,19 +29,67 @@ public class CalciteFlattenCommandIT extends PPLIntegTestCase {
   }
 
   @Test
-  public void testFlattenStruct() throws Exception {
+  public void testFlattenNestedStruct() throws Exception {
     JSONObject result =
         executeQuery(
             String.format("source=%s | flatten message", TEST_INDEX_NESTED_TYPE_WITHOUT_ARRAYS));
     verifySchema(
         result,
-        schema("message", "struct"),
-        schema("info", "string"),
-        schema("author", "string"),
-        schema("dayOfWeek", "integer"),
+        // Nested fields are retrieved as array of nested structs
         schema("comment", "array"),
-        schema("myNum", "integer"),
-        schema("someField", "string"));
-    verifyNumOfRows(result, 5);
+        schema("myNum", "bigint"),
+        schema("someField", "string"),
+        // Nested fields are retrieved as array of nested structs
+        schema("message", "array"),
+        schema("author", "string"),
+        schema("dayOfWeek", "bigint"),
+        schema("info", "string"));
+    verifyDataRows(
+        result,
+        rows(
+            new JSONArray().put(new JSONObject().put("data", "ab").put("likes", 3)),
+            1,
+            "b",
+            new JSONArray()
+                .put(new JSONObject().put("info", "a").put("author", "e").put("dayOfWeek", 1)),
+            "e",
+            1,
+            "a"),
+        rows(
+            new JSONArray().put(new JSONObject().put("data", "aa").put("likes", 2)),
+            2,
+            "a",
+            new JSONArray()
+                .put(new JSONObject().put("info", "b").put("author", "f").put("dayOfWeek", 2)),
+            "f",
+            2,
+            "b"),
+        rows(
+            new JSONArray().put(new JSONObject().put("data", "aa").put("likes", 3)),
+            3,
+            "a",
+            new JSONArray()
+                .put(new JSONObject().put("info", "c").put("author", "g").put("dayOfWeek", 1)),
+            "g",
+            1,
+            "c"),
+        rows(
+            new JSONArray().put(new JSONObject().put("data", "ab").put("likes", 1)),
+            4,
+            "b",
+            new JSONArray()
+                .put(new JSONObject().put("info", "c").put("author", "h").put("dayOfWeek", 4)),
+            "h",
+            4,
+            "c"),
+        rows(
+            new JSONArray().put(new JSONObject().put("data", "bb").put("likes", 10)),
+            3,
+            "a",
+            new JSONArray()
+                .put(new JSONObject().put("info", "zz").put("author", "zz").put("dayOfWeek", 6)),
+            "zz",
+            6,
+            "zz"));
   }
 }
