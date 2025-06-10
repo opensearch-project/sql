98fbb12c4801ca7b42205fc80b2fbda74d631661
Yuanchun Shen <yuanchu@amazon.com>
2025-06-10 09:00:55 +0800

    Make expand empty array IT more specific
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>

--- integ-test/src/test/java/org/opensearch/sql/calcite/remote/CalciteExpandCommandIT.java
+++ integ-test/src/test/java/org/opensearch/sql/calcite/remote/CalciteExpandCommandIT.java
@@ -287,113 +287,20 @@ public class CalciteExpandCommandIT extends PPLIntegTestCase {
     client().performRequest(insertRequest);
 
     JSONObject response =
-        executeQuery(String.format("source=%s | expand address", TEST_INDEX_NESTED_SIMPLE));
+        executeQuery(
+            String.format(
+                "source=%s | where name='ben' | expand address", TEST_INDEX_NESTED_SIMPLE));
     verifySchema(
         response,
         schema("name", "string"),
         schema("age", "bigint"),
         schema("id", "bigint"),
-        schema("address", "struct"));
-    verifyDataRows(
-        response,
-        rows(
-            "abbas",
-            null,
-            24,
-            new JSONObject()
-                .put("city", "New york city")
-                .put("state", "NY")
-                .put("moveInDate", new JSONObject().put("dateAndTime", "1984-04-12 09:07:42"))),
-        rows(
-            "abbas",
-            null,
-            24,
-            new JSONObject()
-                .put("city", "bellevue")
-                .put("state", "WA")
-                .put(
-                    "moveInDate",
-                    new JSONArray()
-                        .put(new JSONObject().put("dateAndTime", "2023-05-03 08:07:42"))
-                        .put(new JSONObject().put("dateAndTime", "2001-11-11 04:07:44")))),
-        rows(
-            "abbas",
-            null,
-            24,
-            new JSONObject()
-                .put("city", "seattle")
-                .put("state", "WA")
-                .put("moveInDate", new JSONObject().put("dateAndTime", "1966-03-19 03:04:55"))),
-        rows(
-            "abbas",
-            null,
-            24,
-            new JSONObject()
-                .put("city", "chicago")
-                .put("state", "IL")
-                .put("moveInDate", new JSONObject().put("dateAndTime", "2011-06-01 01:01:42"))),
-        rows(
-            "chen",
-            null,
-            32,
-            new JSONObject()
-                .put("city", "Miami")
-                .put("state", "Florida")
-                .put("moveInDate", new JSONObject().put("dateAndTime", "1901-08-11 04:03:33"))),
-        rows(
-            "chen",
-            null,
-            32,
-            new JSONObject()
-                .put("city", "los angeles")
-                .put("state", "CA")
-                .put("moveInDate", new JSONObject().put("dateAndTime", "2023-05-03 08:07:42"))),
-        rows(
-            "peng",
-            null,
-            26,
-            new JSONObject()
-                .put("city", "san diego")
-                .put("state", "CA")
-                .put("moveInDate", new JSONObject().put("dateAndTime", "2001-11-11 04:07:44"))),
-        rows(
-            "peng",
-            null,
-            26,
-            new JSONObject()
-                .put("city", "austin")
-                .put("state", "TX")
-                .put("moveInDate", new JSONObject().put("dateAndTime", "1977-07-13 09:04:41"))),
-        rows(
-            "andy",
-            4,
-            19,
-            new JSONObject()
-                .put("city", "houston")
-                .put("state", "TX")
-                .put("moveInDate", new JSONObject().put("dateAndTime", "1933-12-12 05:05:45"))),
-        rows(
-            "david",
-            null,
-            25,
-            new JSONObject()
-                .put("city", "raleigh")
-                .put("state", "NC")
-                .put("moveInDate", new JSONObject().put("dateAndTime", "1909-06-17 01:04:21"))),
-        rows(
-            "david",
-            null,
-            25,
-            new JSONObject()
-                .put("city", "charlotte")
-                .put("state", "SC")
-                .put(
-                    "moveInDate",
-                    new JSONArray()
-                        .put(new JSONObject().put("dateAndTime", "2001-11-11 04:07:44")))),
-        rows("ben", 437821, 47, null));
+        // The type is inferred at runtime. When the array is empty and is the
+        // first element of the column, it is set to "undefined".
+        schema("address", "undefined"));
+    verifyDataRows(response, rows("ben", 437821, 47, null));
 
-    verifyNumOfRows(response, 12);
+    verifyNumOfRows(response, 1);
     Request deleteRequest =
         new Request(
             "DELETE", String.format("/%s/_doc/%d?refresh=true", TEST_INDEX_NESTED_SIMPLE, docId));
