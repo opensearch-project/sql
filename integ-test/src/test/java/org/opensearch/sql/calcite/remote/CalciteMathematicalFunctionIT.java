1379669ca384675f1c7f5dfa3f436ec87ad8b8b1
Xinyu Hao <75524174+ishaoxy@users.noreply.github.com>
2025-06-11 08:48:16 +0800

    Fix: correct ATAN(x, y) and CONV(x, a, b) functions bug (#3748)
    * Fix: correct ATAN(x, y) and CONV(x, a, b) functions bug
    
    Signed-off-by: Hao <haoxinyu@842f57420e85.ant.amazon.com>
    Signed-off-by: Xinyu Hao <haoxinyu@amazon.com>
    
    * Fix: correct ATAN(x, y) and CONV(x, a, b) functions bug
    
    Signed-off-by: Xinyu Hao <haoxinyu@amazon.com>
    
    * Fix: correct atan convolution bug and enable tests
    
    Signed-off-by: Xinyu Hao <haoxinyu@amazon.com>
    
    * Fix: correct ATAN(x, y) and CONV(x, a, b) functions bug and enable tests
    
    Signed-off-by: Xinyu Hao <haoxinyu@amazon.com>
    
    ---------
    
    Signed-off-by: Hao <haoxinyu@842f57420e85.ant.amazon.com>
    Signed-off-by: Xinyu Hao <haoxinyu@amazon.com>
    Signed-off-by: xinyual <xinyual@amazon.com>

--- integ-test/src/test/java/org/opensearch/sql/calcite/remote/CalciteMathematicalFunctionIT.java
+++ integ-test/src/test/java/org/opensearch/sql/calcite/remote/CalciteMathematicalFunctionIT.java
@@ -5,8 +5,6 @@
 
 package org.opensearch.sql.calcite.remote;
 
-import java.io.IOException;
-import org.junit.Ignore;
 import org.opensearch.sql.ppl.MathematicalFunctionIT;
 
 public class CalciteMathematicalFunctionIT extends MathematicalFunctionIT {
@@ -15,17 +13,5 @@ public class CalciteMathematicalFunctionIT extends MathematicalFunctionIT {
     super.init();
     enableCalcite();
     disallowCalciteFallback();
-  }
-
-  @Override
-  @Ignore("https://github.com/opensearch-project/sql/issues/3672")
-  public void testAtan() throws IOException {
-    super.testAtan();
-  }
-
-  @Override
-  @Ignore("https://github.com/opensearch-project/sql/issues/3672")
-  public void testConv() throws IOException {
-    super.testConv();
   }
 }
