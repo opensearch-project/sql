47a4135c243f66e2339ec1a56826e9b9bb7efacc
Yuanchun Shen <yuanchu@amazon.com>
2025-06-17 17:29:23 +0800

    Add explain ITs with Calcite wihtout pushdown
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>

--- integ-test/src/test/java/org/opensearch/sql/ppl/ExplainIT.java
+++ integ-test/src/test/java/org/opensearch/sql/ppl/ExplainIT.java
@@ -23,10 +23,7 @@ public class ExplainIT extends PPLIntegTestCase {
 
   @Test
   public void testExplain() throws IOException {
-    String expected =
-        isCalciteEnabled()
-            ? loadFromFile("expectedOutput/calcite/explain_output.json")
-            : loadFromFile("expectedOutput/ppl/explain_output.json");
+    String expected = loadExpectedPlan("explain_output.json");
     assertJsonEqualsIgnoreId(
         expected,
         explainQueryToString(
@@ -42,10 +39,7 @@ public class ExplainIT extends PPLIntegTestCase {
 
   @Test
   public void testFilterPushDownExplain() throws IOException {
-    String expected =
-        isCalciteEnabled()
-            ? loadFromFile("expectedOutput/calcite/explain_filter_push.json")
-            : loadFromFile("expectedOutput/ppl/explain_filter_push.json");
+    String expected = loadExpectedPlan("explain_filter_push.json");
 
     assertJsonEqualsIgnoreId(
         expected,
@@ -59,10 +53,7 @@ public class ExplainIT extends PPLIntegTestCase {
 
   @Test
   public void testFilterAndAggPushDownExplain() throws IOException {
-    String expected =
-        isCalciteEnabled()
-            ? loadFromFile("expectedOutput/calcite/explain_filter_agg_push.json")
-            : loadFromFile("expectedOutput/ppl/explain_filter_agg_push.json");
+    String expected = loadExpectedPlan("explain_filter_agg_push.json");
 
     assertJsonEqualsIgnoreId(
         expected,
@@ -75,10 +66,7 @@ public class ExplainIT extends PPLIntegTestCase {
   @Test
   public void testSortPushDownExplain() throws IOException {
     // TODO fix after https://github.com/opensearch-project/sql/issues/3380
-    String expected =
-        isCalciteEnabled()
-            ? loadFromFile("expectedOutput/calcite/explain_sort_push.json")
-            : loadFromFile("expectedOutput/ppl/explain_sort_push.json");
+    String expected = loadExpectedPlan("explain_sort_push.json");
 
     assertJsonEqualsIgnoreId(
         expected,
@@ -91,10 +79,7 @@ public class ExplainIT extends PPLIntegTestCase {
 
   @Test
   public void testLimitPushDownExplain() throws IOException {
-    String expected =
-        isCalciteEnabled()
-            ? loadFromFile("expectedOutput/calcite/explain_limit_push.json")
-            : loadFromFile("expectedOutput/ppl/explain_limit_push.json");
+    String expected = loadExpectedPlan("explain_limit_push.json");
 
     assertJsonEqualsIgnoreId(
         expected,
@@ -107,10 +92,7 @@ public class ExplainIT extends PPLIntegTestCase {
 
   @Test
   public void testLimitWithFilterPushdownExplain() throws IOException {
-    String expectedFilterThenLimit =
-        isCalciteEnabled()
-            ? loadFromFile("expectedOutput/calcite/explain_filter_then_limit_push.json")
-            : loadFromFile("expectedOutput/ppl/explain_filter_then_limit_push.json");
+    String expectedFilterThenLimit = loadExpectedPlan("explain_filter_then_limit_push.json");
     assertJsonEqualsIgnoreId(
         expectedFilterThenLimit,
         explainQueryToString(
@@ -121,10 +103,7 @@ public class ExplainIT extends PPLIntegTestCase {
 
     // The filter in limit-then-filter queries should not be pushed since the current DSL will
     // execute it as filter-then-limit
-    String expectedLimitThenFilter =
-        isCalciteEnabled()
-            ? loadFromFile("expectedOutput/calcite/explain_limit_then_filter_push.json")
-            : loadFromFile("expectedOutput/ppl/explain_limit_then_filter_push.json");
+    String expectedLimitThenFilter = loadExpectedPlan("explain_limit_then_filter_push.json");
     assertJsonEqualsIgnoreId(
         expectedLimitThenFilter,
         explainQueryToString(
@@ -136,10 +115,7 @@ public class ExplainIT extends PPLIntegTestCase {
 
   @Test
   public void testMultipleLimitExplain() throws IOException {
-    String expected5Then10 =
-        isCalciteEnabled()
-            ? loadFromFile("expectedOutput/calcite/explain_limit_5_10_push.json")
-            : loadFromFile("expectedOutput/ppl/explain_limit_5_10_push.json");
+    String expected5Then10 = loadExpectedPlan("explain_limit_5_10_push.json");
     assertJsonEqualsIgnoreId(
         expected5Then10,
         explainQueryToString(
@@ -148,10 +124,7 @@ public class ExplainIT extends PPLIntegTestCase {
                 + "| head 10 "
                 + "| fields age"));
 
-    String expected10Then5 =
-        isCalciteEnabled()
-            ? loadFromFile("expectedOutput/calcite/explain_limit_10_5_push.json")
-            : loadFromFile("expectedOutput/ppl/explain_limit_10_5_push.json");
+    String expected10Then5 = loadExpectedPlan("explain_limit_10_5_push.json");
     assertJsonEqualsIgnoreId(
         expected10Then5,
         explainQueryToString(
@@ -160,10 +133,7 @@ public class ExplainIT extends PPLIntegTestCase {
                 + "| head 5 "
                 + "| fields age"));
 
-    String expected10from1then10from2 =
-        isCalciteEnabled()
-            ? loadFromFile("expectedOutput/calcite/explain_limit_10from1_10from2_push.json")
-            : loadFromFile("expectedOutput/ppl/explain_limit_10from1_10from2_push.json");
+    String expected10from1then10from2 = loadExpectedPlan("explain_limit_10from1_10from2_push.json");
     assertJsonEqualsIgnoreId(
         expected10from1then10from2,
         explainQueryToString(
@@ -173,10 +143,7 @@ public class ExplainIT extends PPLIntegTestCase {
                 + "| fields age"));
 
     // The second limit should not be pushed down for limit-filter-limit queries
-    String expected10ThenFilterThen5 =
-        isCalciteEnabled()
-            ? loadFromFile("expectedOutput/calcite/explain_limit_10_filter_5_push.json")
-            : loadFromFile("expectedOutput/ppl/explain_limit_10_filter_5_push.json");
+    String expected10ThenFilterThen5 = loadExpectedPlan("explain_limit_10_filter_5_push.json");
     assertJsonEqualsIgnoreId(
         expected10ThenFilterThen5,
         explainQueryToString(
@@ -189,10 +156,7 @@ public class ExplainIT extends PPLIntegTestCase {
 
   @Test
   public void testLimitWithMultipleOffsetPushdownExplain() throws IOException {
-    String expected =
-        isCalciteEnabled()
-            ? loadFromFile("expectedOutput/calcite/explain_limit_offsets_push.json")
-            : loadFromFile("expectedOutput/ppl/explain_limit_offsets_push.json");
+    String expected = loadExpectedPlan("explain_limit_offsets_push.json");
 
     assertJsonEqualsIgnoreId(
         expected,
@@ -205,10 +169,7 @@ public class ExplainIT extends PPLIntegTestCase {
 
   @Test
   public void testFillNullPushDownExplain() throws IOException {
-    String expected =
-        isCalciteEnabled()
-            ? loadFromFile("expectedOutput/calcite/explain_fillnull_push.json")
-            : loadFromFile("expectedOutput/ppl/explain_fillnull_push.json");
+    String expected = loadExpectedPlan("explain_fillnull_push.json");
 
     assertJsonEqualsIgnoreId(
         expected,
@@ -219,10 +180,7 @@ public class ExplainIT extends PPLIntegTestCase {
 
   @Test
   public void testTrendlinePushDownExplain() throws IOException {
-    String expected =
-        isCalciteEnabled()
-            ? loadFromFile("expectedOutput/calcite/explain_trendline_push.json")
-            : loadFromFile("expectedOutput/ppl/explain_trendline_push.json");
+    String expected = loadExpectedPlan("explain_trendline_push.json");
 
     assertJsonEqualsIgnoreId(
         expected,
@@ -235,10 +193,7 @@ public class ExplainIT extends PPLIntegTestCase {
 
   @Test
   public void testTrendlineWithSortPushDownExplain() throws IOException {
-    String expected =
-        isCalciteEnabled()
-            ? loadFromFile("expectedOutput/calcite/explain_trendline_sort_push.json")
-            : loadFromFile("expectedOutput/ppl/explain_trendline_sort_push.json");
+    String expected = loadExpectedPlan("explain_trendline_sort_push.json");
 
     assertJsonEqualsIgnoreId(
         expected,
@@ -264,10 +219,7 @@ public class ExplainIT extends PPLIntegTestCase {
   @Test
   public void testPatternsSimplePatternMethodWithoutAggExplain() throws IOException {
     // TODO: Correct calcite expected result once pushdown is supported
-    String expected =
-        isCalciteEnabled()
-            ? loadFromFile("expectedOutput/calcite/explain_patterns_simple_pattern.json")
-            : loadFromFile("expectedOutput/ppl/explain_patterns_simple_pattern.json");
+    String expected = loadExpectedPlan("explain_patterns_simple_pattern.json");
 
     assertJsonEqualsIgnoreId(
         expected,
@@ -277,10 +229,7 @@ public class ExplainIT extends PPLIntegTestCase {
   @Test
   public void testPatternsSimplePatternMethodWithAggPushDownExplain() throws IOException {
     // TODO: Correct calcite expected result once pushdown is supported
-    String expected =
-        isCalciteEnabled()
-            ? loadFromFile("expectedOutput/calcite/explain_patterns_simple_pattern_agg_push.json")
-            : loadFromFile("expectedOutput/ppl/explain_patterns_simple_pattern_agg_push.json");
+    String expected = loadExpectedPlan("explain_patterns_simple_pattern_agg_push.json");
 
     assertJsonEqualsIgnoreId(
         expected,
@@ -291,15 +240,26 @@ public class ExplainIT extends PPLIntegTestCase {
   @Test
   public void testPatternsBrainMethodWithAggPushDownExplain() throws IOException {
     // TODO: Correct calcite expected result once pushdown is supported
-    String expected =
-        isCalciteEnabled()
-            ? loadFromFile("expectedOutput/calcite/explain_patterns_brain_agg_push.json")
-            : loadFromFile("expectedOutput/ppl/explain_patterns_brain_agg_push.json");
+    String expected = loadExpectedPlan("explain_patterns_brain_agg_push.json");
 
     assertJsonEqualsIgnoreId(
         expected,
         explainQueryToString(
             "source=opensearch-sql_test_index_account"
                 + "| patterns email method=brain mode=aggregation"));
+  }
+
+  private String loadExpectedPlan(String fileName) throws IOException {
+    String prefix;
+    if (isCalciteEnabled()) {
+      if (isPushdownEnabled()) {
+        prefix = "expectedOutput/calcite/";
+      } else {
+        prefix = "expectedOutput/calcite_no_pushdown/";
+      }
+    } else {
+      prefix = "expectedOutput/ppl/";
+    }
+    return loadFromFile(prefix + fileName);
   }
 }
