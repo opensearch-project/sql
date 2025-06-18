66039bf9995d69ced33bc4b32d33922bd1b2986e
Yuanchun Shen <yuanchu@amazon.com>
2025-06-18 23:42:10 +0800

    [Backport 2.19-dev] Migrate standalone integration tests to remote tests (#3790)
    * Migrate standalone integration tests to remote tests (#3778)
    
    * Use PPL_SPEC in standalone ITs
    
    Additionally:
    - Migrate CalcitePPLBasicIT to remote ITs
    - Delete CalcitePPLBasicPushdownIT since the remote ITs enable pushdown by default
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Migrate array, aggregation, appendcol, and datetime ITs to remote
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Migrate case, datetime, condition, cryptographic function ITs
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Fix return types in CalcitePPLTpchIT
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * migrate calcite IT and delete related pushdown IT
    
    Signed-off-by: Xinyu Hao <haoxinyu@amazon.com>
    
    * Temporarily move datetime ITs back to standalone mode
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * WIP: trying to enbale no-pushdown tests
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * add pushdown IT button
    
    Signed-off-by: Xinyu Hao <haoxinyu@amazon.com>
    
    * Fix remote ITs
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Enable no pushdown tests
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Change pushdown settings only when necessary (fixing CalciteSettingsIT)
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Fix sum null tests with and without pushdown
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Update pushdown with transient flag
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Ignore IP comparison and explain ITs in CalciteNoPushdownIT
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Delete CalcitePushdownIT since all tests are run with pushdown enabled by default
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Restore pushdown config after CalciteNoPushdownIT class
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Move expected explain strings to files to circurmvent issues related to comparison involves windows new lines
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Throw narrower exceptions where it uses loadFromFile
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Replace window line enddings with unix line enddings for remote tests' responses
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Revert "Replace window line enddings with unix line enddings for remote tests' responses"
    
    This reverts commit 812fd0dc4ef511833bb43211a66f09362230986a.
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Use explainQueryToString for explain ITs
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Use executeWithReplace for explain command variants
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Migrate CalcitePPLTpchIT to remote test
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Replace windows carriages with spaces when sanitizing queries to explain
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    ---------
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    Signed-off-by: Xinyu Hao <haoxinyu@amazon.com>
    Co-authored-by: Xinyu Hao <haoxinyu@amazon.com>
    (cherry picked from commit 918c153ca99565e304364a5765f950aab86df64a)
    
    * Fix fields comparison in CalcitePPLRenameIT
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    ---------
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    Signed-off-by: Xinyu Hao <haoxinyu@amazon.com>
    Co-authored-by: Xinyu Hao <haoxinyu@amazon.com>

---
+++ integ-test/src/test/java/org/opensearch/sql/calcite/remote/CalcitePPLInSubqueryIT.java
@@ -0,0 +1,361 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.sql.calcite.remote;
+
+import static org.opensearch.sql.legacy.TestsConstants.TEST_INDEX_OCCUPATION;
+import static org.opensearch.sql.legacy.TestsConstants.TEST_INDEX_WORKER;
+import static org.opensearch.sql.legacy.TestsConstants.TEST_INDEX_WORK_INFORMATION;
+import static org.opensearch.sql.util.MatcherUtils.rows;
+import static org.opensearch.sql.util.MatcherUtils.schema;
+import static org.opensearch.sql.util.MatcherUtils.verifyDataRows;
+import static org.opensearch.sql.util.MatcherUtils.verifyDataRowsInOrder;
+import static org.opensearch.sql.util.MatcherUtils.verifyErrorMessageContains;
+import static org.opensearch.sql.util.MatcherUtils.verifySchema;
+
+import java.io.IOException;
+import org.json.JSONObject;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.opensearch.client.Request;
+import org.opensearch.sql.exception.SemanticCheckException;
+import org.opensearch.sql.ppl.PPLIntegTestCase;
+
+public class CalcitePPLInSubqueryIT extends PPLIntegTestCase {
+
+  @Override
+  public void init() throws Exception {
+    super.init();
+    enableCalcite();
+    disallowCalciteFallback();
+
+    loadIndex(Index.WORKER);
+    loadIndex(Index.WORK_INFORMATION);
+    loadIndex(Index.OCCUPATION);
+
+    // {"index":{"_id":"7"}}
+    // {"id":1006,"name":"Tommy","occupation":"Teacher","country":"USA","salary":30000}
+    Request request1 = new Request("PUT", "/" + TEST_INDEX_WORKER + "/_doc/7?refresh=true");
+    request1.setJsonEntity(
+        "{\"id\":1006,\"name\":\"Tommy\",\"occupation\":\"Teacher\",\"country\":\"USA\",\"salary\":30000}");
+    client().performRequest(request1);
+  }
+
+  @Test
+  public void testSelfInSubquery() throws IOException {
+    var result =
+        executeQuery(
+            String.format(
+                "source = %s| where id in [source=%s | where country = 'USA' | fields id] | fields"
+                    + " name, country, occupation, id, salary",
+                TEST_INDEX_WORKER, TEST_INDEX_WORKER));
+    verifySchema(
+        result,
+        schema("name", "string"),
+        schema("country", "string"),
+        schema("occupation", "string"),
+        schema("id", "int"),
+        schema("salary", "int"));
+    verifyDataRows(
+        result,
+        rows("Hello", "USA", "Artist", 1001, 70000),
+        rows("Tommy", "USA", "Teacher", 1006, 30000));
+  }
+
+  @Test
+  public void testWhereInSubquery() throws IOException {
+    JSONObject result =
+        executeQuery(
+            String.format(
+                "source = %s"
+                    + "| where id in ["
+                    + "    source = %s | fields uid"
+                    + "  ]"
+                    + "| sort  - salary"
+                    + "| fields id, name, salary",
+                TEST_INDEX_WORKER, TEST_INDEX_WORK_INFORMATION));
+    verifySchema(result, schema("id", "int"), schema("name", "string"), schema("salary", "int"));
+    verifyDataRowsInOrder(
+        result,
+        rows(1002, "John", 120000),
+        rows(1003, "David", 120000),
+        rows(1000, "Jake", 100000),
+        rows(1005, "Jane", 90000),
+        rows(1006, "Tommy", 30000));
+  }
+
+  @Test
+  public void testFilterInSubquery() throws IOException {
+    JSONObject result =
+        executeQuery(
+            String.format(
+                "source = %s id in ["
+                    + "    source = %s | fields uid"
+                    + "  ]"
+                    + "| sort  - salary"
+                    + "| fields id, name, salary",
+                TEST_INDEX_WORKER, TEST_INDEX_WORK_INFORMATION));
+    verifySchema(result, schema("id", "int"), schema("name", "string"), schema("salary", "int"));
+    verifyDataRowsInOrder(
+        result,
+        rows(1002, "John", 120000),
+        rows(1003, "David", 120000),
+        rows(1000, "Jake", 100000),
+        rows(1005, "Jane", 90000),
+        rows(1006, "Tommy", 30000));
+  }
+
+  @Test
+  public void testInSubqueryWithParentheses() throws IOException {
+    JSONObject result1 =
+        executeQuery(
+            String.format(
+                "source = %s"
+                    + "| where (id) in ["
+                    + "    source = %s | fields uid"
+                    + "  ]"
+                    + "| sort  - salary"
+                    + "| fields id, name, salary",
+                TEST_INDEX_WORKER, TEST_INDEX_WORK_INFORMATION));
+    JSONObject result2 =
+        executeQuery(
+            String.format(
+                "source = %s (id) in ["
+                    + "    source = %s | fields uid"
+                    + "  ]"
+                    + "| sort  - salary"
+                    + "| fields id, name, salary",
+                TEST_INDEX_WORKER, TEST_INDEX_WORK_INFORMATION));
+    verifySchema(result1, schema("id", "int"), schema("name", "string"), schema("salary", "int"));
+    verifySchema(result2, schema("id", "int"), schema("name", "string"), schema("salary", "int"));
+    verifyDataRowsInOrder(
+        result1,
+        rows(1002, "John", 120000),
+        rows(1003, "David", 120000),
+        rows(1000, "Jake", 100000),
+        rows(1005, "Jane", 90000),
+        rows(1006, "Tommy", 30000));
+    verifyDataRowsInOrder(
+        result2,
+        rows(1002, "John", 120000),
+        rows(1003, "David", 120000),
+        rows(1000, "Jake", 100000),
+        rows(1005, "Jane", 90000),
+        rows(1006, "Tommy", 30000));
+  }
+
+  @Test
+  public void testTwoExpressionsInSubquery() throws IOException {
+    JSONObject result =
+        executeQuery(
+            String.format(
+                "source = %s"
+                    + "| where (id, name) in ["
+                    + "    source = %s | fields uid, name"
+                    + "  ]"
+                    + "| sort  - salary"
+                    + "| fields id, name, salary",
+                TEST_INDEX_WORKER, TEST_INDEX_WORK_INFORMATION));
+    verifySchema(result, schema("id", "int"), schema("name", "string"), schema("salary", "int"));
+    verifyDataRowsInOrder(
+        result,
+        rows(1002, "John", 120000),
+        rows(1003, "David", 120000),
+        rows(1000, "Jake", 100000),
+        rows(1005, "Jane", 90000));
+  }
+
+  @Test
+  public void testWhereNotInSubquery() throws IOException {
+    JSONObject result =
+        executeQuery(
+            String.format(
+                "source = %s"
+                    + "| where id not in ["
+                    + "    source = %s | fields uid"
+                    + "  ]"
+                    + "| sort  - salary"
+                    + "| fields id, name, salary",
+                TEST_INDEX_WORKER, TEST_INDEX_WORK_INFORMATION));
+    verifySchema(result, schema("id", "int"), schema("name", "string"), schema("salary", "int"));
+    verifyDataRowsInOrder(result, rows(1001, "Hello", 70000), rows(1004, "David", 0));
+  }
+
+  @Test
+  public void testFilterNotInSubquery() throws IOException {
+    JSONObject result =
+        executeQuery(
+            String.format(
+                "source = %s id not in ["
+                    + "    source = %s | fields uid"
+                    + "  ]"
+                    + "| sort  - salary"
+                    + "| fields id, name, salary",
+                TEST_INDEX_WORKER, TEST_INDEX_WORK_INFORMATION));
+    verifySchema(result, schema("id", "int"), schema("name", "string"), schema("salary", "int"));
+    verifyDataRowsInOrder(result, rows(1001, "Hello", 70000), rows(1004, "David", 0));
+  }
+
+  @Test
+  public void testTwoExpressionsNotInSubquery() throws IOException {
+    JSONObject result =
+        executeQuery(
+            String.format(
+                "source = %s"
+                    + "| where (id, name) not in ["
+                    + "    source = %s | fields uid, name"
+                    + "  ]"
+                    + "| sort  - salary"
+                    + "| fields id, name, salary",
+                TEST_INDEX_WORKER, TEST_INDEX_WORK_INFORMATION));
+    verifySchema(result, schema("id", "int"), schema("name", "string"), schema("salary", "int"));
+    verifyDataRowsInOrder(
+        result, rows(1001, "Hello", 70000), rows(1006, "Tommy", 30000), rows(1004, "David", 0));
+  }
+
+  @Test
+  public void testEmptyInSubquery() throws IOException {
+    JSONObject result =
+        executeQuery(
+            String.format(
+                "source = %s id not in [source = %s | where uid = 0000 | fields uid]| sort  - salary| fields id, name, salary",
+                TEST_INDEX_WORKER, TEST_INDEX_WORK_INFORMATION));
+    verifySchema(result, schema("id", "int"), schema("name", "string"), schema("salary", "int"));
+    verifyDataRowsInOrder(
+        result,
+        rows(1002, "John", 120000),
+        rows(1003, "David", 120000),
+        rows(1000, "Jake", 100000),
+        rows(1005, "Jane", 90000),
+        rows(1001, "Hello", 70000),
+        rows(1006, "Tommy", 30000),
+        rows(1004, "David", 0));
+  }
+
+  @Test
+  public void testNestedInSubquery() throws IOException {
+    JSONObject result =
+        executeQuery(
+            String.format(
+                "source = %s"
+                    + "| where id in ["
+                    + "    source = %s"
+                    + "    | where occupation in ["
+                    + "        source = %s"
+                    + "        | where occupation != 'Engineer'"
+                    + "        | fields occupation"
+                    + "      ]"
+                    + "    | fields uid"
+                    + "  ]"
+                    + "| sort  - salary"
+                    + "| fields id, name, salary",
+                TEST_INDEX_WORKER, TEST_INDEX_WORK_INFORMATION, TEST_INDEX_OCCUPATION));
+    verifySchema(result, schema("id", "int"), schema("name", "string"), schema("salary", "int"));
+    verifyDataRowsInOrder(
+        result,
+        rows(1002, "John", 120000),
+        rows(1003, "David", 120000),
+        rows(1006, "Tommy", 30000));
+  }
+
+  @Test
+  public void testNestedInSubquery2() throws IOException {
+    JSONObject result =
+        executeQuery(
+            String.format(
+                "source = %s"
+                    + "| where id in ["
+                    + "    source = %s"
+                    + "    | where occupation in ["
+                    + "        source = %s"
+                    + "        | where occupation != 'Engineer'"
+                    + "        | fields occupation"
+                    + "      ]"
+                    + "    | fields uid"
+                    + "  ]"
+                    + "| sort  - salary | fields name, country, occupation, id, salary",
+                TEST_INDEX_WORKER, TEST_INDEX_WORK_INFORMATION, TEST_INDEX_OCCUPATION));
+    verifySchema(
+        result,
+        schema("name", "string"),
+        schema("country", "string"),
+        schema("occupation", "string"),
+        schema("id", "int"),
+        schema("salary", "int"));
+    verifyDataRowsInOrder(
+        result,
+        rows("John", "Canada", "Doctor", 1002, 120000),
+        rows("David", null, "Doctor", 1003, 120000),
+        rows("Tommy", "USA", "Teacher", 1006, 30000));
+  }
+
+  @Ignore // TODO bug? fail in execution, the plan converted is correct
+  public void testInSubqueryAsJoinFilter() throws IOException {
+    JSONObject result =
+        executeQuery(
+            String.format(
+                "source = %s"
+                    + "| inner join left=a, right=b"
+                    + "    ON a.id = b.uid AND b.occupation in ["
+                    + "      source = %s | where occupation != 'Engineer' | fields occupation"
+                    + "    ]"
+                    + "    %s"
+                    + "| fields a.id, a.name, a.salary, b.occupation",
+                TEST_INDEX_WORKER, TEST_INDEX_OCCUPATION, TEST_INDEX_WORK_INFORMATION));
+    verifySchema(result, schema("id", "int"), schema("name", "string"), schema("salary", "int"));
+    verifyDataRowsInOrder(
+        result,
+        rows(1003, "David", 120000),
+        rows(1002, "John", 120000),
+        rows(1006, "Tommy", 30000));
+  }
+
+  @Test
+  public void failWhenNumOfColumnsNotMatchOutputOfSubquery() {
+    Throwable e1 =
+        assertThrowsWithReplace(
+            SemanticCheckException.class,
+            () ->
+                executeQuery(
+                    String.format(
+                        "source = %s| where id in [source = %s | fields uid, department ]| sort  - salary | fields id, name, salary",
+                        TEST_INDEX_WORKER, TEST_INDEX_WORK_INFORMATION)));
+    verifyErrorMessageContains(
+        e1,
+        "The number of columns in the left hand side of an IN subquery does not match the number of"
+            + " columns in the output of subquery");
+
+    Throwable e2 =
+        assertThrowsWithReplace(
+            SemanticCheckException.class,
+            () ->
+                executeQuery(
+                    String.format(
+                        "source = %s| where (id, name, salary) in [source = %s | fields uid, department] | sort  - salary| fields id, name, salary",
+                        TEST_INDEX_WORKER, TEST_INDEX_WORK_INFORMATION)));
+    verifyErrorMessageContains(
+        e2,
+        "The number of columns in the left hand side of an IN subquery does not match the number of"
+            + " columns in the output of subquery");
+  }
+
+  @Test
+  public void testInSubqueryWithTableAlias() throws IOException {
+    JSONObject result =
+        executeQuery(
+            String.format(
+                    "source = %s as o"
+                            + "| where id in ["
+                            + "    source = %s as i"
+                            + "    | where i.department = 'DATA'"
+                            + "    | fields uid"
+                            + "  ]"
+                            + "| sort - o.salary"
+                            + "| fields o.id, o.name, o.salary",
+                TEST_INDEX_WORKER, TEST_INDEX_WORK_INFORMATION));
+    verifySchema(result, schema("id", "int"), schema("name", "string"), schema("salary", "int"));
+    verifyDataRowsInOrder(result, rows(1002, "John", 120000), rows(1005, "Jane", 90000));
+  }
+}
