1dd6ebcb2650aa31e7c173201107ff0ee4e164bb
xinyual <xinyual@amazon.com>
2025-06-23 14:17:08 +0800

    remove useless change
    Signed-off-by: xinyual <xinyual@amazon.com>

--- integ-test/src/test/java/org/opensearch/sql/calcite/remote/CalciteExplainIT.java
+++ integ-test/src/test/java/org/opensearch/sql/calcite/remote/CalciteExplainIT.java
@@ -5,11 +5,8 @@
 
 package org.opensearch.sql.calcite.remote;
 
-import static org.opensearch.sql.util.MatcherUtils.assertJsonEqualsIgnoreId;
-
 import java.io.IOException;
 import org.junit.Ignore;
-import org.junit.jupiter.api.Test;
 import org.opensearch.sql.ppl.ExplainIT;
 
 public class CalciteExplainIT extends ExplainIT {
@@ -23,20 +20,4 @@ public class CalciteExplainIT extends ExplainIT {
   @Override
   @Ignore("test only in v2")
   public void testExplainModeUnsupportedInV2() throws IOException {}
-
-  @Test
-  public void testFilterByCompareStringTimestampPushDownExplain() throws IOException {
-    String expected =
-        isCalciteEnabled()
-            ? loadFromFile(
-                "expectedOutput/calcite/explain_filter_push_compare_timestamp_string.json")
-            : loadFromFile("expectedOutput/ppl/explain_filter_push_compare_timestamp_string.json");
-
-    assertJsonEqualsIgnoreId(
-        expected,
-        explainQueryToString(
-            "source=opensearch-sql_test_index_bank"
-                + "| where birthdate > '2016-12-08 00:00:00.000000000' "
-                + "| where birthdate < '2018-11-09 00:00:00.000000000' "));
-  }
 }
