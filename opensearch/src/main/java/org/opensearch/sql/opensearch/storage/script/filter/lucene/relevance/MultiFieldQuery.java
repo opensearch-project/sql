74b4de067cefc821055076bd108249cef777c3f9
Songkan Tang <songkant@amazon.com>
2025-07-02 17:53:11 +0800

    Support relevance query functions pushdown implementation in Calcite (#3834)
    * Support relevance query functions pushdown implementation
    
    Signed-off-by: Songkan Tang <songkant@amazon.com>
    
    * Remove unused change
    
    Signed-off-by: Songkan Tang <songkant@amazon.com>
    
    * Remove ITs from NoPushDownIT list
    
    Signed-off-by: Songkan Tang <songkant@amazon.com>
    
    * Add more UTs and javadocs
    
    Signed-off-by: Songkan Tang <songkant@amazon.com>
    
    * Support analyzing UnresolvedArgument RexNode and refactor code
    
    Signed-off-by: Songkan Tang <songkant@amazon.com>
    
    * Fix spotless check
    
    Signed-off-by: Songkan Tang <songkant@amazon.com>
    
    ---------
    
    Signed-off-by: Songkan Tang <songkant@amazon.com>

--- opensearch/src/main/java/org/opensearch/sql/opensearch/storage/script/filter/lucene/relevance/MultiFieldQuery.java
+++ opensearch/src/main/java/org/opensearch/sql/opensearch/storage/script/filter/lucene/relevance/MultiFieldQuery.java
@@ -8,6 +8,12 @@ package org.opensearch.sql.opensearch.storage.script.filter.lucene.relevance;
 import com.google.common.collect.ImmutableMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.IntStream;
+import org.apache.calcite.rex.RexCall;
+import org.apache.calcite.rex.RexLiteral;
+import org.apache.calcite.rex.RexNode;
+import org.apache.calcite.util.NlsString;
 import org.opensearch.index.query.QueryBuilder;
 import org.opensearch.sql.exception.SemanticCheckException;
 import org.opensearch.sql.expression.NamedArgumentExpression;
@@ -46,4 +52,35 @@ abstract class MultiFieldQuery<T extends QueryBuilder> extends RelevanceQuery<T>
   }
 
   protected abstract T createBuilder(ImmutableMap<String, Float> fields, String query);
+
+  /**
+   * Build multi-fields relevance query builder based on Calcite function's operands. For
+   * MultiFieldQuery, fields with weights and query string parameter are required.
+   *
+   * @param fieldsRexCall Calcite MAP RexCall that wraps multi-fields and corresponding weights
+   * @param query String query to search
+   * @param optionalArguments Map contains optional relevance query argument key value pairs
+   * @return Final QueryBuilder
+   */
+  public T build(RexCall fieldsRexCall, String query, Map<String, String> optionalArguments) {
+    List<RexNode> fieldAndWeightNodes = fieldsRexCall.getOperands();
+    ImmutableMap<String, Float> fields =
+        IntStream.range(0, fieldsRexCall.getOperands().size() / 2)
+            .map(i -> i * 2)
+            .mapToObj(
+                i -> {
+                  RexLiteral fieldLiteral = (RexLiteral) fieldAndWeightNodes.get(i);
+                  RexLiteral weightLiteral = (RexLiteral) fieldAndWeightNodes.get(i + 1);
+                  String field =
+                      ((NlsString) Objects.requireNonNull(fieldLiteral.getValue())).getValue();
+                  Float weight =
+                      ((Double) Objects.requireNonNull(weightLiteral.getValue())).floatValue();
+                  return Map.entry(field, weight);
+                })
+            .collect(ImmutableMap.toImmutableMap(Map.Entry::getKey, Map.Entry::getValue));
+
+    T queryBuilder = createBuilder(fields, query);
+
+    return applyArguments(queryBuilder, optionalArguments);
+  }
 }
