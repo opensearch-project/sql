62a60848093bd84c7a854907173b2afc419dbf83
Xinyu Hao <haoxinyu@amazon.com>
2025-07-07 13:20:40 +0800

    move switch case to the implement method
    Signed-off-by: Xinyu Hao <haoxinyu@amazon.com>

--- core/src/main/java/org/opensearch/sql/expression/function/udf/ip/CompareIpFunction.java
+++ core/src/main/java/org/opensearch/sql/expression/function/udf/ip/CompareIpFunction.java
@@ -5,7 +5,6 @@
 
 package org.opensearch.sql.expression.function.udf.ip;
 
-import inet.ipaddr.IPAddress;
 import java.util.List;
 import org.apache.calcite.adapter.enumerable.NotNullImplementor;
 import org.apache.calcite.adapter.enumerable.NullPolicy;
@@ -18,7 +17,6 @@ import org.apache.calcite.sql.type.SqlReturnTypeInference;
 import org.opensearch.sql.data.model.ExprIpValue;
 import org.opensearch.sql.expression.function.ImplementorUDF;
 import org.opensearch.sql.expression.function.UDFOperandMetadata;
-import org.opensearch.sql.utils.IPUtils;
 
 /**
  * {@code compare(ip1, ip2)} compares two IP addresses using a provided op.
@@ -81,37 +79,41 @@ public class CompareIpFunction extends ImplementorUDF {
     @Override
     public Expression implement(
         RexToLixTranslator translator, RexCall call, List<Expression> translatedOperands) {
-      return Expressions.call(
-          CompareImplementor.class,
-          "compare",
-          translatedOperands.get(0),
-          translatedOperands.get(1),
-          Expressions.constant(comparisonType));
+      Expression compareResult =
+          Expressions.call(
+              CompareImplementor.class,
+              "compareTo",
+              translatedOperands.get(0),
+              translatedOperands.get(1));
+
+      return generateComparisonExpression(compareResult, comparisonType);
     }
 
-    public static boolean compare(Object obj1, Object obj2, ComparisonType comparisonType) {
-      try {
-        String ip1 = extractIpString(obj1);
-        String ip2 = extractIpString(obj2);
-        IPAddress addr1 = IPUtils.toAddress(ip1);
-        IPAddress addr2 = IPUtils.toAddress(ip2);
-        int result = IPUtils.compare(addr1, addr2);
-        return switch (comparisonType) {
-          case EQUALS -> result == 0;
-          case NOT_EQUALS -> result != 0;
-          case LESS -> result < 0;
-          case LESS_OR_EQUAL -> result <= 0;
-          case GREATER -> result > 0;
-          case GREATER_OR_EQUAL -> result >= 0;
-        };
-      } catch (Exception e) {
-        return false;
+    private static Expression generateComparisonExpression(
+        Expression compareResult, ComparisonType comparisonType) {
+      return switch (comparisonType) {
+        case EQUALS -> Expressions.equal(compareResult, Expressions.constant(0));
+        case NOT_EQUALS -> Expressions.notEqual(compareResult, Expressions.constant(0));
+        case LESS -> Expressions.lessThan(compareResult, Expressions.constant(0));
+        case LESS_OR_EQUAL -> Expressions.lessThanOrEqual(compareResult, Expressions.constant(0));
+        case GREATER -> Expressions.greaterThan(compareResult, Expressions.constant(0));
+        case GREATER_OR_EQUAL -> Expressions.greaterThanOrEqual(
+            compareResult, Expressions.constant(0));
+      };
+    }
+
+    public static int compareTo(Object obj1, Object obj2) {
+      ExprIpValue v1 = toExprIpValue(obj1);
+      ExprIpValue v2 = toExprIpValue(obj2);
+      return v1.compare(v2);
+    }
+
+    private static ExprIpValue toExprIpValue(Object obj) {
+      if (obj instanceof ExprIpValue) {
+        return (ExprIpValue) obj;
+      } else if (obj instanceof String) {
+        return new ExprIpValue((String) obj);
       }
-    }
-
-    private static String extractIpString(Object obj) {
-      if (obj instanceof String) return (String) obj;
-      if (obj instanceof ExprIpValue) return ((ExprIpValue) obj).value();
       throw new IllegalArgumentException("Invalid IP type: " + obj);
     }
   }
