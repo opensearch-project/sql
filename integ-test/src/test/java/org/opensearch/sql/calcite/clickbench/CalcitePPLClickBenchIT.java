0f1912b9b46414dc6c1dd640b66568e4e2dffb54
Lantao Jin <ltjin@amazon.com>
2025-07-09 22:47:41 +0800

    Add clickbench IT Suite
    Signed-off-by: Lantao Jin <ltjin@amazon.com>

--- integ-test/src/test/java/org/opensearch/sql/calcite/clickbench/CalcitePPLClickBenchIT.java
+++ integ-test/src/test/java/org/opensearch/sql/calcite/clickbench/CalcitePPLClickBenchIT.java
@@ -3,18 +3,24 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package org.opensearch.sql.calcite.big5;
+package org.opensearch.sql.calcite.clickbench;
 
+import java.util.Set;
 import org.junit.FixMethodOrder;
 import org.junit.runners.MethodSorters;
 
 @FixMethodOrder(MethodSorters.JVM)
-public class CalcitePPLBig5IT extends PPLBig5IT {
+public class CalcitePPLClickBenchIT extends PPLClickBenchIT {
 
   @Override
   public void init() throws Exception {
     super.init();
     enableCalcite();
     disallowCalciteFallback();
+  }
+
+  @Override
+  protected Set<Integer> ignored() {
+    return Set.of(29, 41, 43);
   }
 }
