a197229cc8f3e6827c26a9c1634b63071d98cfab
Aaron Alvarez <aaarone@amazon.com>
2025-07-27 21:40:24 -0700

    Changing the documentation for the visitTable menthod
    Signed-off-by: Aaron Alvarez <aaarone@amazon.com>

--- core/src/main/java/org/opensearch/sql/ast/AbstractNodeVisitor.java
+++ core/src/main/java/org/opensearch/sql/ast/AbstractNodeVisitor.java
@@ -116,8 +116,8 @@ public abstract class AbstractNodeVisitor<T, C> {
   /**
    * Visit a Table node in the AST.
    *
-   * <p>This method processes a Table node which represents a data source in SQL queries. Tables are
-   * fundamental relation objects that serve as leaf nodes in the query plan.
+   * <p>This method processes a Table node which represents a table operation in SQL queries.
+   * Table nodes can have child nodes representing the source data for the operation.
    *
    * @param node The Table node to visit
    * @param context The context object that carries information throughout the visitor pattern
