eaeba84ac6acbb58dbbbe6e192848920770752fa
Aaron Alvarez <aaarone@amazon.com>
2025-07-29 20:57:41 -0700

    Add support for space-separated fields in addition to comma-separated
    Signed-off-by: Aaron Alvarez <aaarone@amazon.com>

--- integ-test/src/test/java/org/opensearch/sql/calcite/remote/CalciteFieldsCommandIT.java
+++ integ-test/src/test/java/org/opensearch/sql/calcite/remote/CalciteFieldsCommandIT.java
@@ -5,6 +5,12 @@
 
 package org.opensearch.sql.calcite.remote;
 
+import static org.opensearch.sql.legacy.TestsConstants.*;
+import static org.opensearch.sql.util.MatcherUtils.*;
+
+import java.io.IOException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
 import org.opensearch.sql.ppl.FieldsCommandIT;
 
 public class CalciteFieldsCommandIT extends FieldsCommandIT {
@@ -13,5 +19,63 @@ public class CalciteFieldsCommandIT extends FieldsCommandIT {
     super.init();
     enableCalcite();
     disallowCalciteFallback();
+  }
+
+  /**
+   * Tests space-delimited field selection with Calcite engine.
+   */
+  @Test
+  public void testCalciteFieldsWithSpaceDelimitedSyntax() throws IOException {
+    JSONObject result =
+        executeQuery(String.format("source=%s | fields firstname lastname age", TEST_INDEX_ACCOUNT));
+    verifyColumn(result, columnName("firstname"), columnName("lastname"), columnName("age"));
+  }
+
+  /**
+   * Tests equivalence between space-delimited and comma-delimited field syntax.
+   */
+  @Test
+  public void testCalciteFieldsSpaceDelimitedEquivalentToCommaDelimited() throws IOException {
+    JSONObject commaResult =
+        executeQuery(String.format("source=%s | fields firstname, lastname, age | head 3", TEST_INDEX_ACCOUNT));
+    JSONObject spaceResult =
+        executeQuery(String.format("source=%s | fields firstname lastname age | head 3", TEST_INDEX_ACCOUNT));
+    
+    verifySchema(commaResult, 
+        schema("firstname", "string"), 
+        schema("lastname", "string"), 
+        schema("age", "bigint"));
+    verifySchema(spaceResult, 
+        schema("firstname", "string"), 
+        schema("lastname", "string"), 
+        schema("age", "bigint"));
+    
+    verifyDataRows(commaResult, 
+        rows("Amber", "Duke", 32), 
+        rows("Hattie", "Bond", 36), 
+        rows("Nanette", "Bates", 28));
+    verifyDataRows(spaceResult, 
+        rows("Amber", "Duke", 32), 
+        rows("Hattie", "Bond", 36), 
+        rows("Nanette", "Bates", 28));
+  }
+
+  /**
+   * Tests field exclusion using space-delimited syntax.
+   */
+  @Test
+  public void testCalciteFieldsSpaceDelimitedWithRemoval() throws IOException {
+    JSONObject result =
+        executeQuery(String.format("source=%s | fields - firstname lastname | head 3", TEST_INDEX_ACCOUNT));
+    verifyColumn(result, 
+        columnName("account_number"), 
+        columnName("age"), 
+        columnName("gender"), 
+        columnName("address"), 
+        columnName("employer"), 
+        columnName("email"), 
+        columnName("city"), 
+        columnName("state"), 
+        columnName("balance"));
   }
 }
