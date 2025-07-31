cd389833420aee5af31b850a6bbf6a309e861024
Songkan Tang <songkant@amazon.com>
2025-07-31 11:11:18 +0800

    Convert like function call to wildcard query for Calcite filter pushdown (#3915)
    * Convert like function call to wildcard query for Calcite filter pushdown
    
    Signed-off-by: Songkan Tang <songkant@amazon.com>
    
    * Fix V2 expression like function bug and match its behavior in Calcite
    
    Signed-off-by: Songkan Tang <songkant@amazon.com>
    
    * Fix like default escape in Calcite
    
    Signed-off-by: Songkan Tang <songkant@amazon.com>
    
    * Fix tests
    
    Signed-off-by: Songkan Tang <songkant@amazon.com>
    
    * Fix spotless check
    
    Signed-off-by: Songkan Tang <songkant@amazon.com>
    
    * Address comments
    
    Signed-off-by: Songkan Tang <songkant@amazon.com>
    
    * Fix SQL IT correctness
    
    Signed-off-by: Songkan Tang <songkant@amazon.com>
    
    * Remove test log
    
    Signed-off-by: Songkan Tang <songkant@amazon.com>
    
    * Minor improve one CalciteLikeQueryIT
    
    Signed-off-by: Songkan Tang <songkant@amazon.com>
    
    ---------
    
    Signed-off-by: Songkan Tang <songkant@amazon.com>

--- opensearch/src/main/java/org/opensearch/sql/opensearch/storage/script/filter/lucene/LikeQuery.java
+++ opensearch/src/main/java/org/opensearch/sql/opensearch/storage/script/filter/lucene/LikeQuery.java
@@ -10,8 +10,12 @@ import org.opensearch.index.query.QueryBuilders;
 import org.opensearch.index.query.WildcardQueryBuilder;
 import org.opensearch.sql.data.model.ExprValue;
 import org.opensearch.sql.data.type.ExprType;
+import org.opensearch.sql.expression.FunctionExpression;
+import org.opensearch.sql.expression.LiteralExpression;
+import org.opensearch.sql.expression.ReferenceExpression;
 import org.opensearch.sql.opensearch.data.type.OpenSearchTextType;
 import org.opensearch.sql.opensearch.storage.script.StringUtils;
+import org.opensearch.sql.opensearch.storage.script.filter.FilterQueryBuilder.ScriptQueryUnSupportedException;
 
 public class LikeQuery extends LuceneQuery {
   @Override
@@ -26,7 +30,35 @@ public class LikeQuery extends LuceneQuery {
    * ReferenceExpression while wildcard_query are of type NamedArgumentExpression
    */
   protected WildcardQueryBuilder createBuilder(String field, String query) {
-    String matchText = StringUtils.convertSqlWildcardToLucene(query);
+    String matchText = StringUtils.convertSqlWildcardToLuceneSafe(query);
     return QueryBuilders.wildcardQuery(field, matchText).caseInsensitive(true);
+  }
+
+  /**
+   * Verify if the function supports like/wildcard query. Prefer to run wildcard query for keyword
+   * type field. For text type field, it doesn't support cross term match because OpenSearch
+   * internally break text to multiple terms and apply wildcard matching one by one, which is not
+   * same behavior with regular like function without pushdown.
+   *
+   * @param func function Input function expression
+   * @return boolean
+   */
+  @Override
+  public boolean canSupport(FunctionExpression func) {
+    if (func.getArguments().size() == 2
+        && (func.getArguments().get(0) instanceof ReferenceExpression)
+        && (func.getArguments().get(1) instanceof LiteralExpression
+            || literalExpressionWrappedByCast(func))) {
+      ReferenceExpression ref = (ReferenceExpression) func.getArguments().get(0);
+      // Only support keyword type field
+      if (OpenSearchTextType.toKeywordSubField(ref.getRawPath(), ref.getType()) != null) {
+        return true;
+      } else {
+        // Script pushdown is not supported for text type field
+        throw new ScriptQueryUnSupportedException(
+            "text field wildcard doesn't support script query");
+      }
+    }
+    return false;
   }
 }
