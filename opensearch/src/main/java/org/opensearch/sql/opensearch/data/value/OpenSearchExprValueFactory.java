4db6506950aa3282f4f0344b357baa0915f3fc51
Kai Huang <ahkcs@amazon.com>
2025-08-20 09:58:15 -0700

    remove
    Signed-off-by: Kai Huang <ahkcs@amazon.com>

--- opensearch/src/main/java/org/opensearch/sql/opensearch/data/value/OpenSearchExprValueFactory.java
+++ opensearch/src/main/java/org/opensearch/sql/opensearch/data/value/OpenSearchExprValueFactory.java
@@ -206,14 +206,8 @@ public class OpenSearchExprValueFactory {
       String stringValue = (String) content.objectValue();
       // Check if this is a numeric range pattern (e.g., "20-30", "1000.0-10000.0")
       if (stringValue.matches("\\d+\\.?\\d*-\\d+\\.?\\d*")) {
-        // Exclude date formats to avoid false positives:
-        // - YYYY-DDD (ordinal date): "1984-103"
-        // - YYYY-MM-DD (ISO date): "1984-04-12"
-        if (!stringValue.matches("\\d{4}-\\d{1,3}")
-            && !stringValue.matches("\\d{4}-\\d{2}-\\d{2}")) {
-          // This is a range string from bin operation - treat as string value
-          return new ExprStringValue(stringValue);
-        }
+        // This is a range string from bin operation - treat as string value
+        return new ExprStringValue(stringValue);
       }
     }
 
