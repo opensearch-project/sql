c641ef93b306bded212180f2126ee1681afb64f9
Jialiang Liang <jiallian@amazon.com>
2025-08-20 01:32:45 -0700

    add IT for calcite no pushdown
    Signed-off-by: Jialiang Liang <jiallian@amazon.com>

--- integ-test/src/test/java/org/opensearch/sql/calcite/CalciteNoPushdownIT.java
+++ integ-test/src/test/java/org/opensearch/sql/calcite/CalciteNoPushdownIT.java
@@ -82,6 +82,7 @@ import org.opensearch.sql.ppl.PPLIntegTestCase;
   CalcitePrometheusDataSourceCommandsIT.class,
   CalciteQueryAnalysisIT.class,
   CalciteRareCommandIT.class,
+  CalciteRegexCommandIT.class,
   CalciteRenameCommandIT.class,
   CalciteResourceMonitorIT.class,
   CalciteSearchCommandIT.class,
