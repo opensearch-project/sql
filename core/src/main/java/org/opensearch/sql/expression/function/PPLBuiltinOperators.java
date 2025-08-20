80395aeb6916b1d3a611e05fb04b8bfd4b74b087
Jialiang Liang <jiallian@amazon.com>
2025-08-20 11:45:53 -0700

    style - remove some verbose comments
    Signed-off-by: Jialiang Liang <jiallian@amazon.com>

--- core/src/main/java/org/opensearch/sql/expression/function/PPLBuiltinOperators.java
+++ core/src/main/java/org/opensearch/sql/expression/function/PPLBuiltinOperators.java
@@ -47,6 +47,7 @@ import org.opensearch.sql.expression.function.jsonUDF.JsonKeysFunctionImpl;
 import org.opensearch.sql.expression.function.jsonUDF.JsonSetFunctionImpl;
 import org.opensearch.sql.expression.function.udf.CryptographicFunction;
 import org.opensearch.sql.expression.function.udf.GrokFunction;
+import org.opensearch.sql.expression.function.udf.RegexMatchFunctionImpl;
 import org.opensearch.sql.expression.function.udf.RelevanceQueryFunction;
 import org.opensearch.sql.expression.function.udf.SpanFunction;
 import org.opensearch.sql.expression.function.udf.condition.EarliestFunction;
@@ -382,8 +383,7 @@ public class PPLBuiltinOperators extends ReflectiveSqlOperatorTable {
       new NumberToStringFunction().toUDF("NUMBER_TO_STRING");
 
   // Custom regex operator for Calcite engine
-  public static final SqlOperator REGEX_MATCH =
-      new org.opensearch.sql.expression.function.udf.RegexMatchFunctionImpl().toUDF("REGEX_MATCH");
+  public static final SqlOperator REGEX_MATCH = new RegexMatchFunctionImpl().toUDF("REGEX_MATCH");
 
   /**
    * Returns the PPL specific operator table, creating it if necessary.
