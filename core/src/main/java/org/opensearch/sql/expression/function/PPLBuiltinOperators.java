8ec02765a0f39e22bb6a8627451798c5cc810dec
Jialiang Liang <jiallian@amazon.com>
2025-08-22 01:50:30 -0700

    sed - initial implementation
    Signed-off-by: Jialiang Liang <jiallian@amazon.com>

--- core/src/main/java/org/opensearch/sql/expression/function/PPLBuiltinOperators.java
+++ core/src/main/java/org/opensearch/sql/expression/function/PPLBuiltinOperators.java
@@ -51,6 +51,7 @@ import org.opensearch.sql.expression.function.udf.RelevanceQueryFunction;
 import org.opensearch.sql.expression.function.udf.RexExtractFunction;
 import org.opensearch.sql.expression.function.udf.RexExtractMultiFunction;
 import org.opensearch.sql.expression.function.udf.RexOffsetFunction;
+import org.opensearch.sql.expression.function.udf.RexSedFunction;
 import org.opensearch.sql.expression.function.udf.SpanFunction;
 import org.opensearch.sql.expression.function.udf.condition.EarliestFunction;
 import org.opensearch.sql.expression.function.udf.condition.LatestFunction;
@@ -387,6 +388,7 @@ public class PPLBuiltinOperators extends ReflectiveSqlOperatorTable {
   public static final SqlOperator REX_EXTRACT_MULTI =
       new RexExtractMultiFunction().toUDF("REX_EXTRACT_MULTI");
   public static final SqlOperator REX_OFFSET = new RexOffsetFunction().toUDF("REX_OFFSET");
+  public static final SqlOperator REX_SED = new RexSedFunction().toUDF("REX_SED");
 
   /**
    * Returns the PPL specific operator table, creating it if necessary.
