6f7ae56cea5cd0ec30ba87a56fb323574c8d9c54
Kai Huang <ahkcs@amazon.com>
2025-08-21 19:42:07 -0700

    Add UT
    Signed-off-by: Kai Huang <ahkcs@amazon.com>

---
+++ ppl/src/test/java/org/opensearch/sql/ppl/calcite/CalcitePPLBinTest.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.sql.ppl.calcite;
+
+import org.apache.calcite.rel.RelNode;
+import org.apache.calcite.test.CalciteAssert;
+import org.junit.Test;
+
+public class CalcitePPLBinTest extends CalcitePPLAbstractTest {
+
+  public CalcitePPLBinTest() {
+    super(CalciteAssert.SchemaSpec.SCOTT_WITH_TEMPORAL);
+  }
+
+  @Test
+  public void testBinWithSpan() {
+    String ppl = "source=EMP | bin SAL span=1000";
+    RelNode root = getRelNode(ppl);
+
+    String expectedLogical =
+        "LogicalProject(EMPNO=[$0], ENAME=[$1], JOB=[$2], MGR=[$3], HIREDATE=[$4], "
+            + "SAL=[SPAN_BUCKET($5, 1000)], COMM=[$6], DEPTNO=[$7])\n"
+            + "  LogicalTableScan(table=[[scott, EMP]])\n";
+    verifyLogical(root, expectedLogical);
+
+    String expectedSparkSql =
+        "SELECT `EMPNO`, `ENAME`, `JOB`, `MGR`, `HIREDATE`, `SPAN_BUCKET`(`SAL`, 1000) `SAL`,"
+            + " `COMM`, `DEPTNO`\n"
+            + "FROM `scott`.`EMP`";
+    verifyPPLToSparkSQL(root, expectedSparkSql);
+  }
+
+  @Test
+  public void testBinWithBins() {
+    String ppl = "source=EMP | bin SAL bins=10";
+    RelNode root = getRelNode(ppl);
+
+    // Note: WIDTH_BUCKET uses window functions without ROWS UNBOUNDED PRECEDING in the actual
+    // output
+    verifyLogical(
+        root,
+        "LogicalProject(EMPNO=[$0], ENAME=[$1], JOB=[$2], MGR=[$3], HIREDATE=[$4], "
+            + "SAL=[WIDTH_BUCKET($5, 10, "
+            + "-(MAX($5) OVER (), MIN($5) OVER ()), "
+            + "MAX($5) OVER ())], COMM=[$6], DEPTNO=[$7])\n"
+            + "  LogicalTableScan(table=[[scott, EMP]])\n");
+  }
+
+  @Test
+  public void testBinWithMinspan() {
+    String ppl = "source=EMP | bin SAL minspan=100";
+    RelNode root = getRelNode(ppl);
+
+    // Note: MINSPAN_BUCKET converts the minspan to DOUBLE and uses window functions without ROWS
+    // clause
+    verifyLogical(
+        root,
+        "LogicalProject(EMPNO=[$0], ENAME=[$1], JOB=[$2], MGR=[$3], HIREDATE=[$4], "
+            + "SAL=[MINSPAN_BUCKET($5, 100.0E0:DOUBLE, "
+            + "-(MAX($5) OVER (), MIN($5) OVER ()), "
+            + "MAX($5) OVER ())], COMM=[$6], DEPTNO=[$7])\n"
+            + "  LogicalTableScan(table=[[scott, EMP]])\n");
+  }
+
+  @Test
+  public void testBinWithStartEnd() {
+    String ppl = "source=EMP | bin SAL start=1000 end=5000";
+    RelNode root = getRelNode(ppl);
+
+    // Note: RANGE_BUCKET uses window functions without ROWS UNBOUNDED PRECEDING
+    verifyLogical(
+        root,
+        "LogicalProject(EMPNO=[$0], ENAME=[$1], JOB=[$2], MGR=[$3], HIREDATE=[$4], "
+            + "SAL=[RANGE_BUCKET($5, "
+            + "MIN($5) OVER (), MAX($5) OVER (), "
+            + "1000, 5000)], COMM=[$6], DEPTNO=[$7])\n"
+            + "  LogicalTableScan(table=[[scott, EMP]])\n");
+  }
+
+  @Test
+  public void testBinWithTimeSpan() {
+    String ppl = "source=products_temporal | bin SYS_START span=1h";
+    RelNode root = getRelNode(ppl);
+
+    // Time span binning generates FROM_UNIXTIME expression
+    verifyLogical(
+        root,
+        "LogicalProject(ID=[$0], SUPPLIER=[$1], "
+            + "SYS_START=[FROM_UNIXTIME(*(FLOOR(/(/(UNIX_TIMESTAMP($2), 3600), 1)), 3600))], "
+            + "SYS_END=[$3])\n"
+            + "  LogicalTableScan(table=[[scott, products_temporal]])\n");
+
+    verifyPPLToSparkSQL(
+        root,
+        "SELECT `ID`, `SUPPLIER`, `FROM_UNIXTIME`(FLOOR(`UNIX_TIMESTAMP`(`SYS_START`) / 3600 / 1) *"
+            + " 3600) `SYS_START`, `SYS_END`\n"
+            + "FROM `scott`.`products_temporal`");
+  }
+
+  @Test
+  public void testBinWithAligntime() {
+    String ppl = "source=products_temporal | bin SYS_START span=1h aligntime=earliest";
+    RelNode root = getRelNode(ppl);
+
+    // Time span binning with aligntime generates the same expression as without aligntime for this
+    // case
+    verifyLogical(
+        root,
+        "LogicalProject(ID=[$0], SUPPLIER=[$1], "
+            + "SYS_START=[FROM_UNIXTIME(*(FLOOR(/(/(UNIX_TIMESTAMP($2), 3600), 1)), 3600))], "
+            + "SYS_END=[$3])\n"
+            + "  LogicalTableScan(table=[[scott, products_temporal]])\n");
+
+    verifyPPLToSparkSQL(
+        root,
+        "SELECT `ID`, `SUPPLIER`, `FROM_UNIXTIME`(FLOOR(`UNIX_TIMESTAMP`(`SYS_START`) / 3600 / 1) *"
+            + " 3600) `SYS_START`, `SYS_END`\n"
+            + "FROM `scott`.`products_temporal`");
+  }
+}
