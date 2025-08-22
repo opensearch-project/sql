634a67f3f0ea4ff13aa1691c457b81d48751f8fa
Vamsi Manohar <reddyvam@amazon.com>
2025-08-21 22:39:31 -0700

    strftime function implementation
    Signed-off-by: Vamsi Manohar <reddyvam@amazon.com>

--- integ-test/src/test/java/org/opensearch/sql/calcite/remote/CalciteExplainIT.java
+++ integ-test/src/test/java/org/opensearch/sql/calcite/remote/CalciteExplainIT.java
@@ -5,6 +5,8 @@
 
 package org.opensearch.sql.calcite.remote;
 
+import static org.junit.Assert.assertTrue;
+import static org.opensearch.sql.legacy.TestUtils.*;
 import static org.opensearch.sql.legacy.TestsConstants.TEST_INDEX_BANK;
 import static org.opensearch.sql.legacy.TestsConstants.TEST_INDEX_LOGS;
 import static org.opensearch.sql.legacy.TestsConstants.TEST_INDEX_NESTED_SIMPLE;
@@ -639,5 +641,16 @@ public class CalciteExplainIT extends ExplainIT {
   private String executeWithReplace(String ppl) throws IOException {
     var result = executeQueryToString(ppl);
     return result.replace("\\r\\n", "\\n");
+  }
+
+  @Test
+  public void testStrftimeFunctionExplain() throws IOException {
+    // Test explain for strftime function
+    String query =
+        "source=opensearch-sql_test_index_account | eval formatted_date = strftime(1521467703,"
+            + " '%Y-%m-%d') | fields formatted_date | head 1";
+    var result = explainQueryToString(query);
+    String expected = loadExpectedPlan("explain_strftime_function.json");
+    assertJsonEqualsIgnoreId(expected, result);
   }
 }
