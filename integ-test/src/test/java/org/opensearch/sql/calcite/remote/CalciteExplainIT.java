9c67fef3fa6e39b92c59669398beff5e36890b42
qianheng <qianheng@amazon.com>
2025-08-25 11:14:58 +0800

    [Backport 2.19-dev]  SUM aggregation enhancement on operations with literal (#3971) (#4104)
    * SUM aggregation enhancement on operations with literal (#3971)
    
    * SUM aggregation enhancement on operations with literal
    
    Signed-off-by: Heng Qian <qianheng@amazon.com>
    
    * Fix CI
    
    Signed-off-by: Heng Qian <qianheng@amazon.com>
    
    * Keep ignoring q30 for Calcite
    
    Signed-off-by: Heng Qian <qianheng@amazon.com>
    
    * Add UT for PPLAggregateConvertRule
    
    Signed-off-by: Heng Qian <qianheng@amazon.com>
    
    * Add UT for PPLAggregateConvertRule
    
    Signed-off-by: Heng Qian <qianheng@amazon.com>
    
    * Spotless check
    
    Signed-off-by: Heng Qian <qianheng@amazon.com>
    
    ---------
    
    Signed-off-by: Heng Qian <qianheng@amazon.com>
    Co-authored-by: Mitchell Gale <mitchell.gale@improving.com>
    (cherry picked from commit 892355190687033b0f29bae624182c1ba204bf96)
    
    * Fix Compiling
    
    Signed-off-by: Heng Qian <qianheng@amazon.com>
    
    * Fix Compiling
    
    Signed-off-by: Heng Qian <qianheng@amazon.com>
    
    * Fix Compiling
    
    Signed-off-by: Heng Qian <qianheng@amazon.com>
    
    ---------
    
    Signed-off-by: Heng Qian <qianheng@amazon.com>
    Co-authored-by: Mitchell Gale <mitchell.gale@improving.com>
    Co-authored-by: MitchellGale-BitQuill <104795536+mitchellgale-bitquill@users.noreply.github.com>

--- integ-test/src/test/java/org/opensearch/sql/calcite/remote/CalciteExplainIT.java
+++ integ-test/src/test/java/org/opensearch/sql/calcite/remote/CalciteExplainIT.java
@@ -5,6 +5,7 @@
 
 package org.opensearch.sql.calcite.remote;
 
+import static org.opensearch.sql.legacy.TestsConstants.TEST_INDEX_BANK;
 import static org.opensearch.sql.util.MatcherUtils.assertJsonEqualsIgnoreId;
 
 import java.io.IOException;
@@ -214,6 +215,19 @@ public class CalciteExplainIT extends ExplainIT {
                 + " address_length = length(address) | stats count() by address_length");
     String expected = loadFromFile("expectedOutput/calcite/explain_script_push_on_text.json");
     assertJsonEqualsIgnoreId(expected, result);
+  }
+
+  // Only for Calcite, as v2 gets unstable serialized string for function
+  @Test
+  public void testExplainOnAggregationWithSumEnhancement() throws IOException {
+    String expected = loadExpectedPlan("explain_agg_with_sum_enhancement.json");
+    assertJsonEqualsIgnoreId(
+        expected,
+        explainQueryToString(
+            String.format(
+                "source=%s | stats sum(balance), sum(balance + 100), sum(balance - 100),"
+                    + " sum(balance * 100), sum(balance / 100) by gender",
+                TEST_INDEX_BANK)));
   }
 
   /**
