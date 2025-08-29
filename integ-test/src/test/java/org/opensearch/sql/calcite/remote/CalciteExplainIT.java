cf208fcd387a6b29e6ef21d93e291074cec2e095
Jialiang Liang <jiallian@amazon.com>
2025-08-29 01:47:13 -0700

    DECOUPLE SED + OFFSET FIELD
    Signed-off-by: Jialiang Liang <jiallian@amazon.com>

--- integ-test/src/test/java/org/opensearch/sql/calcite/remote/CalciteExplainIT.java
+++ integ-test/src/test/java/org/opensearch/sql/calcite/remote/CalciteExplainIT.java
@@ -375,16 +375,6 @@ public class CalciteExplainIT extends ExplainIT {
     assertJsonEqualsIgnoreId(expected, result);
   }
 
-  @Test
-  public void testRexSedModeExplain() throws IOException {
-    String query =
-        "source=opensearch-sql_test_index_account | rex field=lastname mode=sed \\\"s/^[A-Z]/X/\\\""
-            + " | head 5";
-    var result = explainQueryToString(query);
-    String expected = loadExpectedPlan("explain_rex_sed.json");
-    assertJsonEqualsIgnoreId(expected, result);
-  }
-
   /**
    * Executes the PPL query and returns the result as a string with windows-style line breaks
    * replaced with Unix-style ones.
