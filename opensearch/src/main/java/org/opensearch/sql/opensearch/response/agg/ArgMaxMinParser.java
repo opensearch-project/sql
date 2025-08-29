e87e634d4f7069cf190f61767ea813b612aa487f
Tomoyuki Morita <moritato@amazon.com>
2025-08-29 08:25:24 -0700

    Fix spark sql
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>

--- opensearch/src/main/java/org/opensearch/sql/opensearch/response/agg/ArgMaxMinParser.java
+++ opensearch/src/main/java/org/opensearch/sql/opensearch/response/agg/ArgMaxMinParser.java
@@ -7,19 +7,16 @@ package org.opensearch.sql.opensearch.response.agg;
 
 import java.util.Collections;
 import java.util.Map;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import org.opensearch.search.SearchHit;
 import org.opensearch.search.aggregations.Aggregation;
 import org.opensearch.search.aggregations.metrics.TopHits;
 
 /** {@link TopHits} metric parser for ARG_MAX/ARG_MIN aggregations. */
-@EqualsAndHashCode
-@RequiredArgsConstructor
+@Value
 public class ArgMaxMinParser implements MetricParser {
 
-  @Getter private final String name;
+  String name;
 
   @Override
   public Map<String, Object> parse(Aggregation agg) {
@@ -30,20 +27,11 @@ public class ArgMaxMinParser implements MetricParser {
       return Collections.singletonMap(agg.getName(), null);
     }
 
-    // For ARG_MAX/ARG_MIN, we only need the first (and only) hit
-    SearchHit hit = hits[0];
-    Map<String, Object> source = hit.getSourceAsMap();
+    Map<String, Object> source = hits[0].getSourceAsMap();
 
-    // Since we configured fetchSource to only include the value field,
-    // we should have exactly one field in the source
-    if (source.size() == 1) {
-      Object value = source.values().iterator().next();
-      return Collections.singletonMap(agg.getName(), value);
-    } else if (source.isEmpty()) {
+    if (source.isEmpty()) {
       return Collections.singletonMap(agg.getName(), null);
     } else {
-      // This shouldn't happen if fetchSource is configured correctly,
-      // but as a fallback, return the first value
       Object value = source.values().iterator().next();
       return Collections.singletonMap(agg.getName(), value);
     }
