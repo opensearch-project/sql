dc79b18a851b6caa83e179b0df146b53ad9eeaf3
Simeon Widdis <sawiddis@amazon.com>
2025-09-05 21:56:58 +0000

    I hate license headers
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>

--- async-query/src/test/java/org/opensearch/sql/spark/asyncquery/indexquery/IndexQuerySpecBase.java
+++ async-query/src/test/java/org/opensearch/sql/spark/asyncquery/indexquery/IndexQuerySpecBase.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.opensearch.sql.spark.asyncquery.indexquery;
 
 import org.opensearch.sql.spark.asyncquery.AsyncQueryExecutorServiceSpec;
