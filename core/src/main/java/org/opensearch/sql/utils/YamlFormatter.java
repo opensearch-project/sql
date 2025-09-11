6d694402aa7a7ddc61bdd6756da6f3df9fe8158f
Tomoyuki MORITA <moritato@amazon.com>
2025-09-11 15:46:44 -0700

    Introduce YAML formatter for better testing/debugging (#4274)
    * Implement YamlFormatter
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>
    
    * Enable YAML based plan comparison in tests
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>
    
    * Fix line break issue in Windows
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>
    
    * Minor fix in test case
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>
    
    * Fix line break issue
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>
    
    * Fix comment
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>
    
    ---------
    
    Signed-off-by: Tomoyuki Morita <moritato@amazon.com>

---
+++ core/src/main/java/org/opensearch/sql/utils/YamlFormatter.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.sql.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
+
+/**
+ * YAML formatter utility class. Attributes are sorted alphabetically for consistent output. Check
+ * {@link YamlFormatterTest} for the actual formatting behavior.
+ */
+public class YamlFormatter {
+
+  private static final ObjectMapper YAML_MAPPER;
+
+  static {
+    YAMLFactory yamlFactory = new YAMLFactory();
+    yamlFactory.disable(YAMLGenerator.Feature.WRITE_DOC_START_MARKER);
+    yamlFactory.enable(YAMLGenerator.Feature.MINIMIZE_QUOTES); // Enable smart quoting
+    yamlFactory.enable(
+        YAMLGenerator.Feature.ALWAYS_QUOTE_NUMBERS_AS_STRINGS); // Quote numeric strings
+    yamlFactory.enable(YAMLGenerator.Feature.INDENT_ARRAYS_WITH_INDICATOR);
+    YAML_MAPPER = new ObjectMapper(yamlFactory);
+    YAML_MAPPER.enable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS);
+  }
+
+  /**
+   * Formats any object into YAML format.
+   *
+   * @param object the object to format
+   * @return YAML-formatted string representation
+   */
+  public static String formatToYaml(Object object) {
+    try {
+      return YAML_MAPPER.writeValueAsString(object);
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException("Failed to format object to YAML", e);
+    }
+  }
+}
