83bb88259c5977baae9d64a6543116d3f860a138
ps48 <pshenoy36@gmail.com>
2025-09-12 10:38:32 -0700

    use verifyQueryThrowsException in CalcitePPLFunctionTypeTest
    Signed-off-by: ps48 <pshenoy36@gmail.com>

--- ppl/src/test/java/org/opensearch/sql/ppl/calcite/CalcitePPLFunctionTypeTest.java
+++ ppl/src/test/java/org/opensearch/sql/ppl/calcite/CalcitePPLFunctionTypeTest.java
@@ -176,23 +176,11 @@ public class CalcitePPLFunctionTypeTest extends CalcitePPLAbstractTest {
         "LOG2 function expects {[INTEGER]|[DOUBLE]}, but got [STRING,STRING]");
   }
 
-  @Test
-  public void testValuesFunctionWithScalarTypes() {
-    // Test VALUES function with supported scalar types
-    Assert.assertNotNull(getRelNode("source=EMP | stats values(ENAME) as unique_names"));
-    Assert.assertNotNull(getRelNode("source=EMP | stats values(EMPNO) as unique_ids"));
-    Assert.assertNotNull(getRelNode("source=EMP | stats values(SAL) as unique_salaries"));
-  }
-
+  // Test VALUES function with array expression (which is not a supported scalar type)
   @Test
   public void testValuesFunctionWithArrayArgType() {
-    // Test VALUES function with array expression (which is not a supported scalar type)
-    Exception e =
-        Assert.assertThrows(
-            ExpressionEvaluationException.class,
-            () -> getRelNode("source=EMP | stats values(array(ENAME, JOB)) as unique_values"));
-    verifyErrorMessageContains(
-        e,
+    verifyQueryThrowsException(
+        "source=EMP | stats values(array(ENAME, JOB)) as unique_values",
         "Aggregation function VALUES expects field type"
             + " {[BYTE],[SHORT],[INTEGER],[LONG],[FLOAT],[DOUBLE],[STRING],[BOOLEAN],[DATE],[TIME],[TIMESTAMP],[IP],[BINARY]},"
             + " but got [ARRAY]");
