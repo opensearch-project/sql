f9cd7eeec7f4e41a7c6954ae1cf8667d64b51eae
Simeon Widdis <sawiddis@gmail.com>
2025-09-17 08:05:48 -0700

    Add spotless precommit hook + license check (#4306)
    * Add spotless precommit hook
    
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>
    
    * Decouple plugin spotless versions + upgrade spotless
    
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>
    
    * Enable license headers everywhere
    
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>
    
    * Remove a redundant comment
    
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>
    
    * Fix removed additional licenses
    
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>
    
    ---------
    
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>

--- opensearch/src/main/java/org/opensearch/sql/opensearch/request/AggregateAnalyzer.java
+++ opensearch/src/main/java/org/opensearch/sql/opensearch/request/AggregateAnalyzer.java
@@ -24,6 +24,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.opensearch.sql.opensearch.request;
 
 import static java.util.Objects.requireNonNull;
