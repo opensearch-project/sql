65a3effd132c6eebbcd908fdae98b9ee77f334af
Jialiang Liang <jiallian@amazon.com>
2025-10-08 15:06:50 -0700

    peng - Add test for verify the fix for parse and yamlIT
    Signed-off-by: Jialiang Liang <jiallian@amazon.com>

--- integ-test/src/test/java/org/opensearch/sql/calcite/remote/CalciteParseCommandIT.java
+++ integ-test/src/test/java/org/opensearch/sql/calcite/remote/CalciteParseCommandIT.java
@@ -5,6 +5,10 @@
 
 package org.opensearch.sql.calcite.remote;
 
+import static org.opensearch.sql.legacy.TestsConstants.TEST_INDEX_BANK;
+
+import java.io.IOException;
+import org.junit.Test;
 import org.opensearch.sql.ppl.ParseCommandIT;
 
 public class CalciteParseCommandIT extends ParseCommandIT {
@@ -12,5 +16,19 @@ public class CalciteParseCommandIT extends ParseCommandIT {
   public void init() throws Exception {
     super.init();
     enableCalcite();
+  }
+
+  @Test
+  public void testParseErrorUnderscoreInGroupNames() throws IOException {
+    try {
+      executeQuery(
+          String.format(
+              "source=%s | parse email '.+@(?<host_name>.+)' | fields email", TEST_INDEX_BANK));
+      fail("Should have thrown an exception for underscore in named capture group");
+    } catch (Exception e) {
+      assertTrue(
+          e.getMessage()
+              .contains("Underscores are not permitted in Java Regex capture group names"));
+    }
   }
 }
