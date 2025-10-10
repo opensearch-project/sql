343367ada3ffc4c5db9da05dc87059c1f55b4182
Kai Huang <ahkcs@amazon.com>
2025-10-10 15:51:25 -0700

    error code handling
    Signed-off-by: Kai Huang <ahkcs@amazon.com>

--- plugin/src/main/java/org/opensearch/sql/plugin/rest/RestPPLQueryAction.java
+++ plugin/src/main/java/org/opensearch/sql/plugin/rest/RestPPLQueryAction.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.PatternSyntaxException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.opensearch.OpenSearchException;
@@ -59,7 +60,9 @@ public class RestPPLQueryAction extends BaseRestHandler {
         || e instanceof QueryEngineException
         || e instanceof SyntaxCheckException
         || e instanceof DataSourceClientException
-        || e instanceof IllegalAccessException;
+        || e instanceof IllegalAccessException
+        || e instanceof PatternSyntaxException
+        || (e.getCause() instanceof PatternSyntaxException);
   }
 
   @Override
