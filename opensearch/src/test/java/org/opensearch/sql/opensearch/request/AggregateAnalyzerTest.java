b116dd6badcd20d60bfafbb4601362729c4245b4
Yuanchun Shen <yuanchu@amazon.com>
2025-10-24 16:45:45 +0800

    [Backport 2.19-dev] Pushdown case function in aggregations as range queries (#4400) (#4630)
    * Pushdown case function in aggregations as range queries (#4400)
    
    * WIP: implementing case range analyzer
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Correct case analyzer
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Create bucket aggregation parsers that supports parsing nested sub aggregations
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Fix unit tests
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Fix parsers to multi-range cases
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Update leaf bucket parser
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Unit test case range analyzer
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Add explain ITs for pushing down case in aggregations
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Update CaseRangeAnalyzerTest
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Add a yaml test that replicates issue 4201
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Add integration tests for case in aggregation
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Fix unit tests
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Add a patch to CalcitePPLCaseFunctionIT
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Migrate all composite aggregation parser usage to bucket aggregate parser
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Create a parent abstract classes for BucketAggregationParsers
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Remove an unnecessary bucket agg in AggregationQueryBuilder
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Test pushing down case where there exists null values
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Return empty in CaseRangeAnalyzer to unblock the rest pushdown
    - Additionally test number as result expressions
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Document limitations of pushding case as range queries
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Make case pushdown a private method
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Chores: remove unused helper method
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Unify logics for creating nested aggregations
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Remove a note in condition.rst
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Optmize range aggregation
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Ignore testNestedAggregationsExplain when pushdown is disabled
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Fix explain ITs after merge
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    ---------
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    (cherry picked from commit 18ab4dc9a02589e4348d67fa4b38a66d27ad7f59)
    
    # Conflicts:
    #	integ-test/src/test/java/org/opensearch/sql/calcite/remote/CalciteExplainIT.java
    
    * Downgrade langauge level to java 11
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    * Delete an IT due to system-incompatible formatting issue
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>
    
    ---------
    
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>

--- opensearch/src/test/java/org/opensearch/sql/opensearch/request/AggregateAnalyzerTest.java
+++ opensearch/src/test/java/org/opensearch/sql/opensearch/request/AggregateAnalyzerTest.java
@@ -18,6 +18,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+
 import org.apache.calcite.rel.RelCollations;
 import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.core.Aggregate;
@@ -46,7 +48,7 @@ import org.opensearch.sql.expression.function.PPLBuiltinOperators;
 import org.opensearch.sql.opensearch.data.type.OpenSearchDataType;
 import org.opensearch.sql.opensearch.data.type.OpenSearchDataType.MappingType;
 import org.opensearch.sql.opensearch.request.AggregateAnalyzer.ExpressionNotAnalyzableException;
-import org.opensearch.sql.opensearch.response.agg.CompositeAggregationParser;
+import org.opensearch.sql.opensearch.response.agg.BucketAggregationParser;
 import org.opensearch.sql.opensearch.response.agg.FilterParser;
 import org.opensearch.sql.opensearch.response.agg.MetricParserHelper;
 import org.opensearch.sql.opensearch.response.agg.NoBucketAggregationParser;
@@ -283,9 +285,10 @@ class AggregateAnalyzerTest {
             + "{\"a\":{\"terms\":{\"field\":\"a\",\"missing_bucket\":true,\"missing_order\":\"first\",\"order\":\"asc\"}}},"
             + "{\"b\":{\"terms\":{\"field\":\"b.keyword\",\"missing_bucket\":true,\"missing_order\":\"first\",\"order\":\"asc\"}}}]}}}]",
         result.getLeft().toString());
-    assertInstanceOf(CompositeAggregationParser.class, result.getRight());
+    assertInstanceOf(BucketAggregationParser.class, result.getRight());
+    assertInstanceOf(BucketAggregationParser.class, result.getRight());
     MetricParserHelper metricsParser =
-        ((CompositeAggregationParser) result.getRight()).getMetricsParser();
+        ((BucketAggregationParser) result.getRight()).getMetricsParser();
     assertEquals(1, metricsParser.getMetricParserMap().size());
     metricsParser
         .getMetricParserMap()
@@ -594,8 +597,11 @@ class AggregateAnalyzerTest {
       when(ref.getType()).thenReturn(typeFactory.createSqlType(SqlTypeName.INTEGER));
       rexNodes.add(ref);
     }
+    List<org.apache.calcite.util.Pair<RexNode, String>> namedProjects =
+        rexNodes.stream().map(n -> org.apache.calcite.util.Pair.of(n, n.toString())).collect(Collectors.toList());
     when(project.getProjects()).thenReturn(rexNodes);
     when(project.getRowType()).thenReturn(rowType);
+    when(project.getNamedProjects()).thenReturn(namedProjects);
     return project;
   }
 
