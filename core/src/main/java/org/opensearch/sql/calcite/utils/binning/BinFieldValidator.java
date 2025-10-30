03fd5cc37f0acff95b0c582d78e96723cc353f08
Kai Huang <ahkcs@amazon.com>
2025-10-29 17:28:33 -0700

    move type checking utilities
    Signed-off-by: Kai Huang <ahkcs@amazon.com>

--- core/src/main/java/org/opensearch/sql/calcite/utils/binning/BinFieldValidator.java
+++ core/src/main/java/org/opensearch/sql/calcite/utils/binning/BinFieldValidator.java
@@ -7,13 +7,10 @@ package org.opensearch.sql.calcite.utils.binning;
 
 import java.util.List;
 import org.apache.calcite.rel.type.RelDataType;
-import org.apache.calcite.sql.type.SqlTypeName;
 import org.opensearch.sql.ast.expression.Field;
 import org.opensearch.sql.ast.tree.Bin;
 import org.opensearch.sql.calcite.CalcitePlanContext;
-import org.opensearch.sql.calcite.type.AbstractExprRelDataType;
-import org.opensearch.sql.data.type.ExprCoreType;
-import org.opensearch.sql.data.type.ExprType;
+import org.opensearch.sql.calcite.utils.OpenSearchTypeFactory;
 import org.opensearch.sql.exception.SemanticCheckException;
 
 /** Utility class for field validation and type checking in bin operations. */
@@ -41,24 +38,7 @@ public class BinFieldValidator {
 
   /** Checks if the field type is time-based. */
   public static boolean isTimeBasedField(RelDataType fieldType) {
-    // Check standard SQL time types
-    SqlTypeName sqlType = fieldType.getSqlTypeName();
-    if (sqlType == SqlTypeName.TIMESTAMP
-        || sqlType == SqlTypeName.TIMESTAMP_WITH_LOCAL_TIME_ZONE
-        || sqlType == SqlTypeName.DATE) {
-      return true;
-    }
-
-    // Check for OpenSearch UDT types (EXPR_TIMESTAMP mapped to VARCHAR)
-    if (fieldType instanceof AbstractExprRelDataType<?> exprType) {
-      ExprType udtType = exprType.getExprType();
-      return udtType == ExprCoreType.TIMESTAMP
-          || udtType == ExprCoreType.DATE
-          || udtType == ExprCoreType.TIME;
-    }
-
-    // Check if type string contains EXPR_TIMESTAMP
-    return fieldType.toString().contains("EXPR_TIMESTAMP");
+    return OpenSearchTypeFactory.isTimeBasedType(fieldType);
   }
 
   /** Validates that the field type is numeric for numeric binning operations. */
@@ -74,25 +54,6 @@ public class BinFieldValidator {
 
   /** Checks if the field type is numeric. */
   private static boolean isNumericField(RelDataType fieldType) {
-    // Check standard SQL numeric types
-    SqlTypeName sqlType = fieldType.getSqlTypeName();
-    if (sqlType == SqlTypeName.INTEGER
-        || sqlType == SqlTypeName.BIGINT
-        || sqlType == SqlTypeName.SMALLINT
-        || sqlType == SqlTypeName.TINYINT
-        || sqlType == SqlTypeName.FLOAT
-        || sqlType == SqlTypeName.DOUBLE
-        || sqlType == SqlTypeName.DECIMAL
-        || sqlType == SqlTypeName.REAL) {
-      return true;
-    }
-
-    // Check for OpenSearch UDT numeric types
-    if (fieldType instanceof AbstractExprRelDataType<?> exprType) {
-      ExprType udtType = exprType.getExprType();
-      return ExprCoreType.numberTypes().contains(udtType);
-    }
-
-    return false;
+    return OpenSearchTypeFactory.isNumericType(fieldType);
   }
 }
