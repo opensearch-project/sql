0d40418fc464c92e2543066a02c56f07cda39846
Kai Huang <ahkcs@amazon.com>
2025-11-20 13:22:33 -0800

    Support  eval function
    Signed-off-by: Kai Huang <ahkcs@amazon.com>

--- core/src/main/java/org/opensearch/sql/expression/function/BuiltinFunctionName.java
+++ core/src/main/java/org/opensearch/sql/expression/function/BuiltinFunctionName.java
@@ -75,6 +75,7 @@ public enum BuiltinFunctionName {
   MVAPPEND(FunctionName.of("mvappend")),
   MVJOIN(FunctionName.of("mvjoin")),
   MVINDEX(FunctionName.of("mvindex")),
+  MVFIND(FunctionName.of("mvfind")),
   FORALL(FunctionName.of("forall")),
   EXISTS(FunctionName.of("exists")),
   FILTER(FunctionName.of("filter")),
