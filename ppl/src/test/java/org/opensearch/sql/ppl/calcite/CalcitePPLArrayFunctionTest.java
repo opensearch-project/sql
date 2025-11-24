883f84ced2ca73befda20b3d1969d74112ff03ac
Kai Huang <ahkcs@amazon.com>
2025-11-24 15:30:25 -0800

    update UT
    Signed-off-by: Kai Huang <ahkcs@amazon.com>

--- ppl/src/test/java/org/opensearch/sql/ppl/calcite/CalcitePPLArrayFunctionTest.java
+++ ppl/src/test/java/org/opensearch/sql/ppl/calcite/CalcitePPLArrayFunctionTest.java
@@ -521,8 +521,11 @@ public class CalcitePPLArrayFunctionTest extends CalcitePPLAbstractTest {
             + "      LogicalTableScan(table=[[scott, EMP]])\n";
     verifyLogical(root, expectedLogical);
 
-    String expectedResult = "result=[10, 20, 30]\n";
-    verifyResult(root, expectedResult);
+    String expectedSparkSql =
+        "SELECT TRANSFORM(ARRAY(1, 2, 3), `arr` -> `arr` * 10) `result`\n"
+            + "FROM `scott`.`EMP`\n"
+            + "LIMIT 1";
+    verifyPPLToSparkSQL(root, expectedSparkSql);
   }
 
   @Test
@@ -532,7 +535,19 @@ public class CalcitePPLArrayFunctionTest extends CalcitePPLAbstractTest {
             + " fields result";
     RelNode root = getRelNode(ppl);
 
-    String expectedResult = "result=[6, 7, 8]\n";
-    verifyResult(root, expectedResult);
+    String expectedLogical =
+        "LogicalProject(result=[$9])\n"
+            + "  LogicalSort(fetch=[1])\n"
+            + "    LogicalProject(EMPNO=[$0], ENAME=[$1], JOB=[$2], MGR=[$3], HIREDATE=[$4],"
+            + " SAL=[$5], COMM=[$6], DEPTNO=[$7], arr=[array(1, 2, 3)],"
+            + " result=[transform(array(1, 2, 3), (arr) -> +(arr, 5))])\n"
+            + "      LogicalTableScan(table=[[scott, EMP]])\n";
+    verifyLogical(root, expectedLogical);
+
+    String expectedSparkSql =
+        "SELECT TRANSFORM(ARRAY(1, 2, 3), `arr` -> `arr` + 5) `result`\n"
+            + "FROM `scott`.`EMP`\n"
+            + "LIMIT 1";
+    verifyPPLToSparkSQL(root, expectedSparkSql);
   }
 }
