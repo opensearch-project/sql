8bac909e0bfe278016b4e33ce1d18dc9e34683b4
Yuanchun Shen <yuanchu@amazon.com>
2025-12-08 15:33:56 +0800

    Define operand types for json functions
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>

--- core/src/main/java/org/opensearch/sql/expression/function/jsonUDF/JsonDeleteFunctionImpl.java
+++ core/src/main/java/org/opensearch/sql/expression/function/jsonUDF/JsonDeleteFunctionImpl.java
@@ -20,6 +20,7 @@ import org.apache.calcite.linq4j.tree.Expression;
 import org.apache.calcite.linq4j.tree.Types;
 import org.apache.calcite.rex.RexCall;
 import org.apache.calcite.schema.impl.ScalarFunctionImpl;
+import org.apache.calcite.sql.type.OperandTypes;
 import org.apache.calcite.sql.type.SqlReturnTypeInference;
 import org.opensearch.sql.expression.function.ImplementorUDF;
 import org.opensearch.sql.expression.function.UDFOperandMetadata;
@@ -36,7 +37,7 @@ public class JsonDeleteFunctionImpl extends ImplementorUDF {
 
   @Override
   public UDFOperandMetadata getOperandMetadata() {
-    return null;
+    return UDFOperandMetadata.wrap(OperandTypes.ONE_OR_MORE);
   }
 
   public static class JsonDeleteImplementor implements NotNullImplementor {
