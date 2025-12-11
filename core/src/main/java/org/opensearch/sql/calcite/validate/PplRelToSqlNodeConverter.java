738129d3cfa01cc6c0ec80ce69f3cd70bfa559f6
Yuanchun Shen <yuanchu@amazon.com>
2025-12-11 23:36:33 +0800

    Pass on join type of logical correlate to lateral join (1858/2027)
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>

--- core/src/main/java/org/opensearch/sql/calcite/validate/PplRelToSqlNodeConverter.java
+++ core/src/main/java/org/opensearch/sql/calcite/validate/PplRelToSqlNodeConverter.java
@@ -5,8 +5,15 @@
 
 package org.opensearch.sql.calcite.validate;
 
+import org.apache.calcite.rel.core.Correlate;
+import org.apache.calcite.rel.core.JoinRelType;
 import org.apache.calcite.rel.rel2sql.RelToSqlConverter;
+import org.apache.calcite.sql.JoinConditionType;
+import org.apache.calcite.sql.JoinType;
 import org.apache.calcite.sql.SqlDialect;
+import org.apache.calcite.sql.SqlJoin;
+import org.apache.calcite.sql.SqlLiteral;
+import org.apache.calcite.sql.SqlNode;
 
 /**
  * An extension of {@link RelToSqlConverter} to convert a relation algebra tree, translated from a
@@ -23,5 +30,27 @@ public class PplRelToSqlNodeConverter extends RelToSqlConverter {
    */
   public PplRelToSqlNodeConverter(SqlDialect dialect) {
     super(dialect);
+  }
+
+  /** Override Correlate visitor to pass on join type */
+  @Override
+  public Result visit(Correlate e) {
+    Result result = super.visit(e);
+    SqlNode from = result.asSelect().getFrom();
+    if (e.getJoinType() != JoinRelType.INNER && from instanceof SqlJoin join) {
+      JoinType joinType;
+      try {
+        joinType = JoinType.valueOf(e.getJoinType().name());
+      } catch (IllegalArgumentException ignored) {
+        return result;
+      }
+      join.setOperand(2, joinType.symbol(POS));
+      // INNER, LEFT, RIGHT, FULL, or ASOF join requires a condition
+      // Use ON TRUE to satisfy SQL syntax because the actual correlation condition logic is inside
+      // the subquery's WHERE clause
+      join.setOperand(4, JoinConditionType.ON.symbol(POS));
+      join.setOperand(5, SqlLiteral.createBoolean(true, POS));
+    }
+    return result;
   }
 }
