197cf6039d45bbf472e293e658481e820a1fdf08
Lantao Jin <ltjin@amazon.com>
2025-12-12 14:08:29 +0800

    address comment
    Signed-off-by: Lantao Jin <ltjin@amazon.com>

--- integ-test/src/test/java/org/opensearch/sql/calcite/remote/CalciteExplainIT.java
+++ integ-test/src/test/java/org/opensearch/sql/calcite/remote/CalciteExplainIT.java
@@ -30,7 +30,6 @@ import org.opensearch.sql.ppl.ExplainIT;
 public class CalciteExplainIT extends ExplainIT {
   @Override
   public void init() throws Exception {
-    GlobalPushdownConfig.enabled = false;
     super.init();
     enableCalcite();
     setQueryBucketSize(1000);
