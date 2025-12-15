15f17b34ab689ebad6c69100261d434e088fefd4
Yuanchun Shen <yuanchu@amazon.com>
2025-12-15 17:40:13 +0800

    Fix float literal by inserting compulsory cast
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>

--- core/src/main/java/org/opensearch/sql/calcite/validate/shuttles/PplRelToSqlRelShuttle.java
+++ core/src/main/java/org/opensearch/sql/calcite/validate/shuttles/PplRelToSqlRelShuttle.java
@@ -6,6 +6,7 @@
 package org.opensearch.sql.calcite.validate.shuttles;
 
 import java.math.BigDecimal;
+import java.util.List;
 import org.apache.calcite.avatica.util.TimeUnit;
 import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.RelShuttleImpl;
@@ -14,10 +15,12 @@ import org.apache.calcite.rex.RexLiteral;
 import org.apache.calcite.rex.RexNode;
 import org.apache.calcite.rex.RexShuttle;
 import org.apache.calcite.sql.SqlIntervalQualifier;
+import org.apache.calcite.sql.fun.SqlLibraryOperators;
+import org.apache.calcite.sql.type.SqlTypeName;
 
 /**
  * A RelShuttle that recursively visits all RelNodes and their RexNode expressions to fix interval
- * literals before/after SQL conversion.
+ * literals and float literal before/after SQL conversion.
  *
  * <p>This shuttle extends RelShuttleImpl to ensure it visits the entire RelNode tree recursively,
  * applying the interval literal fixes at each node.
@@ -28,8 +31,23 @@ public class PplRelToSqlRelShuttle extends RelShuttleImpl {
   public PplRelToSqlRelShuttle(RexBuilder rexBuilder, boolean forward) {
     this.rexShuttle =
         new RexShuttle() {
+          /**
+           * This visitor fixes: 1. float literal: when converting logical plan to sql node, float
+           * information is missing. All floats will be treated as double. A compulsory cast is
+           * inserted here to ensure a cast presents in the generated SQL 2. interval literal: we
+           * create and read the interval literal in a different way that how Calcite originally
+           * expected it to be.
+           */
           @Override
           public RexNode visitLiteral(RexLiteral literal) {
+            // 1. Fix float literal
+            SqlTypeName literalType = literal.getType().getSqlTypeName();
+            if (SqlTypeName.REAL.equals(literalType) || SqlTypeName.FLOAT.equals(literalType)) {
+              return rexBuilder.makeCall(
+                  literal.getType(), SqlLibraryOperators.SAFE_CAST, List.of(literal));
+            }
+
+            // 2. Fix interval literal
             SqlIntervalQualifier qualifier = literal.getType().getIntervalQualifier();
             if (qualifier == null) {
               return literal;
