b7af26bb1e20d78cfc4bd3ce2b267d73a9128865
Asif Bashar <asif.bashar@gmail.com>
2026-01-13 00:03:22 -0800

    for cross cluster failure debugging
    Signed-off-by: Asif Bashar <asif.bashar@gmail.com>

--- integ-test/src/test/java/org/opensearch/sql/security/CrossClusterSearchIT.java
+++ integ-test/src/test/java/org/opensearch/sql/security/CrossClusterSearchIT.java
@@ -282,6 +282,7 @@ public class CrossClusterSearchIT extends PPLIntegTestCase {
                     + " firstname='Dale'|sort firstname desc |fields firstname,age,balance |"
                     + " transpose 3 column_name='column_names'",
                 TEST_INDEX_BANK_REMOTE));
+    System.out.println(result.toString());
     verifyDataRows(
         result,
         rows("firstname", "Nanette", "Hattie", "Dale"),
