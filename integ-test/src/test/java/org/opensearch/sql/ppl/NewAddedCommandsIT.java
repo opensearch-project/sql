7fef1b075a5074e2b735b33c1baef0565de2c8de
Asif Bashar <asif.bashar@gmail.com>
2026-01-27 14:36:43 -0800

    added test
    Signed-off-by: Asif Bashar <asif.bashar@gmail.com>

--- integ-test/src/test/java/org/opensearch/sql/ppl/NewAddedCommandsIT.java
+++ integ-test/src/test/java/org/opensearch/sql/ppl/NewAddedCommandsIT.java
@@ -213,21 +213,20 @@ public class NewAddedCommandsIT extends PPLIntegTestCase {
     }
   }
 
-    @Test
-    public void testFieldFormatCommand() throws IOException {
-        JSONObject result;
-        try {
-            executeQuery(
-                    String.format(
-                            "search source=%s  | fieldformat double_balance = balance * 2 ", TEST_INDEX_BANK));
-        } catch (ResponseException e) {
-            result = new JSONObject(TestUtils.getResponseBody(e.getResponse()));
-            verifyQuery(result);
-        }
+  @Test
+  public void testFieldFormatCommand() throws IOException {
+    JSONObject result;
+    try {
+      executeQuery(
+          String.format(
+              "search source=%s  | fieldformat double_balance = balance * 2 ", TEST_INDEX_BANK));
+    } catch (ResponseException e) {
+      result = new JSONObject(TestUtils.getResponseBody(e.getResponse()));
+      verifyQuery(result);
     }
+  }
 
-
-  private void verifyQuery(JSONObject result) throws IOException {
+    private void verifyQuery(JSONObject result) throws IOException {
     if (isCalciteEnabled()) {
       assertFalse(result.getJSONArray("datarows").isEmpty());
     } else {
