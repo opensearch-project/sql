5c594e96ab20309e2de182bfd5f93bb48f54bbd8
Eric Wei <mengwei.eric@gmail.com>
2026-04-07 16:06:37 -0700

    Register VectorSearchTableFunctionResolver in OpenSearchStorageEngine
    Override getFunctions() to expose vectorsearch() table function
    to the query analysis pipeline.
    
    Signed-off-by: Eric Wei <mengwei.eric@gmail.com>

--- opensearch/src/test/java/org/opensearch/sql/opensearch/storage/OpenSearchStorageEngineTest.java
+++ opensearch/src/test/java/org/opensearch/sql/opensearch/storage/OpenSearchStorageEngineTest.java
@@ -6,17 +6,20 @@
 package org.opensearch.sql.opensearch.storage;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.opensearch.sql.analysis.DataSourceSchemaIdentifierNameResolver.DEFAULT_DATASOURCE_NAME;
 import static org.opensearch.sql.utils.SystemIndexUtils.TABLE_INFO;
 
+import java.util.Collection;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.opensearch.sql.DataSourceSchemaName;
 import org.opensearch.sql.common.setting.Settings;
+import org.opensearch.sql.expression.function.FunctionResolver;
 import org.opensearch.sql.opensearch.client.OpenSearchClient;
 import org.opensearch.sql.opensearch.storage.system.OpenSearchSystemIndex;
 import org.opensearch.sql.storage.Table;
@@ -34,6 +37,14 @@ class OpenSearchStorageEngineTest {
     Table table =
         engine.getTable(new DataSourceSchemaName(DEFAULT_DATASOURCE_NAME, "default"), "test");
     assertAll(() -> assertNotNull(table), () -> assertTrue(table instanceof OpenSearchIndex));
+  }
+
+  @Test
+  public void getFunctionsReturnsVectorSearchResolver() {
+    OpenSearchStorageEngine engine = new OpenSearchStorageEngine(client, settings);
+    Collection<FunctionResolver> functions = engine.getFunctions();
+    assertEquals(1, functions.size());
+    assertTrue(functions.iterator().next() instanceof VectorSearchTableFunctionResolver);
   }
 
   @Test
