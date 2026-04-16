91d6ddea52ddf1276cb71a3575f56c96c6390281
Kai Huang <ahkcs@amazon.com>
2026-04-16 14:42:09 -0700

    Replace direct JsonXContentParser construction with public factory API
    OpenSearch 3.7.0's JsonXContentParser constructor requires Jackson 3's
    tools.jackson.core.JsonParser, but call sites were passing Jackson 2's
    com.fasterxml.jackson.core.JsonParser (via JsonFactory.createParser).
    
    Switch all such call sites to JsonXContent.jsonXContent.createParser
    (and YamlXContent.yamlXContent.createParser in HintFactory), which take
    a plain String and hide Jackson's type in their signatures, so the
    underlying migration is invisible to callers.
    
    Signed-off-by: Kai Huang <ahkcs@amazon.com>

--- opensearch/src/test/java/org/opensearch/sql/opensearch/response/AggregationResponseUtils.java
+++ opensearch/src/test/java/org/opensearch/sql/opensearch/response/AggregationResponseUtils.java
@@ -5,13 +5,12 @@
 
 package org.opensearch.sql.opensearch.response;
 
-import com.fasterxml.jackson.core.JsonFactory;
 import com.google.common.collect.ImmutableMap;
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.opensearch.common.xcontent.LoggingDeprecationHandler;
-import org.opensearch.common.xcontent.json.JsonXContentParser;
+import org.opensearch.common.xcontent.json.JsonXContent;
 import org.opensearch.core.ParseField;
 import org.opensearch.core.xcontent.ContextParser;
 import org.opensearch.core.xcontent.NamedXContentRegistry;
@@ -111,10 +110,8 @@ public class AggregationResponseUtils {
   public static Aggregations fromJson(String json) {
     try {
       XContentParser contentParser =
-          new JsonXContentParser(
-              namedXContentRegistry,
-              LoggingDeprecationHandler.INSTANCE,
-              new JsonFactory().createParser(json));
+          JsonXContent.jsonXContent.createParser(
+              namedXContentRegistry, LoggingDeprecationHandler.INSTANCE, json);
       contentParser.nextToken();
       return Aggregations.fromXContent(contentParser);
     } catch (IOException e) {
