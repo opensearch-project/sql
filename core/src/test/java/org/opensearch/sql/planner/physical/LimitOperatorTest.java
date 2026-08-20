81a399bfe5d659e0fbf580ea9299ee9de542ce2f
Yulong Ruan <ruanyu1@gmail.com>
2026-08-20 10:51:48 +0800

    Fix zero-offset limit consuming deduped row (#5701)
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>

--- core/src/test/java/org/opensearch/sql/planner/physical/LimitOperatorTest.java
+++ core/src/test/java/org/opensearch/sql/planner/physical/LimitOperatorTest.java
@@ -8,12 +8,14 @@ package org.opensearch.sql.planner.physical;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.opensearch.sql.data.type.ExprCoreType.INTEGER;
 
 import com.google.common.collect.ImmutableMap;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.opensearch.sql.data.model.ExprValue;
 import org.opensearch.sql.data.model.ExprValueUtils;
+import org.opensearch.sql.expression.DSL;
 
 public class LimitOperatorTest extends PhysicalPlanTestBase {
 
@@ -62,5 +64,16 @@ public class LimitOperatorTest extends PhysicalPlanTestBase {
     PhysicalPlan plan = new LimitOperator(new TestScan(), 1, 6);
     List<ExprValue> result = execute(plan);
     assertEquals(0, result.size());
+  }
+
+  @Test
+  public void zero_offset_limit_does_not_skip_first_deduplicated_row() {
+    PhysicalPlan dedupe = new DedupeOperator(new TestScan(), List.of(DSL.ref("response", INTEGER)));
+    PhysicalPlan plan = new LimitOperator(dedupe, 500, 0);
+
+    List<ExprValue> result = execute(plan);
+
+    assertEquals(3, result.size());
+    assertEquals(200, result.get(0).tupleValue().get("response").integerValue());
   }
 }
